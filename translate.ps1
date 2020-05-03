cfd8114b44b1ed115b39eef199b1204ebdf37695
Markus Fleschutz <markus@fleschutz.de>
2020-05-03 16:54:49 +0000

    Added translate.ps1

---
+++ translate.ps1
@@ -0,0 +1,36 @@
+# PowerShell Script for Translating Texts
+# ---------------------------------------
+# Author:  Markus Fleschutz
+# Source:  github.com/fleschutz/PowerShell
+# License: CC0
+
+$SourceText = "Hello World!"
+$SourceLang = "en"
+$TargetLanguages = "af","da","de","el","es","hr","it","ja","ko","pl","pt","nl","ru","tr","uk","vi"
+
+function TranslateWithGoogle {
+	[CmdletBinding()]
+	param (
+	 	[Parameter(Position = 1, ValueFromPipeline = $true)]
+	 	[ValidateNotNullorEmpty()]
+	 	[string]$Text,
+
+	 	[Parameter(Position = 2)]
+	 	[ValidateNotNullorEmpty()]
+	 	[string]$SourceLang,
+
+	 	[Parameter(Position = 3)]
+	 	[ValidateNotNullorEmpty()]
+	 	[string]$TargetLang
+	)
+
+	$URL = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=$($SourceLang)&tl=$($TargetLang)&dt=t&q=$($Text)"
+	$result = Invoke-RestMethod $URL
+	return $result[0][0][0]
+}
+
+foreach($TargetLang in $TargetLanguages) {
+	$Result = TranslateWithGoogle $SourceText $SourceLang $TargetLang
+	write-output $TargetLang" : "$Result
+}
+exit 0
