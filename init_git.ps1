33c3699d228ae59c88dfbb5f2f40b2b80d68410a
Markus Fleschutz <markus@fleschutz.de>
2020-05-20 13:06:15 +0200

    Added init_git.ps1

---
+++ init_git.ps1
@@ -0,0 +1,12 @@
+#
+
+
+$UserName   = read-host "Your full name: "
+$UserEmail  = read-host "Your email address: "
+$UserEditor = read-host "Your favorite editor (nano, vi, emacs): "
+
+git config --global user.name $UserName
+git config --global user.email $UserEmail
+git config --global core.editor $UserEditor
+echo "Done."
+exit 0
