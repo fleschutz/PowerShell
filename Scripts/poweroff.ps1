91073e4cc441e7c3046bdf27e71d6642e6fbae02
Markus Fleschutz <markus@fleschutz.de>
2020-06-15 16:13:26 +0200

    Added #Requires ...

--- Scripts/poweroff.ps1
+++ Scripts/poweroff.ps1
@@ -1,9 +1,10 @@
 #!/snap/bin/powershell
 #
-# Description:	halts the local computer (administrator rights might be needed)
+# Description:	halts the local computer, administrator rights are required 
 # Author:	Markus Fleschutz
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
 #
+#Requires -RunAsAdministrator
 Stop-Computer
 exit 0
--- Scripts/reboot.ps1
+++ Scripts/reboot.ps1
@@ -1,9 +1,10 @@
 #!/snap/bin/powershell
 #
-# Description:	reboots the local computer (administrator rights might be needed)
+# Description:	reboots the local computer, administrator rights are required
 # Author:	Markus Fleschutz
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
 #
+#Requires -RunAsAdministrator
 Restart-Computer
 exit 0
