444abea63499ed8e1210ff11c8621ae8628d044a
Markus Fleschutz <markus@fleschutz.de>
2020-06-15 16:26:03 +0200

    Added MD5.ps1 and SHA1.ps1

--- Scripts/MD5.ps1
+++ Scripts/MD5.ps1
@@ -1,12 +1,12 @@
 #!/snap/bin/powershell
 #
-# Syntax:	SHA256.ps1 <file>
-# Description:	prints the SHA256 checksum of the given file
+# Syntax:	MD5.ps1 <file>
+# Description:	prints the MD5 checksum of the given file
 # Author: 	Markus Fleschutz
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
 # 
 param([string]$File)
-$Result = get-filehash $File -algorithm SHA256
+$Result = get-filehash $File -algorithm MD5
 write-host $Result.Hash
 exit 0
--- Scripts/SHA1.ps1
+++ Scripts/SHA1.ps1
@@ -1,12 +1,12 @@
 #!/snap/bin/powershell
 #
-# Syntax:	SHA256.ps1 <file>
-# Description:	prints the SHA256 checksum of the given file
+# Syntax:	SHA1.ps1 <file>
+# Description:	prints the SHA1 checksum of the given file
 # Author: 	Markus Fleschutz
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
 # 
 param([string]$File)
-$Result = get-filehash $File -algorithm SHA256
+$Result = get-filehash $File -algorithm SHA1
 write-host $Result.Hash
 exit 0
