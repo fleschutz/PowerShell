a20cd3f04d85310e10637e6e87eb40cf47daa856
Markus Fleschutz <markus@fleschutz.de>
2020-07-28 18:47:29 +0000

    Added lsmods.ps1

---
+++ Scripts/lsmods.ps1
@@ -0,0 +1,10 @@
+#!/snap/bin/powershell
+#
+# Syntax:	lsmod.ps1 
+# Description:	lists the PowerShell modules
+# Author: 	Markus Fleschutz
+# Source:	github.com/fleschutz/PowerShell
+# License:	CC0
+# 
+Get-Module
+exit 0
