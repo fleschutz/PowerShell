5fb4957e12b410b8c3fc335388c4a34e0104508a
Markus Fleschutz <markus@fleschutz.de>
2020-09-29 17:44:17 +0000

    Updated test.ps1

--- Scripts/test.ps1
+++ Scripts/test.ps1
@@ -1,10 +1,11 @@
 #!/snap/bin/powershell
 #
-# Description:	simple test script
+# Syntax:       ./test.ps1
+# Description:	simple PowerShell test script
 # Author:	Markus Fleschutz
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
 # 
-write-output "OK - PowerShell is working. Details are:"
+write-output "✔️ PowerShell works. Details are:"
 echo $PSVersionTable
 exit 0
