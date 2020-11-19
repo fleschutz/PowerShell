81e04069327793cc121c0ba19402171589e4dd91
Markus Fleschutz <markus@fleschutz.de>
2020-11-19 07:17:51 +0000

    Improved the script

--- Scripts/weather-worldwide.ps1
+++ Scripts/weather-worldwide.ps1
@@ -6,11 +6,13 @@
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
  
-$Cities="Hawaii","Los Angeles","Miami","New York","Rio de Janeiro","Paris","London","Berlin","Cape Town","Dubai","Mumbai","Singapore","Hong Kong","Peking","Tokyo","Sydney"
+$Cities="Hawaii","Los Angeles","Mexico City","Miami","New York","Rio de Janeiro","Paris","London","Berlin","Cape Town","Dubai","Mumbai","Singapore","Hong Kong","Peking","Tokyo","Sydney"
 
 try {
+	write-host "Weather Report World-wide"
+	write-host "========================="
 	foreach($City in $Cities) {
-		(Invoke-WebRequest http://wttr.in/${City}?format="%l:+%c+%t+%p+%h+%P+%w +%S ->+%s" -UserAgent "curl" ).Content
+		(Invoke-WebRequest http://wttr.in/${City}?format="* %l:+%c+%t+%p+%h+%P+%w +%S ->+%s" -UserAgent "curl" ).Content
 	}
 	exit 0
 } catch { Write-Error $Error[0] }
