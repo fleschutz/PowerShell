16adcce777cc5df01bb34fe855b28cfe01c16a13
Markus Fleschutz <markus@fleschutz.de>
2020-11-28 17:06:55 +0000

    Updated the script

--- Scripts/scan-ports.ps1
+++ Scripts/scan-ports.ps1
@@ -7,20 +7,20 @@
 # License:	CC0
 
 $network = "192.168.178"
-$port = 8000
+$port = 8080
 $range = 1..254
 $ErrorActionPreference= "silentlycontinue"
 
-foreach ($add in $range) {
+foreach($add in $range) {
 	$ip = "{0}.{1}" -F $network,$add
-	Write-Progress "Scanning Network" $ip -PercentComplete (($add/$range.Count)*100)
+	Write-Progress "Scanning IP $ip" -PercentComplete (($add/$range.Count)*100)
 	if (Test-Connection -BufferSize 32 -Count 1 -quiet -ComputerName $ip) {
 		$socket = new-object System.Net.Sockets.TcpClient($ip, $port)
 		if ($socket.Connected) {
-			write-host "$ip port $port open"
+			write-host "TCP port $port at $ip is open"
 			$socket.Close()
 		} else {
-			write-host "$ip port $port not open"
+			write-host "TCP port $port at $ip is not open"
 		}
 	}
 }
