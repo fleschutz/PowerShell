e2887fa9cd5c8a0b253d1becf4f7b6e6b25ff4eb
Markus Fleschutz <markus@fleschutz.de>
2020-12-21 15:09:54 +0000

    Removed the spacing between characters

--- Scripts/write-big.ps1
+++ Scripts/write-big.ps1
@@ -10,326 +10,326 @@ param([String]$Text)
 
 function BigA() { param([Int]$Row)
 	switch($Row) {
-	1 { return "   __    " }
-	2 { return "  /__\   " }
-	3 { return " /(__)\  " }
-	4 { return "(__)(__) " }
+	1 { return "   __   " }
+	2 { return "  /__\  " }
+	3 { return " /(__)\ " }
+	4 { return "(__/\__)" }
 	}
 }
 
 function BigB() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "| _  ) " }
-	3 { return "| _ (  " }
-	4 { return "|____) " }
+	1 { return " ____ " }
+	2 { return "| _  )" }
+	3 { return "| _ ( " }
+	4 { return "|____)" }
 	}
 }
 
 function BigC() { param([Int]$Row)
 	switch($Row) {
-	1 { return "  ___  " }
-	2 { return " / __) " }
-	3 { return "( (__  " }
-	4 { return " \___) " }
+	1 { return "  ___ " }
+	2 { return " / __)" }
+	3 { return "( (__ " }
+	4 { return " \___)" }
 	}
 }
 
 function BigD() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____   " }
-	2 { return "|  _ \  " }
-	3 { return "| (_) ) " }
-	4 { return "|____/  " }
+	1 { return " ____  " }
+	2 { return "|  _ \ " }
+	3 { return "| (_) )" }
+	4 { return "|____/ " }
 	}
 }
 
 function BigE() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "| ___) " }
-	3 { return "| __)  " }
-	4 { return "|____) " }
+	1 { return " ____ " }
+	2 { return "| ___)" }
+	3 { return "| __) " }
+	4 { return "|____)" }
 	}
 }
 
 function BigF() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "| ___) " }
-	3 { return "| __)  " }
-	4 { return "|_)    " }
+	1 { return " ____ " }
+	2 { return "| ___)" }
+	3 { return "| __) " }
+	4 { return "|_)   " }
 	}
 }
 
 function BigG() { param([Int]$Row)
 	switch($Row) {
-	1 { return "  ____  " }
-	2 { return " / ___) " }
-	3 { return "( (__-. " }
-	4 { return " \____| " }
+	1 { return "  ____ " }
+	2 { return " / ___)" }
+	3 { return "( (__-." }
+	4 { return " \____|" }
 	}
 }
 
 function BigH() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _   _  " }
-	2 { return "| |_| | " }
-	3 { return "|  _  | " }
-	4 { return "|_| |_| " }
+	1 { return " _   _ " }
+	2 { return "| |_| |" }
+	3 { return "|  _  |" }
+	4 { return "|_| |_|" }
 	}
 }
 
 function BigI() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _  " }
-	2 { return "| | " }
-	3 { return "| | " }
-	4 { return "|_| " }
+	1 { return " _ " }
+	2 { return "| |" }
+	3 { return "| |" }
+	4 { return "|_|" }
 	}
 }
 
 function BigJ() { param([Int]$Row)
 	switch($Row) {
-	1 { return "   __  " }
-	2 { return "  (  | " }
-	3 { return ".-_) | " }
-	4 { return "\____| " }
+	1 { return "   __ " }
+	2 { return "  (  |" }
+	3 { return ".-_) |" }
+	4 { return "\____|" }
 	}
 }
 
 function BigK() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _  _  " }
-	2 { return "| |/ ) " }
-	3 { return "|   (  " }
-	4 { return "|_|\_) " }
+	1 { return " _  _ " }
+	2 { return "| |/ )" }
+	3 { return "|   ( " }
+	4 { return "|_|\_)" }
 	}
 }
 
 function BigL() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _     " }
-	2 { return "| |    " }
-	3 { return "| |__  " }
-	4 { return "|____) " }
+	1 { return " _    " }
+	2 { return "| |   " }
+	3 { return "| |__ " }
+	4 { return "|____)" }
 	}
 }
 
 function BigM() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _    _  " }
-	2 { return "| \  / | " }
-	3 { return "|  \/  | " }
-	4 { return "|_|  |_| " }
+	1 { return " _    _ " }
+	2 { return "| \  / |" }
+	3 { return "|  \/  |" }
+	4 { return "|_|  |_|" }
 	}
 }
 
 function BigN() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _   _  " }
-	2 { return "| \ | | " }
-	3 { return "|  \| | " }
-	4 { return "|_| \_| " }
+	1 { return " _   _ " }
+	2 { return "| \ | |" }
+	3 { return "|  \| |" }
+	4 { return "|_| \_|" }
 	}
 }
 
 function BigO() { param([Int]$Row)
 	switch($Row) {
-	1 { return "  ___   " }
-	2 { return " / _ \  " }
-	3 { return "( (_) ) " }
-	4 { return " \___/  " }
+	1 { return "  ___  " }
+	2 { return " / _ \ " }
+	3 { return "( (_) )" }
+	4 { return " \___/ " }
 	}
 }
 
 function BigP() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "|  _ \ " }
-	3 { return "|  __/ " }
-	4 { return "|_|    " }
+	1 { return " ____ " }
+	2 { return "|  _ \" }
+	3 { return "|  __/" }
+	4 { return "|_|   " }
 	}
 }
 
 function BigQ() { param([Int]$Row)
 	switch($Row) {
-	1 { return "  ____   " }
-	2 { return " / _  \  " }
-	3 { return "( (_) (  " }
-	4 { return " \___/\\ " }
+	1 { return "  ____  " }
+	2 { return " / _  \ " }
+	3 { return "( (_) ( " }
+	4 { return " \___/\\" }
 	}
 }
 
 function BigR() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "|  _ \ " }
-	3 { return "|    / " }
-	4 { return "|_|\_) " }
+	1 { return " ____ " }
+	2 { return "|  _ \" }
+	3 { return "|    /" }
+	4 { return "|_|\_)" }
 	}
 }
 
 function BigS() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "/  __) " }
-	3 { return "\__  \ " }
-	4 { return "(____/ " }
+	1 { return " ____ " }
+	2 { return "/  __)" }
+	3 { return "\__  \" }
+	4 { return "(____/" }
 	}
 }
 
 function BigT() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _____  " }
-	2 { return "|_   _| " }
-	3 { return "  | |   " }
-	4 { return "  |_|   " }
+	1 { return " _____ " }
+	2 { return "|_   _|" }
+	3 { return "  | |  " }
+	4 { return "  |_|  " }
 	}
 }
 
 function BigU() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _   _  " }
-	2 { return "| | | | " }
-	3 { return "| |_| | " }
-	4 { return "|_____| " }
+	1 { return " _   _ " }
+	2 { return "| | | |" }
+	3 { return "| |_| |" }
+	4 { return "|_____|" }
 	}
 }
 
 function BigV() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _  _  " }
-	2 { return "( \/ ) " }
-	3 { return " \  /  " }
-	4 { return "  \/   " }
+	1 { return " _  _ " }
+	2 { return "( \/ )" }
+	3 { return " \  / " }
+	4 { return "  \/  " }
 	}
 }
 
 function BigW() { param([Int]$Row)
 	switch($Row) {
-	1 { return " __    __  " }
-	2 { return "\  \/\/  / " }
-	3 { return " \      /  " }
-	4 { return "  \_/\_/   " }
+	1 { return " __    __ " }
+	2 { return "\  \/\/  /" }
+	3 { return " \      / " }
+	4 { return "  \_/\_/  " }
 	}
 }
 
 function BigX() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _  _  " }
-	2 { return "( \/ ) " }
-	3 { return " )  (  " }
-	4 { return "(_/\_) " }
+	1 { return " _  _ " }
+	2 { return "( \/ )" }
+	3 { return " )  ( " }
+	4 { return "(_/\_)" }
 	}
 }
 
 function BigY() { param([Int]$Row)
 	switch($Row) {
-	1 { return " _  _  " }
-	2 { return "( \/ ) " }
-	3 { return " \  /  " }
-	4 { return " (__)  " }
+	1 { return " _  _ " }
+	2 { return "( \/ )" }
+	3 { return " \  / " }
+	4 { return " (__) " }
 	}
 }
 
 function BigZ() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "(_   ) " }
-	3 { return " / /_  " }
-	4 { return "(____) " }
+	1 { return " ____ " }
+	2 { return "(_   )" }
+	3 { return " / /_ " }
+	4 { return "(____)" }
 	}
 }
 
 
 function Big0() { param([Int]$Row)
 	switch($Row) {
-	1 { return "  ___   " }
-	2 { return " / _ \  " }
-	3 { return "( (_) ) " }
-	4 { return " \___/  " }
+	1 { return "  ___  " }
+	2 { return " / _ \ " }
+	3 { return "( (_) )" }
+	4 { return " \___/ " }
 	}
 }
 
 function Big1() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ___  " }
-	2 { return "/_  | " }
-	3 { return "  | | " }
-	4 { return "  |_| " }
+	1 { return " ___ " }
+	2 { return "/_  |" }
+	3 { return "  | |" }
+	4 { return "  |_|" }
 	}
 }
 
 function Big2() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ___   " }
-	2 { return "(__ \  " }
-	3 { return " / _/  " }
-	4 { return "(____) " }
+	1 { return " ___  " }
+	2 { return "(__ \ " }
+	3 { return " / _/ " }
+	4 { return "(____)" }
 	}
 }
 
 function Big3() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ___  " }
-	2 { return "(__ ) " }
-	3 { return " (_ \ " }
-	4 { return "(___/ " }
+	1 { return " ___ " }
+	2 { return "(__ )" }
+	3 { return " (_ \\" }
+	4 { return "(___/" }
 	}
 }
 
 function Big4() { param([Int]$Row)
 	switch($Row) {
-	1 { return "  __   " }
-	2 { return " /. |  " }
-	3 { return "(_  _) " }
-	4 { return "  |_|  " }
+	1 { return "  __  " }
+	2 { return " /. | " }
+	3 { return "(_  _)" }
+	4 { return "  |_| " }
 	}
 }
 
 function Big5() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ____  " }
-	2 { return "| ___) " }
-	3 { return "|__ \  " }
-	4 { return "(___/  " }
+	1 { return " ____ " }
+	2 { return "| ___)" }
+	3 { return "|__ \ " }
+	4 { return "(___/ " }
 	}
 }
 
 function Big6() { param([Int]$Row)
 	switch($Row) {
-	1 { return "  _   " }
-	2 { return " / )  " }
-	3 { return "/ _ \ " }
-	4 { return "\___/ " }
+	1 { return "  _  " }
+	2 { return " / ) " }
+	3 { return "/ _ \" }
+	4 { return "\___/" }
 	}
 }
 
 function Big7() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ___  " }
-	2 { return "(__ ) " }
-	3 { return " / /  " }
-	4 { return "(_/   " }
+	1 { return " ___ " }
+	2 { return "(__ )" }
+	3 { return " / / " }
+	4 { return "(_/  " }
 	}
 } 
 
 function Big8() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ___  " }
-	2 { return "( _ ) " }
-	3 { return "/ _ \ " }
-	4 { return "\___/ " }
+	1 { return " ___ " }
+	2 { return "( _ )" }
+	3 { return "/ _ \" }
+	4 { return "\___/" }
 	}
 } 
 
 function Big9() { param([Int]$Row)
 	switch($Row) {
-	1 { return " ___  " }
-	2 { return "/ _ \ " }
-	3 { return "\_  / " }
-	4 { return " (_/  " }
+	1 { return " ___ " }
+	2 { return "/ _ \" }
+	3 { return "\_  /" }
+	4 { return " (_/ " }
 	}
 } 
 
@@ -372,7 +372,7 @@ function BigChar() { param([String]$Char, [Int]$Row)
 	'8' { return Big8 $Row }
 	'9' { return Big9 $Row }
 	}
-	return "        "
+	return "      "
 }
 
 try {
