6d0a7b8efa4185bb0add884d21162add2ef57cd6
Markus Fleschutz <markus@fleschutz.de>
2020-12-22 11:06:34 +0000

    Updated switch-shelly1.ps1

--- Scripts/switch-shelly1.ps1
+++ Scripts/switch-shelly1.ps1
@@ -1,29 +1,28 @@
 #!/snap/bin/powershell
 
-# Syntax:       ./switch-shelly1.ps1 [<IP addr>] [<turn mode>] [<timer>]
+# Syntax:       ./switch-shelly1.ps1 [<IP addr>] [<turn-mode>] [<timer>]
 # Description:	switches a Shelly1 device in the local network
 # Author:	Markus Fleschutz
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
 
-param([string]$IPaddr, [string]$TurnMode, [int]$Timer)
-if ($IPaddr -eq "" ) {
-	$IPaddr = read-host "Enter IP address of Shelly1 device"
-}
-if ($TurnMode -eq "" ) {
-	$TurnMode = read-host "Enter turn mode (on/off/toggle)"
-}
-if ($Timer -eq 0 ) {
-	[int]$Timer = read-host "Enter timer (0=endless)"
-}
+param([String]$IPaddr, [String]$TurnMode, [Int]$Timer)
 
 try {
-	$URL = "http://$($IPaddr)/relay/0?turn=$($TurnMode)&timer=$($Timer)"
-	$result = Invoke-RestMethod $URL
+	if ($IPaddr -eq "" ) {
+		[String]$IPaddr = read-host "Enter IP address of the Shelly1 device"
+	}
+	if ($TurnMode -eq "" ) {
+		[String]$TurnMode = read-host "Enter turn mode (on/off/toggle)"
+	}
+	if ($Timer -eq 0 ) {
+		[Int]$Timer = read-host "Enter timer (0=endless)"
+	}
 
-	write-host "OK - switched Shelly1 device at $IPaddr to $TurnMode for $Timer second(s)"
+	$Result = Invoke-RestMethod "http://$($IPaddr)/relay/0?turn=$($TurnMode)&timer=$($Timer)"
+	write-output "OK - switched Shelly1 device at $IPaddr to $TurnMode for $Timer second(s)"
 	exit 0
 } catch {
-	Write-Error "ERROR in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	write-error "ERROR in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
 	exit 1
 }
