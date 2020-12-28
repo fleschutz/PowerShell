ae47715591bbc3d63e0522cf035c1dcf2f9bf39f
Markus Fleschutz <markus@fleschutz.de>
2020-12-28 10:13:11 +0000

    Updated the close-* scripts

--- Scripts/close-windows-terminal.ps1
+++ Scripts/close-windows-terminal.ps1
@@ -6,6 +6,4 @@
 # Source:	github.com/fleschutz/PowerShell
 # License:	CC0
 
-$PathToRepo=(get-item $MyInvocation.MyCommand.Path).directory.parent
-
-."$PathToRepo/Scripts/close-program.ps1" "WindowsTerminal" 
+./close-program "WindowsTerminal" 
