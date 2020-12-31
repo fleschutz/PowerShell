49c71a22ae608f4ba4e59b7e8bc7a40d51e95245
Markus Fleschutz <markus@fleschutz.de>
2020-12-31 10:25:41 +0000

    Improved the script

--- Scripts/speak-test.ps1
+++ Scripts/speak-test.ps1
@@ -8,27 +8,38 @@
 
 try {
 	$Voice = new-object -ComObject SAPI.SPVoice
-	$Result = $Voice.Speak("This is the default voice")
+	$DefaultVolume = $Voice.volume
+	$DefaultRate = $Voice.rate
+	$Result = $Voice.Speak("This is the default voice with default volume $DefaultVolume and speed $DefaultRate")
 
-	$Voice.rate = 8
-	$Result = $Voice.Speak("Let's speak very, very fast")
 	$Voice.rate = -10
 	$Result = $Voice.Speak("Let's speak very, very slow")
-	$Voice.rate = 0
+	$Voice.rate = -5
+	$Result = $Voice.Speak("Let's speak very slow")
+	$Voice.rate = 2
+	$Result = $Voice.Speak("Let's speak fast")
+	$Voice.rate = 5
+	$Result = $Voice.Speak("Let's speak very fast")
+	$Voice.rate = 10
+	$Result = $Voice.Speak("Let's speak very, very fast")
+	$Voice.rate = $DefaultRate
 
-	$PrevVolume = $Voice.Volume
 	$Voice.volume = 100
-	$Result = $Voice.Speak("Let's speak with maximum volume")
+	$Result = $Voice.Speak("Let's try 100% volume")
+	$Voice.volume = 75
+	$Result = $Voice.Speak("Let's try 75% volume")
 	$Voice.volume = 50
-	$Result = $Voice.Speak("Let's speak with half volume")
-	$Voice.volume = $PrevVolume
+	$Result = $Voice.Speak("Let's try 50% volume")
+	$Voice.volume = 25
+	$Result = $Voice.Speak("Let's try 25% volume")
+	$Voice.volume = $DefaultVolume
 
 	$Voices = $Voice.GetVoices()
 	foreach ($OtherVoice in $Voices) {
 		$Description = $OtherVoice.GetDescription()
-		write-output "This is: $Description"
+		write-output "Voice: $Description"
 		$Voice.Voice = $OtherVoice
-		$Result = $Voice.Speak("1 2 3 - this is $Description")
+		$Result = $Voice.Speak("Hello, I'm the voice called $Description")
 	}
 	exit 0
 } catch {
