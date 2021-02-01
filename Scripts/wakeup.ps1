fbfd13d63619525e55b296986faaacf8b6bde850
Markus Fleschutz <markus@fleschutz.de>
2021-02-01 08:02:39 +0100

    Improved wakeup.ps1

--- Scripts/wakeup.ps1
+++ Scripts/wakeup.ps1
@@ -1,65 +1,35 @@
 #!/snap/bin/powershell
 <#
-.SYNTAX         ./wakeup.ps1 [<hostname>]
+.SYNTAX         ./wakeup.ps1 [<hostname>] [<MAC-address>]
 .DESCRIPTION	sends a magic packet to the given computer, waking him up
 .LINK		https://github.com/fleschutz/PowerShell
 .NOTES		Author:	Markus Fleschutz / License: CC0
 #>
 
-param([string]$Hostname = "")
-if ($Hostname -eq "" ) {
-	$Hostname = read-host "Enter hostname"
-}
+param([string]$Hostname = "", [string]MACaddress = "")
 
-function Send-WOL { 
-<#  
-  .SYNOPSIS   
-    Send a WOL packet to a broadcast address 
-  .PARAMETER mac 
-   The MAC address of the device that need to wake up 
-  .PARAMETER ip 
-   The IP address where the WOL packet will be sent to 
-  .EXAMPLE  
-   Send-WOL -mac 00:11:32:21:2D:11 -ip 192.168.8.255  
-  .EXAMPLE  
-   Send-WOL -mac 00:11:32:21:2D:11  
-#> 
- 
-[CmdletBinding()] 
-param( 
-[Parameter(Mandatory=$True,Position=1)] 
-[string]$mac, 
-[string]$ip="255.255.255.255",  
-[int]$port=9 
-) 
-$broadcast = [Net.IPAddress]::Parse($ip) 
+function Send-WOL { param([string]$mac, [string]$ip="255.255.255.255", [int]$port=9) 
+	$broadcast = [Net.IPAddress]::Parse($ip) 
   
-$mac=(($mac.replace(":","")).replace("-","")).replace(".","") 
-$target=0,2,4,6,8,10 | % {[convert]::ToByte($mac.substring($_,2),16)} 
-$packet = (,[byte]255 * 6) + ($target * 16) 
+	$mac=(($mac.replace(":","")).replace("-","")).replace(".","") 
+	$target=0,2,4,6,8,10 | % {[convert]::ToByte($mac.substring($_,2),16)} 
+	$packet = (,[byte]255 * 6) + ($target * 16) 
   
-$UDPclient = new-Object System.Net.Sockets.UdpClient 
-$UDPclient.Connect($broadcast,$port) 
-[void]$UDPclient.Send($packet, 102)  
+	$UDPclient = new-Object System.Net.Sockets.UdpClient 
+	$UDPclient.Connect($broadcast,$port) 
+	[void]$UDPclient.Send($packet, 102)  
 } 
 
-$MyMACAddresses = "io=11:22:33:44:55:66","pi=11:22:33:44:55:66"
-
 try {
-	foreach($item in $MyMACAddresses) {
-		$array = $item.Split("=")
-		$thisHost=$array[0]
-		$thisMAC=$array[1]
-		if ($thisHost -like $Hostname) {
-			Send-WOL $thisMAC
-			write-output "OK - host $thisHost waked up (MAC $thisMAC)"
-			exit 0
-		}
+	if ($Hostname -eq "" ) {
+		$Hostname = read-host "Enter hostname or IP address"
 	}
-
-	write-output "Sorry, hostname $Hostname is unknown."
-	pause
-	exit 1
+	if ($MACaddress -eq "" ) {
+		$MACaddress = read-host "Enter the MAC address"
+	}
+	Send-WOL $MACaddress $Hostname
+	write-output "OK - sent magic packet to $Hostname (MAC $MACaddress) to wake him up"
+	exit 0
 } catch {
 	write-error "ERROR in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
 	exit 1
