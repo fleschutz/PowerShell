b57c776d6450b1088b53e9b42403d43d8b81845c
Markus Fleschutz <markus@fleschutz.de>
2021-02-01 07:30:14 +0100

    Added list-environment-variables.ps1

---
+++ Scripts/list-environment-variables.ps1
@@ -0,0 +1,15 @@
+#!/snap/bin/powershell
+<#
+.SYNTAX         ./list-environment-variables.ps1
+.DESCRIPTION	lists all environment variables
+.LINK		https://github.com/fleschutz/PowerShell
+.NOTES		Author:	Markus Fleschutz / License: CC0
+#>
+
+try {
+	Get-ChildItem env:
+	exit 0
+} catch {
+	write-error "ERROR in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
