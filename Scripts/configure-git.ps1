5f4734149d40b1d2bc9de1bc26202ec4988f0acc
Markus Fleschutz <markus@fleschutz.de>
2021-02-02 10:06:16 +0100

    Improved configure-git.ps1

--- Scripts/configure-git.ps1
+++ Scripts/configure-git.ps1
@@ -8,15 +8,18 @@
 #>
 
 $FullName       = read-host "Enter your full name"
-$EmailAddress   = read-host "Enter your email address"
-$FavoriteEditor = read-host "Enter your favorite editor (emacs,nano,vi,vim,...)"
+$EmailAddress   = read-host "Enter your e-mail address"
+$FavoriteEditor = read-host "Enter your favorite text editor (emacs,nano,vi,vim,...)"
 
 try {
 	git config --global user.name $FullName
 	git config --global user.email $EmailAddress
 	git config --global core.editor $FavoriteEditor
-	git config --global init.defaultBranch main
-	write-output "OK - git configuration is now:"
+	git config --global http.sslVerify false
+	git config --global core.autocrlf false
+	git config --global core.symlinks true
+#	git config --global init.defaultBranch main
+	write-output "OK - your Git configuration is now:"
 	git config --list
 	exit 0
 } catch {
