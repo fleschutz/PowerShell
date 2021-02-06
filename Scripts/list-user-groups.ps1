fe16d787c5c2e5f3c804358e14d02b3abdd6b3dc
Markus Fleschutz <markus@fleschutz.de>
2021-02-06 13:50:21 +0100

    Improved list-user-groups.ps1

--- Scripts/list-user-groups.ps1
+++ Scripts/list-user-groups.ps1
@@ -1,14 +1,13 @@
 #!/snap/bin/powershell
 <#
 .SYNTAX         ./list-user-groups.ps1 
-.DESCRIPTION	lists all user groups
+.DESCRIPTION	lists the user groups on the local computer
 .LINK		https://github.com/fleschutz/PowerShell
 .NOTES		Author:	Markus Fleschutz / License: CC0
 #>
 
 try {
 	Get-LocalGroup
-	Get-LocalGroupMember -name users
 	exit 0
 } catch {
 	write-error "ERROR in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
