eb4197dd5955eb72b3e813a02eff85c5a8ed157f
Markus Fleschutz <markus@fleschutz.de>
2021-02-12 12:28:59 +0100

    Update switch-shelly1.ps1

--- Scripts/switch-shelly1.ps1
+++ Scripts/switch-shelly1.ps1
@@ -6,20 +6,21 @@
 .NOTES		Author:	Markus Fleschutz / License: CC0
 #>
 
-param([string]$IPaddr = "", [string]$TurnMode = "", [int]$Timer = -999)
+param($IPaddr = "", $TurnMode = "", [int]$Timer = -999)
 
 try {
 	if ($IPaddr -eq "") {
-		[String]$IPaddr = read-host "Enter IP address of the Shelly1 device"
+		$IPaddr = read-host "Enter IP address of the Shelly1 device"
 	}
 	if ($TurnMode -eq "") {
-		[String]$TurnMode = read-host "Enter turn mode (on/off/toggle)"
+		$TurnMode = read-host "Enter turn mode (on/off/toggle)"
 	}
 	if ($Timer -eq -999) {
-		[Int]$Timer = read-host "Enter timer in seconds (0=endless)"
+		[int]$Timer = read-host "Enter timer in seconds (0=endless)"
 	}
 
 	$Result = Invoke-RestMethod "http://$($IPaddr)/relay/0?turn=$($TurnMode)&timer=$($Timer)"
+	
 	write-host -foregroundColor green "Done - switched Shelly1 device at $IPaddr to $TurnMode for $Timer second(s)"
 	exit 0
 } catch {
