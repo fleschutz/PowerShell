76c16d7e9af51eb20d00c2d4aee2b834b8f00da2
Markus Fleschutz <markus@fleschutz.de>
2021-02-18 07:44:08 +0100

    Update switch-shelly1.ps1

--- Scripts/switch-shelly1.ps1
+++ Scripts/switch-shelly1.ps1
@@ -6,11 +6,11 @@
 .NOTES		Author:	Markus Fleschutz / License: CC0
 #>
 
-param($IPaddr = "", $TurnMode = "", [int]$Timer = -999)
+param($IPaddress = "", $TurnMode = "", [int]$Timer = -999)
 
 try {
-	if ($IPaddr -eq "") {
-		$IPaddr = read-host "Enter IP address of the Shelly1 device"
+	if ($IPaddress -eq "") {
+		$IPaddress = read-host "Enter IP address of the Shelly1 device"
 	}
 	if ($TurnMode -eq "") {
 		$TurnMode = read-host "Enter turn mode (on/off/toggle)"
@@ -19,9 +19,9 @@ try {
 		[int]$Timer = read-host "Enter timer in seconds (0=endless)"
 	}
 
-	$Result = Invoke-RestMethod "http://$($IPaddr)/relay/0?turn=$($TurnMode)&timer=$($Timer)"
+	$Result = Invoke-RestMethod "http://$($IPaddress)/relay/0?turn=$($TurnMode)&timer=$($Timer)"
 	
-	write-host -foregroundColor green "Done - switched Shelly1 device at $IPaddr to $TurnMode for $Timer second(s)"
+	write-host -foregroundColor green "Done - switched Shelly1 device at $IPaddress to $TurnMode for $Timer second(s)"
 	exit 0
 } catch {
 	write-error "ERROR: line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
