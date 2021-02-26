2ef09c2390f6431c992bd61f5b02c803f02df136
Markus Fleschutz <markus@fleschutz.de>
2021-02-26 19:58:47 +0100

    Improved check-windows-system-files.ps1

--- Scripts/check-windows-system-files.ps1
+++ Scripts/check-windows-system-files.ps1
@@ -1,14 +1,18 @@
 #!/bin/powershell
 <#
 .SYNTAX         ./check-windows-system-files.ps1 
-.DESCRIPTION	checks the validity of the Windows system files 
+.DESCRIPTION	checks the validity of the Windows system files (requires admin rights)
 .LINK		https://github.com/fleschutz/PowerShell
 .NOTES		Author:	Markus Fleschutz / License: CC0
 #>
 
+#Requires -RunAsAdministrator
+
 try {
 	sfc /verifyOnly
+	if ($lastExitCode -ne "0") { throw "'sfc /verifyOnly' failed" }
 
+	write-host -foregroundColor green "OK - Windows system files have been checked"
 	exit 0
 } catch {
 	write-error "ERROR: line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
