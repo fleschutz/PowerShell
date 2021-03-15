242da203458e46294c44e8c56dbd887adfec57ad
Markus Fleschutz <markus@fleschutz.de>
2021-03-15 14:00:11 +0100

    Update the description

--- Scripts/list-branches.ps1
+++ Scripts/list-branches.ps1
@@ -1,7 +1,7 @@
 #!/bin/powershell
 <#
 .SYNTAX         ./list-branches.ps1 [<repo-dir>] [<pattern>]
-.DESCRIPTION	lists all branches in the current/given Git repository 
+.DESCRIPTION	lists the Git branches in the current/given repository 
 .LINK		https://github.com/fleschutz/PowerShell
 .NOTES		Author:	Markus Fleschutz / License: CC0
 #>
