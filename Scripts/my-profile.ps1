5cb182921ce5a152ffb5e8be95b9dff6b5537763
Markus Fleschutz <markus@fleschutz.de>
2021-03-15 16:21:59 +0100

    Update my-profile.ps1

--- Scripts/my-profile.ps1
+++ Scripts/my-profile.ps1
@@ -3,10 +3,17 @@
 # 
 # NOTE: simply comment/uncomment/adapt the following lines:
 
-#function prompt {$null} # PS> 
+# function prompt {$null}
+# resulting prompt is: PS> 
 
-#function prompt { "$ " } # $
+# function prompt { "$ " }
+# resultinng prompt is: $
 
-function prompt { write-host -foregroundColor blue -noNewLine "$(Get-Location)"; return "> " } # C:\> 
+function prompt {
+	$host.ui.RawUI.WindowTitle = "$(whoami)@$(hostname)"
+	write-host -foregroundColor blue -noNewLine "$(Get-Location)"
+	return "> "
+}
+# resulting prompt is: C:\> 
 
 set-alias -name lsf -value get-childitem # lsf means list directory formatted
