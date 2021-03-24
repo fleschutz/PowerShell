f7baec471661de0f6b0a81aa9bc276f368d8b99f
Markus Fleschutz <markus@fleschutz.de>
2021-03-24 11:45:30 +0100

    Update clean-branch.ps1

--- Scripts/clean-branch.ps1
+++ Scripts/clean-branch.ps1
@@ -1,7 +1,7 @@
 #!/bin/powershell
 <#
 .SYNTAX       ./clean-branch.ps1 [<repo-dir>]
-.DESCRIPTION  cleans the current/given Git repository from generated files (including submodules, e.g. for a fresh build)
+.DESCRIPTION  cleans the current/given Git repository from untracked files (including submodules, e.g. for a fresh build)
 .LINK         https://github.com/fleschutz/PowerShell
 .NOTES        Author: Markus Fleschutz / License: CC0
 #>
@@ -9,17 +9,13 @@
 param($RepoDir = "$PWD")
 
 try {
-	& git --version
-} catch {
-	write-error "ERROR: can't execute 'git' - make sure Git is installed and available"
-	exit 1
-}
-
-try {
-	write-progress "Cleaning Git repository $RepoDir from generated files ..."
+	"Cleaning Git repository $RepoDir from untracked files ..."
 
 	if (-not(test-path "$RepoDir" -pathType container)) { throw "Can't access directory: $RepoDir" }
 	set-location "$RepoDir"
+	
+	& git --version
+	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 	
 	& git clean -fdx # force + recurse into dirs + don't use the standard ignore rules
 	if ($lastExitCode -ne "0") { throw "'git clean -fdx' failed" }
