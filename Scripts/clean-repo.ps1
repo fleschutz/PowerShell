20c1d4beec63e0ed494e76fab78fb16d8f10b429
Markus Fleschutz <markus@fleschutz.de>
2021-04-01 14:27:45 +0200

    Rename to clean-repo.ps1

--- Scripts/clean-repo.ps1
+++ Scripts/clean-repo.ps1
@@ -1,6 +1,6 @@
 #!/bin/powershell
 <#
-.SYNTAX       ./clean-branch.ps1 [<repo-dir>]
+.SYNTAX       ./clean-repo.ps1 [<repo-dir>]
 .DESCRIPTION  cleans the current/given Git repository from untracked files (including submodules, e.g. for a fresh build)
 .LINK         https://github.com/fleschutz/PowerShell
 .NOTES        Author: Markus Fleschutz / License: CC0
