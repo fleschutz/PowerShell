c65d4455e644eb916daa11198357ebdfb3344ac3
Markus Fleschutz <markus@fleschutz.de>
2021-04-07 14:12:57 +0200

    Add open-file-explorer.ps1

---
+++ Scripts/open-file-explorer.ps1
@@ -0,0 +1,15 @@
+#!/usr/bin/pwsh
+<#
+.SYNTAX       ./open-file-explorer.ps1
+.DESCRIPTION  starts the File Explorerer
+.LINK         https://github.com/fleschutz/PowerShell
+.NOTES        Author: Markus Fleschutz / License: CC0
+#>
+
+try {
+	start-process explorer.exe
+	exit 0
+} catch {
+	write-error "ERROR: line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
