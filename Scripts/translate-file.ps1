2ae727019b09fea0df5c190d7434794ad3354889
Markus Fleschutz <markus@fleschutz.de>
2021-04-08 11:19:53 +0200

    Improve translate-file.ps1 and translate-text.ps1

--- Scripts/translate-file.ps1
+++ Scripts/translate-file.ps1
@@ -12,13 +12,24 @@ if ($File -eq "" ) { $File = read-host "Enter path to file" }
 if ($SourceLang -eq "" ) { $SourceLang = read-host "Enter language used in this file" }
 if ($TargetLang -eq "" ) { $TargetLang = read-host "Enter language to translate to" }
 
+function UseLibreTranslate { param([string]$Text, [string]$SourceLang, [string]$TargetLang)
+	$Parameters = @{"q"="$Text"; "source"="$SourceLang"; "target"="$TargetLang"; }
+	$Result = (Invoke-WebRequest -Uri https://libretranslate.com/translate -Method POST -Body ($Parameters|ConvertTo-Json) -ContentType "application/json").content | ConvertFrom-Json
+	start-sleep -milliseconds 3000 # 20 requests per minute maximum
+	return $Result.translatedText
+}
+
 try {
-	$PathToRepo = "$PSScriptRoot/.."
-
-	$Engine = "google" # aspell | google | bing | spell | hunspell | apertium | yandex
-
-	start-process -filePath "$PathToRepo/Data/trans" -argumentList "-i $File -s $SourceLang -t $TargetLang -e $Engine -brief" -noNewWindow -wait
-
+	$Lines = Get-Content -path $File
+	foreach($Line in $Lines) {
+		if ("$Line" -eq "") { write-output "$Line"; continue }
+		if ("$Line" -eq " ") { write-output "$Line"; continue }
+		if ("$Line" -like "===*") { write-output "$Line"; continue }
+		if ("$Line" -like "---*") { write-output "$Line"; continue }
+		if ("$Line" -like "!*(/*)") { write-output "$Line"; continue }
+		$Result = UseLibreTranslate $Line $SourceLang $TargetLang
+		write-output $Result
+	}
 	exit 0
 } catch {
 	write-error "ERROR: line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
--- Scripts/translate-text.ps1
+++ Scripts/translate-text.ps1
@@ -9,19 +9,48 @@
 param($Text = "", $SourceLang = "en")
 
 if ($Text -eq "" ) { $Text = read-host "Enter text in English to translate" }
-$TargetLanguages = "af","da","de","el","es","fr","hr","it","ja","ko","pl","pt","nl","ru","tr","uk","vi"
+$TargetLanguages = "Arabic","Chinese","French","German","Hindi","Irish","Italian","Japanese","Korean","Portuguese","Russian","Spanish"
 
-function TranslateUsingGoogle { param([string]$Text, [string]$SourceLang, [string]$TargetLang)
+function Language2Code { param([string]$Language)
+	$Code = switch($Language) {
+	"Arabic"  {"ar"}
+	"Chinese" {"zh"}
+	"French"  {"fr"}
+	"German"  {"de"}
+	"Hindi"   {"hi"}
+	"Irish"   {"ga"}
+	"Italian" {"it"}
+	"Japanese"{"ja"}
+	"Korean"  {"ko"}
+	"Portuguese"{"pt"}
+	"Russian" {"ru"}
+	"Spanish" {"es"}
+	Default   {$Language}
+	}
+	return $Code
+}
 
-	$URL = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=$($SourceLang)&tl=$($TargetLang)&dt=t&q=$($Text)"
-	$result = Invoke-RestMethod $URL
-	return $result[0][0][0]
+function UseGoogleTranslate { param([string]$Text, [string]$SourceLang, [string]$TargetLang)
+	$Result = Invoke-RestMethod "https://translate.googleapis.com/translate_a/single?client=gtx&sl=$($SourceLang)&tl=$($TargetLang)&dt=t&q=$($Text)"
+	return $Result[0][0][0]
+}
+
+function UseLibreTranslate { param([string]$Text, [string]$SourceLang, [string]$TargetLang)
+	$Parameters = @{"q"="$Text"; "source"="$SourceLang"; "target"="$TargetLang"; }
+	$Result = (Invoke-WebRequest -Uri https://libretranslate.com/translate -Method POST -Body ($Parameters|ConvertTo-Json) -ContentType "application/json").content | ConvertFrom-Json
+	return $Result.translatedText
+}
+
+function UseArgosTranslateCLI { param([string]$Text, [string]$SourceLang, [string]$TargetLang)
+	$Result = (argos-translate.cli --from-lang $SourceLang --to-lang $TargetLang $Text)
+	return $Result
 }
 
 try {
-	foreach($TargetLang in $TargetLanguages) {
-		$Result = TranslateUsingGoogle $Text $SourceLang $TargetLang
-		write-output "$TargetLang : $Result"
+	foreach($TargetLanguage in $TargetLanguages) {
+		$TargetLangCode = Language2Code $TargetLanguage
+		$Result = UseLibreTranslate $Text $SourceLang $TargetLangCode
+		write-output "$TargetLanguage : $Result"
 	}
 	exit 0
 } catch {
