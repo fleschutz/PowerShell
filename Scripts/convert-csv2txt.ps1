38f7ddb157b8d44381f38ac5daa076e92d6e9378
Markus Fleschutz <markus@fleschutz.de>
2021-04-10 10:17:09 +0200

    Rename to convert-csv2txt.ps1

--- Scripts/convert-csv2txt.ps1
+++ Scripts/convert-csv2txt.ps1
@@ -1,23 +1,19 @@
 #!/usr/bin/pwsh
 <#
-.SYNTAX       csv-to-text.ps1 [<csv-file>]
+.SYNTAX       convert-csv2txt.ps1 [<csv-file>]
 .DESCRIPTION  converts the given CSV file into a text list
 .LINK         https://github.com/fleschutz/PowerShell
 .NOTES        Author: Markus Fleschutz / License: CC0
 #>
 
-param([String]$Path)
-
-if ($Path -eq "" ) {
-	$Path = read-host "Enter path to CSV file"
-}
+param($Path = "")
+if ($Path -eq "" ) { $Path = read-host "Enter path to CSV file" }
 
 try {
 	$Table = Import-CSV -path "$Path" -header A,B,C,D,E,F,G,H
 
 	foreach($Row in $Table) {
 		write-output "* $($Row.A) $($Row.B) $($Row.C) $($Row.D) $($Row.E) $($Row.F) $($Row.G) $($Row.H)"
-#		write-output "* [$($Row.B)](ipfs::$($Row.A))"
 	}
 	exit 0
 } catch {
