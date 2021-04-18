fbad5259f79f3163e1b0da6db3aa0c103569a0ec
Markus Fleschutz <markus@fleschutz.de>
2021-04-18 11:01:38 +0200

    Add sync-repo.ps1

---
+++ Scripts/sync-repo.ps1
@@ -0,0 +1,31 @@
+﻿#!/usr/bin/pwsh
+<#
+.SYNTAX       sync-repo.ps1 [<repo-dir>]
+.DESCRIPTION  synchronizes a Git repository (pull&push, including submodules)
+.LINK         https://github.com/fleschutz/PowerShell
+.NOTES        Author: Markus Fleschutz / License: CC0
+#>
+
+param($RepoDir = "$PWD")
+
+try {
+	"⏳ Synchronizing Git repository $($RepoDir)..."
+
+	if (-not(test-path "$RepoDir" -pathType container)) { throw "Can't access directory: $RepoDir" }
+	set-location "$RepoDir"
+
+	$Null = (git --version)
+	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
+
+	& git pull --all --recurse-submodules --jobs=4
+	if ($lastExitCode -ne "0") { throw "'git pull' failed" }
+
+	& git push
+	if ($lastExitCode -ne "0") { throw "'git push' failed" }
+
+	"✔️ Git repository $RepoDir synchronized"
+	exit 0
+} catch {
+	write-error "ERROR: line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
