b078b794e3dc36443c45953f48a22039d3ef6a43
Markus Fleschutz <markus@fleschutz.de>
2021-04-18 18:12:19 +0200

    Improve list-profiles.ps1

--- Scripts/list-profiles.ps1
+++ Scripts/list-profiles.ps1
@@ -1,4 +1,4 @@
-#!/usr/bin/pwsh
+﻿#!/usr/bin/pwsh
 <#
 .SYNTAX       list-profiles.ps1
 .DESCRIPTION  lists your PowerShell profiles
@@ -6,25 +6,23 @@
 .NOTES        Author: Markus Fleschutz / License: CC0
 #>
 
-function ShowProfile { param([int]$Level, [string]$Name, [string]$Filename)
-	write-output "Level: $Level"
-	write-output "Name:  $Name"
+function ShowProfile { param([string]$Level, [string]$Name, [string]$Filename)
+	"$Level Profile '$Name'"
 	if (test-path "$Filename") {
-		write-output "File:  $Filename"
+		"  at $Filename"
 	} else {
-		write-output "File:  $Filename (file missing)"
+		"  at $Filename (file missing)"
 	}
-	write-output ""
 }
 
 try {
-	write-output ""
-	write-output "PowerShell Profiles"
-	write-output "==================="
-	ShowProfile 1 "AllUsersAllHosts"       $PROFILE.AllUsersAllHosts
-	ShowProfile 2 "AllUsersCurrentHost"    $PROFILE.AllUsersCurrentHost
-	ShowProfile 3 "CurrentUserAllHosts"    $PROFILE.CurrentUserAllHosts
-	ShowProfile 4 "CurrentUserCurrentHost" $PROFILE.CurrentUserCurrentHost
+	ShowProfile "1" "AllUsersAllHosts"       $PROFILE.AllUsersAllHosts
+	""
+	ShowProfile "2" "AllUsersCurrentHost"    $PROFILE.AllUsersCurrentHost
+	""
+	ShowProfile "3" "CurrentUserAllHosts"    $PROFILE.CurrentUserAllHosts
+	""
+	ShowProfile "4" "CurrentUserCurrentHost" $PROFILE.CurrentUserCurrentHost
 	exit 0
 } catch {
 	write-error "ERROR: line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
