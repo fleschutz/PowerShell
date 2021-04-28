478d62aa896f54522db3d83639ea5a75c3e4c4da
Markus Fleschutz <markus@fleschutz.de>
2021-04-28 13:01:05 +0200

    Add list-latest-tag.ps1

--- README.md
+++ README.md
@@ -146,6 +146,7 @@ Mega Collection of PowerShell Scripts
 * [fetch-repos.ps1](Scripts/fetch-repos.ps1) - fetches updates for all Git repositories under the current/given directory (including submodules)
 * [list-branches.ps1](Scripts/list-branches.ps1) - lists all branches in the current/given Git repository 
 * [list-commits.ps1](Scripts/list-commits.ps1) - lists all commits in the current/given Git repository 
+* [list-latest-tag.ps1](Scripts/list-latest-tag.ps1) - lists the latest tag on the current branch in a Git repository
 * [list-tags.ps1](Scripts/list-tags.ps1) - lists all tags in the current/given Git repository 
 * [pull-repo.ps1](Scripts/pull-repo.ps1) - pulls updates for the current/given Git repository (including submodules)
 * [pull-repos.ps1](Scripts/pull-repos.ps1) - pulls updates for all Git repositories under the current/given directory (including submodules)
---
+++ Scripts/list-latest-tag.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNTAX       list-latest-tag.ps1 [<repo-dir>] 
+.DESCRIPTION  lists the latest tag on the current branch in a Git repository
+.LINK         https://github.com/fleschutz/PowerShell
+.NOTES        Author: Markus Fleschutz / License: CC0
+#>
+
+param($RepoDir = "$PWD")
+
+try {
+	if (-not(test-path "$RepoDir" -pathType container)) { throw "Can't access directory: $RepoDir" }
+	set-location "$RepoDir"
+
+	$RepoDirName = (get-item "$RepoDir").Name
+	"⏳ Fetching updates for Git repository 📂$RepoDirName ..."
+
+	$Null = (git --version)
+	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
+
+	& git fetch --all --recurse-submodules --jobs=4
+	if ($lastExitCode -ne "0") { throw "'git fetch' failed" }
+
+	& git describe --tags --abbrev=0
+	exit 0
+} catch {
+	write-error "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
