6230f7ccc29905ea48c9d2509e6bb37a4a8e8a40
Markus Fleschutz <markus@fleschutz.de>
2021-05-13 10:14:01 +0200

    Improve list-earthquakes.ps1

--- Scripts/list-earthquakes.ps1
+++ Scripts/list-earthquakes.ps1
@@ -6,12 +6,19 @@
 #>
 
 $Format="csv" # csv, geojson, kml, text, xml
-$MinMagnitude=6.0
-$OrderBy="time" # time, time-asc, magnitude, magnitude-asc
+$MinMagnitude=5.8
+$OrderBy="magnitude" # time, time-asc, magnitude, magnitude-asc
+
+function ListEarthquakes { 
+	write-progress "Loading data ..."
+	$Earthquakes = (Invoke-WebRequest -Uri "https://earthquake.usgs.gov/fdsnws/event/1/query?format=$Format&minmagnitude=$MinMagnitude&orderby=$OrderBy" -UserAgent "curl" ).Content | ConvertFrom-CSV
+	foreach ($Quake in $Earthquakes) {
+		new-object PSObject -Property @{ Magnitude=$Quake.mag; Depth=$Quake.depth; Place=$Quake.place; Time=$Quake.time }
+	}
+}
  
 try {
-	write-progress "Querying earthquakes for the last 30 days ..."
-	(Invoke-WebRequest -Uri "https://earthquake.usgs.gov/fdsnws/event/1/query?format=$Format&minmagnitude=$MinMagnitude&orderby=$OrderBy" -UserAgent "curl" ).Content
+	ListEarthquakes | format-table -autoSize -property Magnitude,Depth,Place,Time
 	exit 0
 } catch {
 	write-error "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
