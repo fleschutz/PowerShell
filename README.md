476108bcbc1ff6d560069b8116b4ea0c5e0a0da1
Markus Fleschutz <markus@fleschutz.de>
2021-07-16 16:49:42 +0200

    Add start-calibre-server.ps1

--- README.md
+++ README.md
@@ -221,6 +221,7 @@ Mega Collection of PowerShell Scripts
 * [set-timer.ps1](Scripts/set-timer.ps1) - sets a timer for a countdown
 * [simulate-matrix.ps1](Scripts/simulate-matrix.ps1) - simulates the Matrix (fun)
 * [simulate-presence.ps1](Scripts/simulate-presence.ps1) - simulates the human presence against burglars
+* [start-calibre-server.ps1](Scripts/start-calibre-server.ps1) - starts a local Calibre server 
 * [start-ipfs-server.ps1](Scripts/start-ipfs-server.ps1) - starts a local IPFS server
 * [switch-shelly1.ps1](Scripts/switch-shelly1.ps1) - switches a Shelly1 device in the local network
 * [translate-file.ps1](Scripts/translate-file.ps1) - translates the given text file into other languages
---
+++ Scripts/start-calibre-server.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+	start-calibre-server.ps1
+.DESCRIPTION
+	Starts a local Calibre server as a daemon process
+.EXAMPLE
+	PS> .\start-calibre-server.ps1
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+#>
+
+try {
+	$StopWatch = [system.diagnostics.stopwatch]::startNew()
+
+	$Result = (calibre-server --version)
+	if ($lastExitCode -ne "0") { throw "Can't execute 'calibre-server' - make sure Calibre is installed and available" }
+
+	& calibre-server --port 8099 --num-per-page 100 --userdb $HOME/CalibreUsers.sqlite --log $HOME/CalibreServer.log --daemonize $HOME/'Calibre Library'
+
+	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
+	"✔️ started Calibre Server in $Elapsed sec"
+	exit 0
+} catch {
+	write-error "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
