d460bd3f312234a23db2e89f932b721e8f46dd3d
Markus Fleschutz <markus@fleschutz.de>
2021-08-17 20:14:27 +0200

    Add close-netflix.ps1

---
+++ Scripts/close-netflix.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+	close-netflix.ps1 
+.DESCRIPTION
+	Closes Netflix gracefully
+.EXAMPLE
+	PS> .\close-netflix.ps1
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author:  Markus Fleschutz
+	License: CC0
+#>
+
+& "$PSScriptRoot/close-program.ps1" "Netflix" "netflix" "netflix"
+exit 0
