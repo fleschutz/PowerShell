5b6fd65397f0fd2ce8011170310d46a32158caca
Markus Fleschutz <markus@fleschutz.de>
2021-09-14 19:47:42 +0200

    Update install-ssh-server.ps1

--- Scripts/install-ssh-server.ps1
+++ Scripts/install-ssh-server.ps1
@@ -19,13 +19,25 @@ try {
 	if ($IsLinux) {
 		apt install openssh-server
 	} else {
-		Add-WindowsCapability -Online -Name OpenSSH.Server*
+		# Install the OpenSSH Server
+		Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+		# Start the sshd service
 		Start-Service sshd
+
+		# OPTIONAL but recommended:
 		Set-Service -Name sshd -StartupType 'Automatic'
+
+		# Confirm the firewall rule is configured. It should be created automatically by setup.
+		Get-NetFirewallRule -Name *ssh*
+
+		# There should be a firewall rule named "OpenSSH-Server-In-TCP", which should be enabled
+		# If the firewall does not exist, create one
+		New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 	}
 
 	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ installed SSH server in $Elapsed sec"
+	"✔️ installed and started SSH server in $Elapsed sec"
 	exit 0
 } catch {
 	write-error "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
