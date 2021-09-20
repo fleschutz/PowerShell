c8820b0e9eecd00790af8883b23af61a23afcbf1
Markus Fleschutz <markus@fleschutz.de>
2021-09-20 12:29:49 +0200

    Update start-calibre-server.ps1

--- Scripts/start-calibre-server.ps1
+++ Scripts/start-calibre-server.ps1
@@ -1,8 +1,8 @@
 ﻿<#
 .SYNOPSIS
-	start-calibre-server.ps1
+	start-calibre-server.ps1 [<port>]
 .DESCRIPTION
-	Starts a local Calibre server as a daemon process.
+	Starts a local Calibre server as background process (Web port number is 8099 by default).
 .EXAMPLE
 	PS> .\start-calibre-server.ps1
 .NOTES
@@ -11,16 +11,20 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
+param([int]port = 8099)
+
 try {
 	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	$Result = (calibre-server --version)
+	"Step 1/2: Searching for Calibre Server executable..."
+	& calibre-server --version
 	if ($lastExitCode -ne "0") { throw "Can't execute 'calibre-server' - make sure Calibre is installed and available" }
 
-	& calibre-server --port 8099 --num-per-page 100 --userdb $HOME/CalibreUsers.sqlite --log $HOME/CalibreServer.log --daemonize $HOME/'Calibre Library'
+	"Step 2/2: Starting Calibre Server as background process..."
+	& calibre-server --port $port --num-per-page 100 --userdb $HOME/CalibreUsers.sqlite --log $HOME/CalibreServer.log --daemonize $HOME/'Calibre Library'
 
 	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ started Calibre Server in $Elapsed sec"
+	"✔️ started Calibre Server with port $port in $Elapsed sec"
 	exit 0
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
