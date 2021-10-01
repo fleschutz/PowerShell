ff545d827e8ea7a1fcd3dc4dd462fe1a55df7e32
Markus Fleschutz <markus@fleschutz.de>
2021-10-01 12:38:37 +0200

    Update list-profiles.ps1

--- Scripts/list-profiles.ps1
+++ Scripts/list-profiles.ps1
@@ -2,9 +2,16 @@
 .SYNOPSIS
 	list-profiles.ps1
 .DESCRIPTION
-	Lists your PowerShell profiles
+	Lists your PowerShell profiles.
 .EXAMPLE
 	PS> ./list-profiles
+	
+	Level Profile                Location                                                         Existent
+	----- -------                --------                                                         --------
+	1     AllUsersAllHosts       /opt/PowerShell/profile.ps1                                      no
+	2     AllUsersCurrentHost    /opt/PowerShell/Microsoft.PowerShell_profile.ps1                 no
+	3     CurrentUserAllHosts    /home/markus/.config/powershell/profile.ps1                      no
+	4     CurrentUserCurrentHost /home/markus/.config/powershell/Microsoft.PowerShell_profile.ps1 yes
 .NOTES
 	Author: Markus Fleschutz · License: CC0
 .LINK
@@ -13,12 +20,7 @@
 
 function ListProfile { param([int]$Level, [string]$Profile, [string]$Location)
 	if (test-path "$Location") { $Existent = "yes" } else { $Existent = "no" }
-	new-object PSObject -Property @{
-		'Level' = "$Level"
-		'Profile' = "$Profile"
-		'Location' = "$Location"
-		'Existent' = "$Existent"
-	}
+	New-Object PSObject -Property @{ 'Level'="$Level"; 'Profile'="$Profile"; 'Location'="$Location"; 'Existent'="$Existent"	}
 }
 
 function ListProfiles { 
@@ -27,7 +29,6 @@ function ListProfiles {
 	ListProfile 3 "CurrentUserAllHosts"    $PROFILE.CurrentUserAllHosts
 	ListProfile 4 "CurrentUserCurrentHost" $PROFILE.CurrentUserCurrentHost
 }
-
 
 try {
 	ListProfiles | format-table -property Level,Profile,Location,Existent
