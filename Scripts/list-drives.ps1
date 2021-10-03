8b6993561ae1209977eb65725738784bafaec851
Markus Fleschutz <markus@fleschutz.de>
2021-10-03 20:56:22 +0200

    Update list-drives.ps1

--- Scripts/list-drives.ps1
+++ Scripts/list-drives.ps1
@@ -2,14 +2,14 @@
 .SYNOPSIS
 	list-drives.ps1
 .DESCRIPTION
-	Lists all drives connected to the computer
+	Lists all local drives.
 .EXAMPLE
 	PS> ./list-drives
 
 	Name Root Used (GB) Free (GB)
 	---- ---- --------- ---------
-	C    C:\      76,14     35,05
-	D    D:\    6648,12    744,16
+	C    C:\       76,1      35,0
+	D    D:\     6648,1     744,2
 .NOTES
 	Author: Markus Fleschutz · License: CC0
 .LINK
@@ -17,7 +17,7 @@
 #>
 
 try {
-	Get-PSDrive -PSProvider FileSystem | format-table -property Name,Root,@{n="Used (GB)";e={[math]::Round($_.Used/1GB,2)}},@{n="Free (GB)";e={[math]::Round($_.Free/1GB,2)}}
+	Get-PSDrive -PSProvider FileSystem | format-table -property Name,Root,@{n="Used (GB)";e={[math]::Round($_.Used/1GB,1)}},@{n="Free (GB)";e={[math]::Round($_.Free/1GB,1)}}
 	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
