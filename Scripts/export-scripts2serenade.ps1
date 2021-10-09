c7c82645513a1e49f347d8d50c5693424a784ebb
Markus Fleschutz <markus@fleschutz.de>
2021-10-09 09:34:26 +0200

    Add export-scripts2manuals.ps1 and export-scripts2serenade.ps1

---
+++ Scripts/export-scripts2serenade.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	← enter overview of script here
+.DESCRIPTION
+	← enter brief description of script here
+.INPUTS
+	← enter inputs here (if any, otherwise state None)
+.OUTPUTS
+	← enter outputs here (if any, otherwise state None)
+.EXAMPLE
+	PS> .\template.ps1 ← enter example here (repeat this attribute for more than one example)
+.NOTES
+	Author:        ← enter full name here
+	License:       ← enter license here
+.LINK
+	← enter URL here
+#>
+
+#requires -version 4
+
+param() # ← enter script parameters here
+
+#	← enter functions here 
+
+try {
+#	← enter instructions here 
+
+	"✔️ Done."
+	exit 0
+} catch {
+	write-error "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
