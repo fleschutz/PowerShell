848c7b13a92301b02985fbee84c6d02b6f1e57c4
Markus Fleschutz <markus@fleschutz.de>
2021-10-13 13:32:10 +0200

    Add thank-you.ps1

---
+++ Scripts/thank-you.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Say an answer to 'thank you'
+.DESCRIPTION
+	This script answers to "thank you" by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./thank-you
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	& "$PSScriptRoot/speak-english.ps1" "It's a pleasure."
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
