86f936df085f6cfc58932353e53989c089409714
Markus Fleschutz <markus@fleschutz.de>
2021-10-23 11:02:20 +0200

    Fix typo

--- Scripts/open-downloads-folder.ps1
+++ Scripts/open-downloads-folder.ps1
@@ -16,7 +16,7 @@ try {
 	if (-not(test-path "$TargetDir" -pathType container)) {
 		throw "Downloads folder at 📂$TargetDir doesn't exist (yet)"
 	}
-	"$PSScriptRoot/open-file-explorer.ps1" "$TargetDir"
+	& "$PSScriptRoot/open-file-explorer.ps1" "$TargetDir"
 	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
