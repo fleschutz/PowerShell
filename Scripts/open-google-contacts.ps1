312b38a04a35b1406b7937cb1380661e8a1066b0
Markus Fleschutz <markus@fleschutz.de>
2021-10-25 11:18:43 +0200

    Add open-google-contacts.ps1 and open-google-news.ps1

---
+++ Scripts/open-google-contacts.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Opens Google Contacts
+.DESCRIPTION
+	This script starts the Web browser and opens Google Contacts.
+.EXAMPLE
+	PS> ./open-google-contacts
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	& "$PSScriptRoot/open-browser.ps1" "https://contacts.google.com"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
---
+++ Scripts/open-google-news.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Opens Google News
+.DESCRIPTION
+	This script starts the Web browser and opens Google News.
+.EXAMPLE
+	PS> ./open-google-news
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	& "$PSScriptRoot/open-browser.ps1" "https://news.google.com"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
