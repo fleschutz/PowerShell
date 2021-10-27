b9c7e36ff7844564152c78b0a8756655dc090106
Markus Fleschutz <markus@fleschutz.de>
2021-10-27 07:24:10 +0200

    Update open-c-drive.ps1

--- Scripts/open-c-drive.ps1
+++ Scripts/open-c-drive.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Opens the C: drive folder
 .DESCRIPTION
-	This script starts the File Explorer and shows the C: drive folder.
+	This script starts the File Explorer showing the C: drive folder.
 .EXAMPLE
 	PS> ./open-c-drive
 .NOTES
