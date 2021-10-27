edec1485f396ddf7a9ea4bd0dcdea461c56852bd
Markus Fleschutz <markus@fleschutz.de>
2021-10-27 08:32:12 +0200

    Update open-amazon.ps1

--- Scripts/open-amazon.ps1
+++ Scripts/open-amazon.ps1
@@ -11,10 +11,4 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
-try {
-	& "$PSScriptRoot/open-browser.ps1" "https://www.amazon.com"
-	exit 0 # success
-} catch {
-	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
-	exit 1
-}
+& "$PSScriptRoot/open-browser.ps1" "https://www.amazon.com"
