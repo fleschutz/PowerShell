a5d61a40b0d9a6e1196dd2a307136d4a2066fcd4
Markus Fleschutz <markus@fleschutz.de>
2021-10-29 12:56:26 +0200

    Add open-google-chrome.ps1

---
+++ Scripts/open-google-chrome.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Launches the Google Chrome Web browser
+.DESCRIPTION
+	This script launches the Google Chrome Web browser.
+.EXAMPLE
+	PS> ./open-google-chrome
+.PARAMETER URL
+	Specifies an optional URL
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+param([string]$URL = "")
+
+try {
+	if ("$URL" -ne "") {
+		start-process chrome.exe "$URL"
+	} else {
+		start-process chrome.exe
+	}
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
