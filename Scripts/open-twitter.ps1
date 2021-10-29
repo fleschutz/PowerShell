ec40505b19e6d01d5a90343d3a67af0b6258c4c4
Markus Fleschutz <markus@fleschutz.de>
2021-10-29 13:04:18 +0200

    Add open-twitter.ps1

---
+++ Scripts/open-twitter.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Twitter website
+.DESCRIPTION
+	This script launches the Web browser with the Twitter website.
+.EXAMPLE
+	PS> ./open-twitter
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.twitter.com"
+exit 0 # success
