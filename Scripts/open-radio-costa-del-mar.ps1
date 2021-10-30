736bfc276a215df035f9a8f0130f710a4c705ba1
Markus Fleschutz <markus@fleschutz.de>
2021-10-30 15:46:09 +0200

    Add 2 scripts

---
+++ Scripts/open-radio-costa-del-mar.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the radio station Costa Del Mar
+.DESCRIPTION
+	This script launches the Web browser with the radio station Costa Del Mar
+.EXAMPLE
+	PS> ./open-radio-costa-del-mar
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.radio.de/s/costadelmar"
+exit 0 # success
