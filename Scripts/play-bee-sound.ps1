7956943d824b0682eb480a134313c2dbcfd9c80c
Markus <markus@fleschutz.de>
2021-11-01 17:43:45 +0100

    Add play-bee-sound.ps1

---
+++ Scripts/play-bee-sound.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.SYNOPSIS
+	Plays a bee sound
+.DESCRIPTION
+	This script plays a bee sound.
+.EXAMPLE
+	PS> ./open-bee-sound
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	& "$PSScriptRoot/play-mp3.ps1" "$PSScriptRoot/Data/Sounds/bee.mp3"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+}
