535986d50fb089e91aac4577a1c1d6e35578178d
Markus Fleschutz <markus@fleschutz.de>
2021-11-02 11:20:25 +0100

    Add open-booking-com.ps1

---
+++ Scripts/open-booking-com.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Booking.com website
+.DESCRIPTION
+	This script launches the Web browser with the Booking.com website.
+.EXAMPLE
+	PS> ./open-booking-com
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.booking.com"
+exit 0 # success
