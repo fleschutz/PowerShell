8ddf6f0e37adaba1476a4404e964199c1fb8201e
Markus Fleschutz <markus@fleschutz.de>
2021-11-03 17:00:49 +0100

    Add 2 scripts

---
+++ Scripts/open-egg-timer.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens an egg timer
+.DESCRIPTION
+	This script launches the Web browser with the eggtimer website.
+.EXAMPLE
+	PS> ./open-egg-timer
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://e.ggtimer.com"
+exit 0 # success
---
+++ Scripts/open-jitsi-meet.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Jitsi Meet website
+.DESCRIPTION
+	This script launches the Web browser with the Jitsi Meet website.
+.EXAMPLE
+	PS> ./open-jitsi-meet
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://meet.jit.si/"
+exit 0 # success
