eda717a62663065568b506097e2065a03351075e
Markus Fleschutz <markus@fleschutz.de>
2021-11-03 19:51:32 +0100

    Add show-new-york-city.ps1

---
+++ Scripts/show-new-york-city.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Show NYC in Google Maps 
+.DESCRIPTION
+	This script launches the Web browser with Google Maps at New York City.
+.EXAMPLE
+	PS> ./show-new-york-city
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.google.com/maps/place/New+York+City"
+exit 0 # success
