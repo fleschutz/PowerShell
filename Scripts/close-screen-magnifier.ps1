98ac588a2311b16bdc0ae29c6851d13982a9d6d9
Markus Fleschutz <markus@fleschutz.de>
2021-11-04 08:43:36 +0100

    Add open-screen-magnifier.ps1 and close-screen-magnifier.ps1

---
+++ Scripts/close-screen-magnifier.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Closes the Screen Magnifier
+.DESCRIPTION
+	This script closes the Windows Screen Magnifier application gracefully.
+.EXAMPLE
+	PS> ./close-screen-magnifier
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/close-program.ps1" "Screen Magnifier" "magnify.exe" ""
+exit 0 # success
---
+++ Scripts/open-screen-magnifier.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Launch the Screen Magnifier
+.DESCRIPTION
+	This script launches the Windows Screen Magnifier application.
+.EXAMPLE
+	PS> ./open-screen-magnifier
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	start-process magnify.exe
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
