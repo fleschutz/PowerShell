5fd52e0648dcb7ab3bfc4b31ed172d0e3b381ee4
Markus Fleschutz <markus@fleschutz.de>
2021-11-04 15:07:07 +0100

    Add find-android-phone.ps1

---
+++ Scripts/find-android-phone.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens Google Find My Device 
+.DESCRIPTION
+	This script launches the Web browser with the Google Find My Device website.
+.EXAMPLE
+	PS> ./open-android-phone
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.google.com/android/find"
+exit 0 # success
