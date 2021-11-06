2a3b93c792db2766c58bd015e7454d2c278aab8f
Markus Fleschutz <markus@fleschutz.de>
2021-11-06 11:14:15 +0100

    Add open-e-drive.ps1

---
+++ Scripts/open-e-drive.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.SYNOPSIS
+	Opens the E: drive folder
+.DESCRIPTION
+	This script launches the File Explorer with the E: drive folder.
+.EXAMPLE
+	PS> ./open-e-drive
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-file-explorer.ps1" "E:"
