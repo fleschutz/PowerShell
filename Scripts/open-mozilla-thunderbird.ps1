e9abedaf1e9af96a931aa4bc655bef8a91be06ab
Markus Fleschutz <markus@fleschutz.de>
2021-11-06 11:52:58 +0100

    Add open-mozilla-thunderbird.ps1

---
+++ Scripts/open-mozilla-thunderbird.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Launches Mozilla Thunderbird
+.DESCRIPTION
+	This script launches the Mozilla Thunderbird email client.
+.EXAMPLE
+	PS> ./open-mozilla-thunderbird
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+Start-Process "C:\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe"
+exit 0 # success
