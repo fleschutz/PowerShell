071ad49d5c8bf00cdb5f58b993f6a300ed06bd01
Markus Fleschutz <markus@fleschutz.de>
2021-11-06 11:28:39 +0100

    Add show-lightnings.ps1

---
+++ Scripts/show-lightnings.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows Lightnings in OpenStreetMap
+.DESCRIPTION
+	This script launches the Web browser and shows Lightnings in OpenStreetMap.
+.EXAMPLE
+	PS> ./show-lightnings
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.lightningmaps.org"
+exit 0 # success
