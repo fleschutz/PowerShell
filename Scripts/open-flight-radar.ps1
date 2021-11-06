c2c5501f06604648f7be54a284a7f7462853965f
Markus Fleschutz <markus@fleschutz.de>
2021-11-06 17:35:23 +0100

    Fix typo

--- Scripts/open-flight-radar.ps1
+++ Scripts/open-flight-radar.ps1
@@ -11,5 +11,5 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
-& "$PSScriptRoot/open-browser.ps1" "https://www.flightradar.com"
+& "$PSScriptRoot/open-browser.ps1" "https://www.flightradar24.com"
 exit 0 # success
