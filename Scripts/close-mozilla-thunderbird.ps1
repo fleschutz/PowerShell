07f46cb36aa253b152de87c06a63e2d94d007b37
Markus Fleschutz <markus@fleschutz.de>
2021-11-06 11:45:00 +0100

    Add close-mozilla-thunderbird.ps1

---
+++ Scripts/close-mozilla-thunderbird.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Closes Mozilla's Thunderbird email client 
+.DESCRIPTION
+	This script closes the Mozilla Thunderbird email client gracefully.
+.EXAMPLE
+	PS> ./close-mozilla-thunderbird
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+taskkill /im thunderbird.exe
+exit 0 # success
