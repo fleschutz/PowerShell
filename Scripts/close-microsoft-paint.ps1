2386fa4df9be7e230096f241b0e5375d078a33eb
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-11-09 09:23:29 +0100

    Add open-microsoft-paint.ps1 and close-microsoft-paint.ps1

---
+++ Scripts/close-microsoft-paint.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Closes the Microsoft Paint app 
+.DESCRIPTION
+	This script closes the Microsoft Paint application gracefully.
+.EXAMPLE
+	PS> ./close-microsoft-paint
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+taskkill /im mspaint.exe
+exit 0 # success
---
+++ Scripts/open-microsoft-paint.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Launches the Microsoft Paint app
+.DESCRIPTION
+	This script launches the Microsoft Paint application.
+.EXAMPLE
+	PS> ./open-microsoft-paint
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	start-process mspaint.exe
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
