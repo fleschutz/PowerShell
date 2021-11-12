5ed926b608d5cf888363f07d62af16d60e1f83b9
Markus Fleschutz <markus@fleschutz.de>
2021-11-12 22:12:25 +0100

    Add close-three-d-viewer.ps1

---
+++ Scripts/close-three-d-viewer.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Closes the 3D Viewer app 
+.DESCRIPTION
+	This script closes the 3D Viewer application gracefully.
+.EXAMPLE
+	PS> ./close-three-d-viewer
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+taskkill /f /im 3DViewer.exe
+exit 0 # success
