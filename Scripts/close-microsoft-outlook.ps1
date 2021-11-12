9d240326c882d45f4e1a7daf7e67816ae135bb02
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-11-12 12:02:07 +0100

    Add open-microsoft-outlook.ps1 and close-microsoft-outlook.ps1

---
+++ Scripts/close-microsoft-outlook.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Closes Microsoft Outlook 
+.DESCRIPTION
+	This script closes the Microsoft Outlook email application gracefully.
+.EXAMPLE
+	PS> ./close-microsoft-outlook
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+taskkill /im outlook.exe
+exit 0 # success
---
+++ Scripts/open-microsoft-outlook.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Launches Microsoft Outlook
+.DESCRIPTION
+	This script launches the Microsoft Outlook email application.
+.EXAMPLE
+	PS> ./open-microsoft-outlook
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+function TryLaunching { param($Path)
+	if (test-path "$Path" -pathType leaf) {
+		start-process "$Path"
+		exit 0 # success
+	}
+}
+
+try {
+	TryLaunching "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+	TryLaunching "C:\Programs\Microsoft Office\Office14\OUTLOOK.EXE"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
