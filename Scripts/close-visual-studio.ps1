9c902b4d508d9484a292cfcef69ba60c2fa352ca
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-11-12 12:45:13 +0100

    Add open-visual-studio.ps1 and close-visual-studio.ps1

---
+++ Scripts/close-visual-studio.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Closes Visual Studio
+.DESCRIPTION
+	This script closes the Microsoft Visual Studio application gracefully.
+.EXAMPLE
+	PS> ./close-visual-studio
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+taskkill /im devenv.exe
+exit 0 # success
---
+++ Scripts/open-visual-studio.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Launches Visual Studio
+.DESCRIPTION
+	This script launches the Microsoft Visual Studio application.
+.EXAMPLE
+	PS> ./open-visual-studio
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+function TryLaunching { param($Path)
+	if (test-path "$Path" -pathType leaf) {
+		start-process "$Path"
+		exit 0 # success
+	}
+}
+
+try {
+	TryLaunching "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"
+	TryLaunching "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
