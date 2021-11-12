bfa95f9a5b83b5e26c89c4e1a4371c90e9366e18
Markus Fleschutz <markus@fleschutz.de>
2021-11-12 22:05:50 +0100

    Add open-three-d-viewer.ps1

---
+++ Scripts/open-three-d-viewer.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Launches the 3D-Viewer app
+.DESCRIPTION
+	This script launches the 3D-Viewer application.
+.EXAMPLE
+	PS> ./open-three-d-viewer
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+Start-Process com.microsoft.3dviewer:
+exit 0 # success
