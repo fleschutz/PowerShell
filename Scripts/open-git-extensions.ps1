24a26a718ce7dee3c8b80f3b0e1e3c6259673ac7
Markus <markus@fleschutz.de>
2021-11-14 11:49:59 +0100

    Improve open-git-extensions.ps1

--- Scripts/open-git-extensions.ps1
+++ Scripts/open-git-extensions.ps1
@@ -11,16 +11,17 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
-function TryLaunching { param($Path)
-	if (test-path "$Path" -pathType leaf) {
-		start-process "$Path"
+function TryExec { param($Folder, $Binary)
+	if (test-path "$Folder/$Binary" -pathType leaf) {
+		start-process "$Folder/$Binary" -WorkingDirectory "$Folder"
 		exit 0 # success
 	}
 }
 
 try {
-	TryLaunching "C:\Program Files (x86)\GitExtensions\GitExtensions.exe"
-	TryLaunching "C:\Program Files\GitExtensions\GitExtensions.exe"
+	TryExec "C:\Program Files (x86)\GitExtensions" "GitExtensions.exe"
+	TryExec "C:\Program Files\GitExtensions" "GitExtensions.exe"
+	& "$PSScriptRoot/speak-english.ps1" "Sorry, I can't find Git Extensions"
 	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
