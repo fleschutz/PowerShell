ba95630b7871e9c9aa012fcfcda7da756008e9d7
Markus Fleschutz <markus@fleschutz.de>
2021-11-15 09:55:08 +0100

    Add open-fritz-repeater.ps1

---
+++ Scripts/open-fritz-repeater.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens FRITZ!Repeater's Web interface
+.DESCRIPTION
+	This script launches the Web browser with the Web interface of FRITZ!Repeater.
+.EXAMPLE
+	PS> ./open-fritz-repeater
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "http://fritz.repeater"
+exit 0 # success
