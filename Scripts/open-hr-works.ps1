243a2cdef5adf18152fbd52a759df582f9a09a46
Markus Fleschutz <markus@fleschutz.de>
2021-11-15 12:57:23 +0100

    Add open-hr-works.ps1

---
+++ Scripts/open-hr-works.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the HRworks website
+.DESCRIPTION
+	This script launches the Web browser with the HRworks website.
+.EXAMPLE
+	PS> ./open-hr-works
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.hrworks.de"
+exit 0 # success
