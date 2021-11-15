23d02c432e26443d50b48254653c6a4257c05582
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-11-15 14:08:58 +0100

    Update close-microsoft-edge.ps1

--- Scripts/close-microsoft-edge.ps1
+++ Scripts/close-microsoft-edge.ps1
@@ -11,8 +11,8 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
-$result = TaskKill /im msedge.exe
-if ($result -ne 0) {
+TaskKill /im msedge.exe /f /t
+if ($lastExitCode -ne "0") {
 	& "$PSScriptRoot/speak-english.ps1" "Sorry, can't close Microsoft Edge"
 	exit 1
 }
