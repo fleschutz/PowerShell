3da0c69477942306d66b0b11e73212a26e368547
Markus Fleschutz <markus@fleschutz.de>
2021-11-18 07:24:45 +0100

    Add 2 scripts

---
+++ Scripts/open-distro-watch.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the DistroWatch website
+.DESCRIPTION
+	This script launches the Web browser with the DistroWatch website.
+.EXAMPLE
+	PS> ./open-distro-watch
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://distrowatch.com/"
+exit 0 # success
---
+++ Scripts/open-whats-app.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the WhatsApp website
+.DESCRIPTION
+	This script launches the Web browser with the WhatsApp website.
+.EXAMPLE
+	PS> ./open-whats-app
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.whatsapp.com/"
+exit 0 # success
