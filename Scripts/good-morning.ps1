d013e3e60b3fd708c9d90f30b4d1ac00ea7fe68e
Markus Fleschutz <markus@fleschutz.de>
2021-11-18 07:31:23 +0100

    Add good-morning.ps1

---
+++ Scripts/good-morning.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Say an answer to 'good morning'
+.DESCRIPTION
+	This script answers "good morning" by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./good-morning
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	& "$PSScriptRoot/speak-english.ps1" "Well, good morning to you too"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
