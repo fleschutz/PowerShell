72f75c2870036f0a684c655de1eda75930689381
Markus Fleschutz <markus@fleschutz.de>
2021-11-18 11:34:20 +0100

    Add how-are-you.ps1

---
+++ Scripts/how-are-you.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Say an answer to 'how are you?'
+.DESCRIPTION
+	This script answers "how are you?" by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./how-are-you
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	& "$PSScriptRoot/speak-english.ps1" "I'm fine, thanks."
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
