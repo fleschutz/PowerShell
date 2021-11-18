c0ac182041ebcf3903b7ea82304e2aa0b02b5526
Markus Fleschutz <markus@fleschutz.de>
2021-11-18 16:50:40 +0100

    Add open-walmart-website.ps1

---
+++ Scripts/open-walmart-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Walmart website
+.DESCRIPTION
+	This script launches the Web browser with the Walmart website.
+.EXAMPLE
+	PS> ./open-walmart-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.walmart.com/"
+exit 0 # success
