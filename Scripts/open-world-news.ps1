c19fcecc799c4a22b2618eb3a51c94e1127c2e84
Markus Fleschutz <markus@fleschutz.de>
2021-11-18 07:53:21 +0100

    Add open-world-news.ps1

---
+++ Scripts/open-world-news.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens Reuters World News website
+.DESCRIPTION
+	This script launches the Web browser with the Reuters World News website.
+.EXAMPLE
+	PS> ./open-world-news
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.reuters.com/news/archive/worldNews"
+exit 0 # success
