22e7873c9e0d5f598b5435c4ad0f554a04ba3818
Markus Fleschutz <markus@fleschutz.de>
2021-11-18 15:25:43 +0100

    Add 2 scripts

---
+++ Scripts/open-cdc-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the CDC website
+.DESCRIPTION
+	This script launches the Web browser with the CDC website (Centers for Disease Control and Prevention).
+.EXAMPLE
+	PS> ./open-cdc-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.cdc.gov/"
+exit 0 # success
