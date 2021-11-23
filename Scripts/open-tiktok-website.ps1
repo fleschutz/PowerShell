2e5be6ac68ab6125e1911aad7832c5b54e168429
Markus Fleschutz <markus@fleschutz.de>
2021-11-23 07:16:04 +0100

    Add open-tiktok-website.ps1

---
+++ Scripts/open-tiktok-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the TikTok website
+.DESCRIPTION
+	This script launches the Web browser with the TikTok website.
+.EXAMPLE
+	PS> ./open-tiktop-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-browser.ps1" "https://www.tiktok.com"
+exit 0 # success
