ce5311e427d32b92bdab0cc16283d076b099b5f9
Markus Fleschutz <markus@fleschutz.de>
2021-11-23 12:24:37 +0100

    Simple rename

--- Scripts/open-task-bar-settings.ps1
+++ Scripts/open-task-bar-settings.ps1
@@ -1,10 +1,10 @@
 ﻿<#
 .SYNOPSIS
-	Opens Windows taskbar settings 
+	Opens the Windows taskbar settings 
 .DESCRIPTION
 	This script launches the Windows taskbar settings application.
 .EXAMPLE
-	PS> ./open-taskbar-settings
+	PS> ./open-task-bar-settings
 .NOTES
 	Author: Markus Fleschutz · License: CC0
 .LINK
