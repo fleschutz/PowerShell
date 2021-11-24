01e333675d4a2a65e183dfa81a03bef2d615932e
Markus Fleschutz <markus@fleschutz.de>
2021-11-24 14:16:41 +0100

    Update good-night.ps1

--- Scripts/good-night.ps1
+++ Scripts/good-night.ps1
@@ -12,13 +12,7 @@
 #>
 
 try {
-	$Generator = New-Object System.Random
-	$Answer = switch([int]$Generator.next(0, 3)) {
-	0 { "Good night to you, too." }
-	1 { "Good night to you, my friend." }
-	2 { "Have a good night. Sleep well." }
-	3 { "Good night and sweet dreams." }
-	}
+	$Answer = "Good night to you, too.", "Good night to you, my friend.", "Have a good night. Sleep well.", "Good night and sweet dreams." | Get-Random
 	& "$PSScriptRoot/speak-english.ps1" "$Answer"
 	exit 0 # success
 } catch {
