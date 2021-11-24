6320b1f287138501ff21c13f56dfe7e14b94957a
Markus Fleschutz <markus@fleschutz.de>
2021-11-24 13:26:13 +0100

    Add i-will-be-back.ps1

---
+++ Scripts/i-will-be-back.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Answers 'I'll be back'
+.DESCRIPTION
+	This script answers "I'll be back" by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./i-will-be-back
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/speak-english.ps1" "Hasta la vista, baby."
+exit 0 # success
