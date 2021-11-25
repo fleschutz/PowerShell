8e34e347143ce7e1a2ee4d3e0b83e59c5e7aa743
Markus Fleschutz <markus@fleschutz.de>
2021-11-25 17:15:57 +0100

    Add open-pay-pal-website.ps1

---
+++ Scripts/open-pay-pal-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the PayPal website
+.DESCRIPTION
+	This script launches the Web browser with the PayPal website.
+.EXAMPLE
+	PS> ./open-pay-pal-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.paypal.com"
+exit 0 # success
