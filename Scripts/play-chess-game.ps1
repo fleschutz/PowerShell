6d9ff7f7502d8a8e52bed3fa6df2ee890d833714
Markus Fleschutz <markus@fleschutz.de>
2021-11-25 17:11:17 +0100

    Add play-chess-game.ps1

---
+++ Scripts/play-chess-game.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Plays the Chess game
+.DESCRIPTION
+	This script launches the Web browser with the Chess game.
+.EXAMPLE
+	PS> ./play-chess-game
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.chess.com/"
+exit 0 # success
