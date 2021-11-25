01eed5424a74f2c05324dd634db50d31b12c68e4
Markus Fleschutz <markus@fleschutz.de>
2021-11-25 17:00:36 +0100

    Update close-task-manager.ps1

--- Scripts/close-task-manager.ps1
+++ Scripts/close-task-manager.ps1
@@ -11,7 +11,5 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
-#Requires -RunAsAdministrator
-
-& "$PSScriptRoot/close-program.ps1" "Task Manager" "Taskmgr" "Taskmgr.exe"
+tskill taskmgr
 exit 0 # success
