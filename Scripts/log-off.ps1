ae2ba5bf3059e1d14ea631fb10af77bdfa76ac27
Markus <markus@fleschutz.de>
2021-11-27 12:30:29 +0100

    Add log-off.ps1

---
+++ Scripts/log-off.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Log off the current user
+.DESCRIPTION
+	This script logs off the current Windows user.
+.EXAMPLE
+	PS> ./log-off
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+
+try {
+	Invoke-CimMethod -ClassName Win32_Operatingsystem -MethodName Win32Shutdown -Arguments @{ Flags = 0 }
+
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
