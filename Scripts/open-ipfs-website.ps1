304d2fc2658a573b6f8c1cc8704b720836400acc
Markus Fleschutz <markus@fleschutz.de>
2021-11-29 21:45:02 +0100

    Add 2 websites

---
+++ Scripts/open-ipfs-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the IPFS website
+.DESCRIPTION
+	This script launches the Web browser with the IPFS website.
+.EXAMPLE
+	PS> ./open-ipfs-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://ipfs.io/"
+exit 0 # success
---
+++ Scripts/open-snap-store-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Snap Store website
+.DESCRIPTION
+	This script launches the Web browser with the Snap Store website.
+.EXAMPLE
+	PS> ./open-snap-store-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://snapcraft.io/store"
+exit 0 # success
