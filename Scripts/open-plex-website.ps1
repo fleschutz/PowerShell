116086a1aa029e1be04cee45ac1fcc4763d22ba2
Markus Fleschutz <markus@fleschutz.de>
2021-11-30 15:55:40 +0100

    Add open-plex-website.ps1

---
+++ Scripts/open-plex-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Plex website
+.DESCRIPTION
+	This script launches the Web browser with the Plex website.
+.EXAMPLE
+	PS> ./open-plex-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.plex.tv/"
+exit 0 # success
