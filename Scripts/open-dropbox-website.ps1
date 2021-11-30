76c805abf8aec8166fde90e194e945e8f7c4b6ce
Markus Fleschutz <markus@fleschutz.de>
2021-11-30 15:51:03 +0100

    Add open-dropbox-website.ps1

---
+++ Scripts/open-dropbox-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Dropbox website
+.DESCRIPTION
+	This script launches the Web browser with the Dropbox website.
+.EXAMPLE
+	PS> ./open-dropbox-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.dropbox.com"
+exit 0 # success
