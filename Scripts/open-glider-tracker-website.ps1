26a062a9beabbeee930ad517d0d9106f9f03078a
Markus Fleschutz <markus@fleschutz.de>
2021-11-30 16:17:47 +0100

    Add open-glider-tracker-website.ps1

---
+++ Scripts/open-glider-tracker-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the GliderTracker website
+.DESCRIPTION
+	This script launches the Web browser with the GliderTracker website.
+.EXAMPLE
+	PS> ./open-glider-tracker-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://glidertracker.de/"
+exit 0 # success
