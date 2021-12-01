b7bc6bbb822a3a079eacecd109e86e65b74113cb
Markus Fleschutz <markus@fleschutz.de>
2021-12-01 08:20:46 +0100

    Add install-obs-studio.ps1

---
+++ Scripts/install-obs-studio.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+	Installs OBS Studio (needs admin rights)
+.DESCRIPTION
+	This script installs OBS Studio (admin rights are needed).
+.EXAMPLE
+	PS> ./install-obs-studio
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+#Requires -RunAsAdministrator
+
+try {
+	$StopWatch = [system.diagnostics.stopwatch]::startNew()
+
+	if ($IsLinux) {
+		"Sorry, not supported yet."
+	} else {
+		winget install obsproject.obsstudio
+	}
+
+	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
+	"✔️ installed OBS Studio in $Elapsed sec"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
