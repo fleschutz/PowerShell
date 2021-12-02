b3e6c4711f0452c1fa95b27f5b542c51f7c07731
Markus Fleschutz <markus@fleschutz.de>
2021-12-02 14:49:17 +0100

    Add check-vpn.ps1

---
+++ Scripts/check-vpn.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Checks the VPN connection(s)
+.DESCRIPTION
+	This script checks the VPN connection(s).
+.EXAMPLE
+	PS> ./check-vpn
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+#>
+
+try {
+	$Connections = (Get-VPNConnection)
+	$Reply = ""
+	foreach($Connection in $Connections) {
+		$Reply += "connection $($Connection.Name) is $($Connection.ConnectionStatus), "
+	}
+	if ("$Reply" -eq "") { $Reply = "No VPN connection available"
+
+	"✔️ $Reply"
+	& "$PSScriptRoot/speak-english.ps1" "$Reply"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
