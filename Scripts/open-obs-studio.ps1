1d5c57c7584de45585b7cda7d322ea4cfb4315dd
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-03 13:07:13 +0100

    Fix open-obs-studio.ps1

--- Scripts/open-obs-studio.ps1
+++ Scripts/open-obs-studio.ps1
@@ -11,16 +11,16 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
-function TryLaunching { param($Path)
+function TryLaunching { param([string]$Path, [string]$Dir)
 	if (test-path "$Path" -pathType leaf) {
-		start-process "$Path"
+		start-process -FilePath "$Path" -WorkingDirectory "$Dir"
 		exit 0 # success
 	}
 }
 
 try {
-	TryLaunching "C:\Program Files (x86)\OBS Studio\bin\64bit\obs64.exe"
-	TryLaunching "C:\Program Files\OBS Studio\bin\64bit\obs64.exe"
+	TryLaunching "C:\Program Files (x86)\OBS Studio\bin\64bit\obs64.exe" "C:\Program Files (x86)\OBS Studio\bin\64bit\"
+	TryLaunching "C:\Program Files\OBS Studio\bin\64bit\obs64.exe" "C:\Program Files\OBS Studio\bin\64bit\"
 	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
