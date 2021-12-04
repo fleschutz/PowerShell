3e72ee7e3c0b61d8d2de24bde442b60efd8d0be3
Markus Fleschutz <markus@fleschutz.de>
2021-12-04 16:22:16 +0100

    Add show-ant-manual.ps1 and show-msbuild-manual.ps1

---
+++ Scripts/show-ant-manual.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the Ant manual
+.DESCRIPTION
+	This script launches the Web browser with the Apache Ant manual.
+.EXAMPLE
+	PS> ./show-ant-manual
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://ant.apache.org/manual/index.html"
+exit 0 # success
---
+++ Scripts/show-msbuild-manual.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the MSBuild manual
+.DESCRIPTION
+	This script launches the Web browser with the MSBuild manual.
+.EXAMPLE
+	PS> ./show-msbuild-manual
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://docs.microsoft.com/de-de/visualstudio/msbuild/msbuild"
+exit 0 # success
