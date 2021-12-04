70422aaeb0887ca9e848e8c300dff52a57fed69d
Markus Fleschutz <markus@fleschutz.de>
2021-12-04 17:51:31 +0100

    Add check-iss.ps1

---
+++ Scripts/check-iss.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Checks the ISS 
+.DESCRIPTION
+	This script checks the International Space Station (ISS) and report the position by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./check-iss
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+#>
+
+try {
+	$ISS = (Invoke-WebRequest "http://api.open-notify.org/iss-now.json" -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
+
+	$Reply = "ISS is at $($ISS.iss_position.longitude)° longitude, $($ISS.iss_position.latitude)° latitude."
+	"✔️ $Reply"
+	& "$PSScriptRoot/speak-english.ps1" "$Reply"
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
