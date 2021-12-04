4e5cd0a94e3a4c88f22a655c8722dff28c6e6def
Markus Fleschutz <markus@fleschutz.de>
2021-12-04 13:27:16 +0100

    Add open-serenade-website.ps1

---
+++ Scripts/open-serenade-website.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Opens the Serenade website
+.DESCRIPTION
+	This script launches the Web browser with the Serenade.ai website.
+.EXAMPLE
+	PS> ./open-serenade-website
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://serenade.ai/"
+exit 0 # success
