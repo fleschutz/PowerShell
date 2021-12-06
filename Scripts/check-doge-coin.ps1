8301a63a6a82de62e2be7d3e101a31a4f3769858
Markus Fleschutz <markus@fleschutz.de>
2021-12-06 19:07:45 +0100

    Simple rename

--- Scripts/check-doge-coin.ps1
+++ Scripts/check-doge-coin.ps1
@@ -4,7 +4,7 @@
 .DESCRIPTION
 	This script queries the current Dogecoin exchange rates and answers by text-to-speech (TTS).
 .EXAMPLE
-	PS> ./check-dogecoin
+	PS> ./check-doge-coin
 .NOTES
 	Author: Markus Fleschutz · License: CC0
 .LINK
