9c405e1f6d0cd0cd71e28bc663738627b28d046f
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-07 07:22:39 +0100

    Add show-euro-rate.ps1 and show-us-dollar-rate.ps1

---
+++ Scripts/show-euro-rate.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the Euro rate 
+.DESCRIPTION
+	This script launches the Web browser and shows the Euro (EUR/€) rate.
+.EXAMPLE
+	PS> ./show-euro-rate
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.bitstamp.net/markets/eur/usd/"
+exit 0 # success
---
+++ Scripts/show-us-dollar-rate.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the US$ rate 
+.DESCRIPTION
+	This script launches the Web browser and shows the US Dollar (USD/US$) rate.
+.EXAMPLE
+	PS> ./show-us-dollar-rate
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.bitstamp.net/markets/eur/usd"
+exit 0 # success
