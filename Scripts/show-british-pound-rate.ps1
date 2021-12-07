86a19bb19da2c555ab28dbd860598e2bb3bdb1ae
Markus <markus@fleschutz.de>
2021-12-07 17:36:52 +0100

    Add show-british-pound-rate.ps1

---
+++ Scripts/show-british-pound-rate.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the British Pound rate 
+.DESCRIPTION
+	This script launches the Web browser and shows the British Pound (GBP) rate.
+.EXAMPLE
+	PS> ./show-british-pound-rate
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.bitstamp.net/markets/gbp/usd/"
+exit 0 # success
