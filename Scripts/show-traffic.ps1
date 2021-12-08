40f628dae411e959c6f877d208f57aeb7673b22f
Markus Fleschutz <markus.fleschutz@atcsim.de>
2021-12-08 11:24:36 +0100

    Add show-traffic.ps1

---
+++ Scripts/show-traffic.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Shows the current traffic 
+.DESCRIPTION
+	This script launches the Web browser with MSN showing the current traffic.
+.EXAMPLE
+	PS> ./show-traffic
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.msn.com/en-us/traffic?lvl=11&intent=roadClosures"
+exit 0 # success
