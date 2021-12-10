68c00db77a5bb8e0e6afe002fc6c463acbae916a
Markus Fleschutz <markus@fleschutz.de>
2021-12-10 19:28:42 +0100

    Add greeting scripts

---
+++ Scripts/happy-holidays.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+	Replies to "Hi"
+.DESCRIPTION
+	This script replies to 'Hi' by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./hi
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+$Reply = "Hey!", "Hello!", "Hi there!", "Hey there!", "Hey! How's it going?", "What's up?", "What's happening?", "How goes it?" | Get-Random
+
+& "$PSScriptRoot/give-reply.ps1" "$Reply"
+exit 0 # success
