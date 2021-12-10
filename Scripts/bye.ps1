b4c2f03fbe6a88510c7eacf0813b1eb294ae9d76
Markus Fleschutz <markus@fleschutz.de>
2021-12-10 19:47:36 +0100

    Add bye.ps1

---
+++ Scripts/bye.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+	Replies to 'bye'
+.DESCRIPTION
+	This script says a reply to 'bye' by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./bye
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+$Reply = "Bye.", "Good bye.", "See you.", "Bye bye." | Get-Random
+
+& "$PSScriptRoot/give-reply.ps1" "$Reply"
+exit 0 # success
