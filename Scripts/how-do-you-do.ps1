d23894fa80aeae532089f556dff85f9c91454f47
Markus Fleschutz <markus@fleschutz.de>
2021-12-10 19:40:13 +0100

    Add how-do-you-do.ps1

---
+++ Scripts/how-do-you-do.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+	Answers to 'how are you?'
+.DESCRIPTION
+	This script says a reply to "how are you?" by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./how-are-you
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+$Reply = "I'm fine, thanks. How are you?", "I'm fine, maybe a little tired. I need some more coffee.", "Great, thank you. How are you?", "Good, thanks, and you?", "Fine, thanks. How are you?" | Get-Random
+
+& "$PSScriptRoot/give-reply.ps1" "$Reply"
+exit 0 # success
