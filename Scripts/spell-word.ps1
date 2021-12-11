e188da6c5eea06cd99964f9a2cad4d9dcb73a430
Markus Fleschutz <markus@fleschutz.de>
2021-12-11 15:49:59 +0100

    Update spell-word.ps1

--- Scripts/spell-word.ps1
+++ Scripts/spell-word.ps1
@@ -3,6 +3,8 @@
 	Spells a word
 .DESCRIPTION
 	This script spells the given word by text-to-speech (TTS).
+.PARAMETER word
+        Specifies the word to spell
 .EXAMPLE
 	PS> ./spell-word
 .NOTES
@@ -11,7 +13,20 @@
 	https://github.com/fleschutz/PowerShell
 #>
 
-$Reply = "T E S T"
+param([string]$word = "")
 
-& "$PSScriptRoot/give-reply.ps1" "$Reply"
-exit 0 # success
+try {
+	if ($word -eq "" ) { $word = read-host "Enter word to spell" }
+
+	[char[]]$ArrayOfChars = $word.ToUpper()
+	$Reply = ""
+	foreach($Char in $ArrayOfChars) {
+		$Reply += $Char
+		$Reply += " "
+	}
+	& "$PSScriptRoot/give-reply.ps1" "$Reply"
+	exit 0 # success
+} catch {
+        "⚠️Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+        exit 1
+}
