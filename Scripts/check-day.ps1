9259ab452a4f29298cc81a613d90703d6deac804
Markus <markus@fleschutz.de>
2021-12-11 17:48:35 +0100

    Add check-day.ps1

---
+++ Scripts/check-day.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+	Determines the current day 
+.DESCRIPTION
+	This script determines and speaks the current day by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./check-day
+	✔️ It's Sunday.
+.NOTES
+	Author: Markus Fleschutz · License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+try {
+	[system.threading.thread]::currentthread.currentculture=[system.globalization.cultureinfo]"en-US"
+	$Weekday = (Get-Date -format "dddd")
+	& "$PSScriptRoot/give-reply.ps1" "It's $Weekday."
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
