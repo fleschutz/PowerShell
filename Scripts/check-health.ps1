757eba69d97a10129f3c7b9d5e82adc94d8ba0b0
Markus Fleschutz <markus@fleschutz.de>
2022-01-29 11:36:17 +0100

    check-health.ps1

--- Scripts/check-health.ps1
+++ Scripts/check-health.ps1
@@ -13,8 +13,7 @@
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
-	Author:  Markus Fleschutz
-	License: CC0
+	Author: Markus Fleschutz / License: CC0
 #>
 
 & "$PSScriptRoot/check-uptime.ps1"
