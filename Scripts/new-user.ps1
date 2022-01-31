bf7bdf11f8419a1575c5c0a1a2df262a88bb069a
Markus Fleschutz <markus@fleschutz.de>
2022-01-31 14:24:06 +0100

    Add new-user.ps1

---
+++ Scripts/new-user.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+	Creates a new user account
+.DESCRIPTION
+	This PowerShell script creates a new user account.
+.EXAMPLE
+	PS> ./new-user
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+#>
+
+param([string]$Username = "")
+
+try {
+	if ($Username -eq "") { $Username = read-host "Enter new user name" }
+	$StopWatch = [system.diagnostics.stopwatch]::startNew()
+
+	if ($IsLinux) {
+		& sudo adduser --encrypt-home $Username
+	} else {
+		throw "Not supported yet"
+	}
+
+	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
+	"✔️ created new user '$Username' with encrypted home directory in $Elapsed sec"
+	exit 0 # success
+} catch {
+	"⚠️ Sorry: $($Error[0])"
+	exit 1
+}
