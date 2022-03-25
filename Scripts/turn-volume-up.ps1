12639db86165b59003abd9bace4f5a4a48a49263
Markus Fleschutz <markus@fleschutz.de>
2022-03-25 10:09:07 +0100

    Update turn-volume-up.ps1

--- Scripts/turn-volume-up.ps1
+++ Scripts/turn-volume-up.ps1
@@ -1,8 +1,8 @@
 ﻿<#
 .SYNOPSIS
-	Turns the audio volume up 
+	Turns the volume up 
 .DESCRIPTION
-	This PowerShell script turns the audio volume up (by +10% by default)
+	This PowerShell script turns the audio volume up (+10% by default).
 .PARAMETER percent
 	Specifies the percent number
 .EXAMPLE
@@ -10,7 +10,7 @@
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
-	Author: Markus Fleschutz / License: CC0
+	Author: Markus Fleschutz | License: CC0
 #>
 
 param([int]$percent = 10)
@@ -20,7 +20,7 @@ try {
 	for ([int]$i = 0; $i -lt $percent; $i += 2) {
 		$obj.SendKeys([char]175) # each tick is +2%
 	}
-	& "$PSScriptRoot/give-reply.ps1" "OK, $($percent)% louder."
+	& "$PSScriptRoot/give-reply.ps1" "$($percent)% louder."
 	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
