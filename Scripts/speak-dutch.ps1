2ff3b1e4a157505935d86475809e44fd06d29ad8
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-03-28 15:22:32 +0200

    Add speak-dutch.ps1

---
+++ Scripts/speak-dutch.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Speaks text in Dutch
+.DESCRIPTION
+	This PowerShell script speaks the given text with a Dutch text-to-speech (TTS) voice.
+.PARAMETER text
+	Specifies the text to speak
+.EXAMPLE
+	PS> ./speak-dutch Hallo
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$text = "")
+
+try {
+	if ($text -eq "") { $text = read-host "Enter the Dutch text to speak" }
+
+	$TTSVoice = New-Object -ComObject SAPI.SPVoice
+	foreach ($Voice in $TTSVoice.GetVoices()) {
+		if ($Voice.GetDescription() -like "*- Dutch*") { 
+			$TTSVoice.Voice = $Voice
+			[void]$TTSVoice.Speak($text)
+			exit 0 # success
+		}
+	}
+	throw "No Dutch text-to-speech voice found - please install one"
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
