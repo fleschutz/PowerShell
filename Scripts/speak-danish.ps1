485bcbfe657bfe2bea4f8ddf694aa287ed148935
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-03-28 14:55:41 +0200

    Add speak-danish.ps1 and speak-polish.ps1

---
+++ Scripts/speak-danish.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Speaks text with a Danish text-to-speech voice
+.DESCRIPTION
+	This PowerShell script speaks the given text with a Danish text-to-speech (TTS) voice.
+.PARAMETER text
+	Specifies the text to speak
+.EXAMPLE
+	PS> ./speak-danish Hej
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+#>
+
+param([string]$text = "")
+
+try {
+	if ($text -eq "") { $text = read-host "Enter the Danish text to speak" }
+
+	$TTSVoice = New-Object -ComObject SAPI.SPVoice
+	foreach ($Voice in $TTSVoice.GetVoices()) {
+		if ($Voice.GetDescription() -like "*- Danish*") { 
+			$TTSVoice.Voice = $Voice
+			[void]$TTSVoice.Speak($text)
+			exit 0 # success
+		}
+	}
+	throw "No Danish text-to-speech voice found - please install one"
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
