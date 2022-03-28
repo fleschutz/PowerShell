12ca2bf138e2406c3f6e6e66737b00a0db22df72
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-03-28 11:24:50 +0200

    Add turn-volume-fully-up.ps1

---
+++ Scripts/turn-volume-fully-up.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+	Turn volume fully up
+.DESCRIPTION
+	This PowerShell script turns the audio volume fully up to 100%.
+.EXAMPLE
+	PS> ./turn-volume-fully-up
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+#>
+
+try {
+	$obj = New-Object -com wscript.shell
+	for ([int]$i = 0; $i -lt 100; $i += 2) {
+		$obj.SendKeys([char]175) # each tick is +2%
+	}
+	& "$PSScriptRoot/give-reply.ps1" "Hundred percent now."
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
