149bf8baf8492ab11301de7301e7a8246654dd6d
Markus Fleschutz <markus@fleschutz.de>
2022-04-11 08:54:09 +0200

    Update list-voices.ps1

--- Scripts/list-voices.ps1
+++ Scripts/list-voices.ps1
@@ -1,19 +1,19 @@
 ﻿<#
 .SYNOPSIS
-	Lists the installed text-to-speech voices
+	Lists TTS voices
 .DESCRIPTION
 	This PowerShell script lists the installed text-to-speech (TTS) voices.
 .EXAMPLE
 	PS> ./list-voices
 
-	Name                    Culture Gender   Age
-	----                    ------- ------   ---
-	Microsoft David Desktop en-US     Male Adult
-	Microsoft Zira Desktop  en-US   Female Adult
+	Name                     Culture  Gender     Age
+	----                     -------  ------     ---
+	Microsoft David Desktop  en-US      Male   Adult
+	...
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
-	Author: Markus Fleschutz / License: CC0
+	Author: Markus Fleschutz | License: CC0
 #>
 
 #Requires -Version 2.0
