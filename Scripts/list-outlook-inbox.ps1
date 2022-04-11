b1106be61598a41205297cfc0112391450f93768
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-11 11:03:35 +0200

    Add list-outlook-inbox.ps1

---
+++ Scripts/list-outlook-inbox.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+	Lists Outlook's Inbox 
+.DESCRIPTION
+	This PowerShell script lists the emails in the inbox of Outlook.
+.EXAMPLE
+	PS> ./list-outlook-inbox
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	$Outlook = New-Object -com Outlook.application
+	$MAPI = $Outlook.GetNameSpace("MAPI")
+	$Inbox = $MAPI.GetDefaultFolder(6) # 6 = olFolderInbox
+	$Inbox.items | Select Received,Subject | Format-Table -AutoSize
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
