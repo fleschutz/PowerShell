d68df88c4d9d827f0bd9b23440d15088dcfcc1de
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-11 11:17:45 +0200

    Add list-outlook-sent.ps1

---
+++ Scripts/list-outlook-sent.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+	Lists Outlook's Sent Mails
+.DESCRIPTION
+	This PowerShell script lists the mails in the Sent Mail folder of Outlook.
+.EXAMPLE
+	PS> ./list-outlook-sent
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	$Outlook = New-Object -com Outlook.application
+	$MAPI = $Outlook.GetNameSpace("MAPI")
+	$Inbox = $MAPI.GetDefaultFolder(5) # 5 = olFolderSentMail
+	$Inbox.items | Select SentOn,Subject | Format-Table -AutoSize
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
