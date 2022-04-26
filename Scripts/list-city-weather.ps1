7e0bee76d6b83799222488a4471eab1880f7b79a
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-04-26 15:36:21 +0200

    Update list-city-weather.ps1

--- Scripts/list-city-weather.ps1
+++ Scripts/list-city-weather.ps1
@@ -8,16 +8,21 @@
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
-	Author: Markus Fleschutz / License: CC0
+	Author: Markus Fleschutz | License: CC0
 #>
 
-$Cities="Hawaii","Los Angeles","Mexico City","Miami","New York","Rio de Janeiro","Paris","London","Berlin","Cape Town","Dubai","Mumbai","Singapore","Hong Kong","Peking","Tokyo","Sydney"
+$Cities="Hawaii","Los Angeles","Mexico City","Dallas","Miami","New York","Rio de Janeiro","Paris","London","Berlin","Cape Town","Dubai","Mumbai","Singapore","Hong Kong","Perth","Peking","Tokyo","Sydney"
+
+function ListCityWeather {
+	foreach($City in $Cities) {
+		$Conditions = (Invoke-WebRequest http://wttr.in/${City}?format="%c  +%t`t+%p`t+%h`t+%P  +%w" -UserAgent "curl" -useBasicParsing).Content
+		$Sun = (Invoke-WebRequest http://wttr.in/${City}?format="+%S →+%s" -UserAgent "curl" -useBasicParsing).Content
+		New-Object PSObject -Property @{ City="$City"; Conditions="$Conditions"; Sun="$Sun" }
+	}
+}
 
 try {
-	foreach($City in $Cities) {
-		$Line = (Invoke-WebRequest http://wttr.in/${City}?format="%c %l+%t+%p+%h+%P+%w +%S →+%s" -UserAgent "curl" -useBasicParsing).Content
-		"$Line"
-	}
+	ListCityWeather | Format-Table -property City,Conditions,Sun
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
