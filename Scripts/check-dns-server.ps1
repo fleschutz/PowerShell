1803b09fad7a8138e791a3b4ebc828556641adf5
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-05-03 08:59:36 +0200

    Update check-dns-server.ps1

--- Scripts/check-dns-server.ps1
+++ Scripts/check-dns-server.ps1
@@ -20,15 +20,16 @@ function CheckDNS { param($IP, $Name)
  
 try {
 	"Checking speed of public DNS server..."
-	CheckDNS 217.237.150.188 "1&1"
 	CheckDNS 1.1.1.1 "Cloudflare (primary)"
 	CheckDNS 1.0.0.1 "Cloudflare (secondary)"
 	CheckDNS 1.1.1.2 "Cloudflare with malware blocklist"
 	CheckDNS 1.0.0.2 "Cloudflare with malware blocklist"
 	CheckDNS 1.1.1.3 "Cloudflare with malware+adult blocklist"
 	CheckDNS 1.0.0.3 "Cloudflare with malware+adult blocklist" 
-	CheckDNS 217.5.100.185 "Deutsche Telekom AG"
-	CheckDNS 217.5.100.186 "Deutsche Telekom AG"
+	CheckDNS 84.200.69.80 "DNS.Watch (primary)"
+	CheckDNS 84.200.70.40 "DNS.Watch (secondary)"
+	CheckDNS 37.235.1.174 "FreeDNS Vienna (primary)"
+	CheckDNS 37.235.1.177 "FreeDNS Vienna (secondary)"
 	CheckDNS 8.8.8.8 "Google Public DNS (primary)"
 	CheckDNS 8.8.4.4 "Google Public DNS (secondary)"
 	CheckDNS 4.2.2.1 "Level3 one"
@@ -41,10 +42,13 @@ try {
 	CheckDNS 208.67.220.220 "OpenDNS Basic (secondary)"
 	CheckDNS 208.67.222.123 "OpenDNS Family Shield (primary)"
 	CheckDNS 208.67.220.123 "OpenDNS Family Shield (secondary)"
+	CheckDNS 94.247.43.254 "OpenNIC"
 	CheckDNS 9.9.9.9 "Quad9 with malware blocklist, with DNSSEC"
 	CheckDNS 9.9.9.10 "Quad9, no malware blocklist, no DNSSEC"
 	CheckDNS 9.9.9.11 "Quad9, with malware blocklist, with DNSSEC, with EDNS"
 	CheckDNS 9.9.9.12 "Quad9, with malware blocklist, with DNSSEC, NXDOMAIN only"
+	CheckDNS 64.6.64.6 "Verisign Public DNS (primary)"
+	CheckDNS 64.6.65.6 "Verisign Public DNS (secondary)"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
