b185f9b1453f818b76161e26e9c968dbe98cb94d
Markus Fleschutz <markus@fleschutz.de>
2022-05-07 15:24:52 +0200

    Add print-image.ps1

---
+++ Scripts/print-image.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+	Prints an image
+.DESCRIPTION
+	This PowerShell script shows the printer dialogue to print the given image file.
+.PARAMETER Path
+	Specifies the path to the image file
+.EXAMPLE
+	PS> ./print-image dog.jpg
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param($Path = "")
+
+try {
+	if ("$Path" -eq "") {
+		$Path = Read-Host "Enter path to the image file"
+	}
+	Start-Process -filepath $Path -verb print
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
