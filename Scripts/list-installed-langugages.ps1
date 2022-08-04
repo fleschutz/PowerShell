2310b53e6a173218f74061dc7febfd7e36177f48
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-08-04 15:45:33 +0200

    Add list-installed-languages.ps1

---
+++ Scripts/list-installed-langugages.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+	Lists the installed languages
+.DESCRIPTION
+	This PowerShell script lists the installed languages.
+.EXAMPLE
+	PS> ./list-installed-languages
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+function ListInstalledLanguages { 
+	$List = Get-WinUserLanguageList
+	foreach ($Item in $List) {
+		New-Object PSObject -property @{ 'Tag' = "$($Item.LanguageTag)"; 'Autonym' = "$($Item.Autonym)"; 'English' = "$($Item.EnglishName)"; 'Spellchecking' = "$($Item.Spellchecking)"; 'Handwriting' = "$($Item.Handwriting)" }
+	}
+}
+
+try {
+	ListInstalledLanguages | Format-Table -property Tag,Autonym,English,Spellchecking,Handwriting
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
