ba03ac8412a9af084526a3e77fd965f86d5fa986
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-08-10 15:16:54 +0200

    Update pull-repo.ps1

--- Scripts/pull-repo.ps1
+++ Scripts/pull-repo.ps1
@@ -1,6 +1,6 @@
 ﻿<#
 .SYNOPSIS
-	Pulls updates for a Git repo
+	Pulls Git repository updates 
 .DESCRIPTION
 	This PowerShell script pulls updates for a local Git repository (including submodules).
 .PARAMETER RepoDir
@@ -18,27 +18,27 @@ param([string]$RepoDir = "$PWD")
 try {
 	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	"⏳ Step 1/3: Checking requirements... "
-	$null = (git --version)
+	"⏳ Step 1/4 - Searching for Git executable..."
+	& git --version
 	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 
+	"⏳ Step 2/4 - Checking Git repository..."
 	if (-not(Test-Path "$RepoDir" -pathType container)) { throw "Can't access folder: $RepoDir" }
 
 	$Result = (git -C "$RepoDir" status)
 	if ("$Result" -match "HEAD detached at ") { throw "Not on a branch, so nothing to pull (in detached HEAD state)" }
 
-	"⏳ Step 2/3: Pulling updates... "
+	"⏳ Step 3/4 - Pulling updates..."
 	& git -C "$RepoDir" pull --recurse-submodules --jobs=4
 	if ($lastExitCode -ne "0") { throw "'git pull' failed with exit code $lastExitCode" }
 
-	"⏳ Step 3/3: Updating submodules... "
+	"⏳ Step 4/4 - Updating submodules... "
 	& git -C "$RepoDir" submodule update --init --recursive
 	if ($lastExitCode -ne "0") { throw "'git submodule update' failed with exit code $lastExitCode" }
 
 	$RepoDirName = (Get-Item "$RepoDir").Name
 	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
 	"✔️ pulled updates for 📂$RepoDirName repo in $Elapsed sec"
-
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
