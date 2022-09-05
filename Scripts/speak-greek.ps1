4fbc4dcccb21cf301adc10a44270d95853bfbad9
Markus Fleschutz <markus@fleschutz.de>
2022-09-05 19:21:49 +0200

    Add speak-greek.ps1

---
+++ Scripts/speak-greek.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Speaks text with a Greek text-to-speech voice
+.DESCRIPTION
+	This PowerShell script speaks the given text with a Greek text-to-speech (TTS) voice.
+.PARAMETER text
+	Specifies the text to speak
+.EXAMPLE
+	PS> ./speak-greek "γεια"
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$text = "")
+
+try {
+	if ($text -eq "") { $text = read-host "Enter the Greek text to speak" }
+
+	$TTSVoice = New-Object -ComObject SAPI.SPVoice
+	foreach ($Voice in $TTSVoice.GetVoices()) {
+		if ($Voice.GetDescription() -like "*- Greek*") { 
+			$TTSVoice.Voice = $Voice
+			[void]$TTSVoice.Speak($text)
+			exit 0 # success
+		}
+	}
+	throw "No Greek voice for text-to-speech (TTS) found - please install one"
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
