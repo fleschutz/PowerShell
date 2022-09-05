fe1a4f79f2efdf5efbee2e84bc63df81a71cbeb2
Markus Fleschutz <markus@fleschutz.de>
2022-09-05 19:25:03 +0200

    Add speak-arabic.ps1

---
+++ Scripts/speak-arabic.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Speaks text with an Arabic text-to-speech voice
+.DESCRIPTION
+	This PowerShell script speaks the given text with an Arabic text-to-speech (TTS) voice.
+.PARAMETER text
+	Specifies the text to speak
+.EXAMPLE
+	PS> ./speak-arabic "أهلاً"
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$text = "")
+
+try {
+	if ($text -eq "") { $text = read-host "Enter the Arabic text to speak" }
+
+	$TTSVoice = New-Object -ComObject SAPI.SPVoice
+	foreach ($Voice in $TTSVoice.GetVoices()) {
+		if ($Voice.GetDescription() -like "*- Arabic*") { 
+			$TTSVoice.Voice = $Voice
+			[void]$TTSVoice.Speak($text)
+			exit 0 # success
+		}
+	}
+	throw "No Arabic voice for text-to-speech (TTS) found - please install one"
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
