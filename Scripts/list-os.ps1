eb51263d84c5e83a0094881f14e33f14390be266
Markus Fleschutz <markus@fleschutz.de>
2022-09-25 18:04:16 +0200

    Add list-os.ps1

---
+++ Scripts/list-os.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Lists the operating system version
+.DESCRIPTION
+	This PowerShell script lists the exact operating system version.
+.EXAMPLE
+	PS> ./list-os
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	if ($IsLinux) {
+		"🐧(uname -sr)"
+	} else {
+		$OS = Get-WmiObject -class Win32_OperatingSystem
+		$OSname = $OS.Caption
+		$OSarchitecture = $OS.OSArchitecture
+		$OSversion = $OS.Version
+		"🌐$OSname for $OSarchitecture version $OSversion"
+	}
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
