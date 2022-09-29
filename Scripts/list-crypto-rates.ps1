50f80e57c13b0ce27250a0434d3fdcf4af53213a
Markus Fleschutz <markus@fleschutz.de>
2022-09-29 14:42:49 +0200

    Update list-crypto-rates.ps1

--- Scripts/list-crypto-rates.ps1
+++ Scripts/list-crypto-rates.ps1
@@ -1,8 +1,8 @@
 ﻿<#
 .SYNOPSIS
-	Lists the current crypto exchange rates
+	Lists crypto exchange rates
 .DESCRIPTION
-	This PowerShell script lists the current crypto exchange rates.
+	This PowerShell script queries and lists the current crypto exchange rates from cryptocompare.com.
 .EXAMPLE
 	PS> ./list-crypto-rates
 .LINK
@@ -12,39 +12,38 @@
 #>
 
 function ListCryptoRate { param([string]$Symbol, [string]$Name)
-	$Rates = (invoke-webRequest -uri "https://min-api.cryptocompare.com/data/price?fsym=$Symbol&tsyms=USD,EUR,RUB,CNY" -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
-	new-object PSObject -property @{ 'Cryptocurrency' = "1 $Name ($Symbol) ="; 'USD' = "$($Rates.USD)"; 'EUR' = "$($Rates.EUR)"; 'RUB' = "$($Rates.RUB)"; 'CNY' = "$($Rates.CNY)" }
+	$Rates = (Invoke-WebRequest -URI "https://min-api.cryptocompare.com/data/price?fsym=$Symbol&tsyms=USD,EUR,RUB,CNY" -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
+	New-Object PSObject -property @{ 'Cryptocurrency' = "1 $Name ($Symbol) ="; 'USD' = "$($Rates.USD)"; 'EUR' = "$($Rates.EUR)"; 'RUB' = "$($Rates.RUB)"; 'CNY' = "$($Rates.CNY)" }
 }
 
 function ListCryptoRates { 
-	ListCryptoRate BTC "Bitcoin"
-	ListCryptoRate ETH "Ethereum"
-	ListCryptoRate ADA "Cardano"
-	ListCryptoRate BNB "Binance Coin"
-	ListCryptoRate USDT "Tether"
-	ListCryptoRate XRP "XRP"
-	ListCryptoRate DOGE "Dogecoin"
-	ListCryptoRate USDC "USD Coin"
-	ListCryptoRate DOT "Polkadot"
-	ListCryptoRate SOL "Solana"
-	ListCryptoRate UNI "Uniswap"
-	ListCryptoRate BUSD "Binance USD"
-	ListCryptoRate BCH "Bitcoin Cash"
-	ListCryptoRate LTC "Litecoin"
-	ListCryptoRate LINK "Chainlink"
-	ListCryptoRate LUNA "Terra"
-	ListCryptoRate ICP "Internet Computer"
-	ListCryptoRate WBTC "Wrapped Bitcoin"
+	ListCryptoRate BTC   "Bitcoin"
+	ListCryptoRate ETH   "Ethereum"
+	ListCryptoRate ADA   "Cardano"
+	ListCryptoRate BNB   "Binance Coin"
+	ListCryptoRate USDT  "Tether"
+	ListCryptoRate XRP   "XRP"
+	ListCryptoRate DOGE  "Dogecoin"
+	ListCryptoRate USDC  "USD Coin"
+	ListCryptoRate DOT   "Polkadot"
+	ListCryptoRate SOL   "Solana"
+	ListCryptoRate UNI   "Uniswap"
+	ListCryptoRate BUSD  "Binance USD"
+	ListCryptoRate BCH   "Bitcoin Cash"
+	ListCryptoRate LTC   "Litecoin"
+	ListCryptoRate LINK  "Chainlink"
+	ListCryptoRate LUNA  "Terra"
+	ListCryptoRate ICP   "Internet Computer"
+	ListCryptoRate WBTC  "Wrapped Bitcoin"
 	ListCryptoRate MATIC "Polygon"
-	ListCryptoRate XLM "Stellar"
+	ListCryptoRate XLM   "Stellar"
 }
 
 try {
-	""
-	"Current Crypto Exchange Rates (source: cryptocompare.com)"
-	"============================="
-
-	ListCryptoRates | format-table -property @{e='Cryptocurrency';width=28},USD,EUR,RUB,CNY
+	" "
+	"Current Crypto Exchange Rates by cryptocompare.com"
+	"=================================================="
+	ListCryptoRates | Format-Table -property @{e='Cryptocurrency';width=28},USD,EUR,RUB,CNY
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
