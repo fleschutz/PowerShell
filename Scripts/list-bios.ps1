da9c58db56647074fe68b02d7923f1f2549ef44a
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-10-04 14:55:09 +0200

    Add list-bios.ps1

---
+++ Scripts/list-bios.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Lists BIOS details
+.DESCRIPTION
+	This PowerShell script lists the BIOS details.
+.EXAMPLE
+	PS> ./list-bios
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	Get-CimInstance -ClassName Win32_BIOS
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
