742c44d8aa94318f3ca63e6676478a1be93cfe5b
Markus Fleschutz <markus@fleschutz.de>
2022-10-14 17:04:44 +0200

    Update check-ram.ps1

--- Scripts/check-ram.ps1
+++ Scripts/check-ram.ps1
@@ -56,7 +56,7 @@ try {
 			} else {
 				$Location = $Bank.DeviceLocator
 			}
-			"✅ $($Capacity)GB $Type ($($Speed)MHz, $($Voltage)V, $Vendor) at $Location."
+			"✅ $($Capacity)GB $Type ($($Speed)MHz, $($Voltage)V) by $Vendor at $Location."
 		}
 	}
 	exit 0 # success
