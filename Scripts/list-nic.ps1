7754be79952a4af19d76d2ea726a4b0d678df357
Markus Fleschutz <markus@fleschutz.de>
2022-10-17 19:55:53 +0200

    Update list-nic.ps1

--- Scripts/list-nic.ps1
+++ Scripts/list-nic.ps1
@@ -12,7 +12,7 @@
 #>
 
 try {
-	Get-WmiObject -Class Win32_NetworkAdapterConfiguration
+	Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Format-Table -property ServiceName,Description,IPAddress,DHCPEnabled -AutoSize
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
