3aefcfd2a07a1b67bc9b7022edf67a73853c511d
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-10-25 15:58:33 +0200

    Add list-user-accounts.ps1

---
+++ Scripts/list-user-accounts.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Lists user accounts
+.DESCRIPTION
+	This PowerShell script lists the user accounts on the local computer.
+.EXAMPLE
+	PS> ./list-user-accounts
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	net user
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
