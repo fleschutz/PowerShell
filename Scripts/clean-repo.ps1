d494d50d91c0da1f76c626cf974fff37042cb7ef
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-10-26 12:15:16 +0200

    Update clean-repo.ps1

--- Scripts/clean-repo.ps1
+++ Scripts/clean-repo.ps1
@@ -19,15 +19,15 @@ param([string]$RepoDir = "$PWD")
 try {
 	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	Write-Host "⏳ Step 1/4 - Searching for Git executable...  " -noNewline
+	Write-Host "⏳ (1/4) Searching for Git executable...  " -noNewline
 	& git --version
 	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 
 	$RepoDirName = (Get-Item "$RepoDir").Name
-	"⏳ Step 2/4 - Checking folder 📂$RepoDirName..."
+	"⏳ (2/4) Checking folder 📂$RepoDirName..."
 	if (-not(Test-Path "$RepoDir" -pathType container)) { throw "Can't access folder '$RepoDir' - maybe a typo or missing folder permissions?" }
 
-	"⏳ Step 3/4 - Cleaning repository from untracked files..."
+	"⏳ (3/4) Removing untracked files in repository..."
 	& git -C "$RepoDir" clean -xfd -f # to delete all untracked files in the main repo
 	if ($lastExitCode -ne "0") {
 		"'git clean' failed with exit code $lastExitCode, retrying once..."
@@ -35,7 +35,7 @@ try {
 		if ($lastExitCode -ne "0") { throw "'git clean' failed with exit code $lastExitCode" }
 	}
 
-	"⏳ Step 4/4 - Cleaning submodules from untracked files..."
+	"⏳ (4/4) Removing untracked files in submodules..."
 	& git -C "$RepoDir" submodule foreach --recursive git clean -xfd -f # to delete all untracked files in the submodules
 	if ($lastExitCode -ne "0") { throw "'git clean' in the submodules failed with exit code $lastExitCode" }
 
