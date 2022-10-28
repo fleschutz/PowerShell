8022c28fc768055234e1624758aa2eeb330cd37d
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-10-28 12:39:58 +0200

    Update check-ping.ps1

--- Scripts/check-ping.ps1
+++ Scripts/check-ping.ps1
@@ -1,14 +1,13 @@
 ﻿<#
 .SYNOPSIS
-	Checks the ping latency to selected Internet hosts
+	Checks the ping latency 
 .DESCRIPTION
-	This PowerShell script checks the ping latency from the local computer to selected Internet hosts.
-	The hosts by default are: amazon.com,bing.com,cnn.com,dropbox.com,facebook.com,google.com,live.com,twitter.com,youtube.com
+	This PowerShell script checks the ping latency from the local computer to some Internet hosts.
 .PARAMETER hosts
-	Specifies the hosts to check (separated by comma)
+	Specifies the hosts to check, seperated by comma (default is: amazon.com,bing.com,cnn.com,dropbox.com,facebook.com,google.com,live.com,twitter.com,youtube.com)
 .EXAMPLE
 	PS> ./check-ping
-	✅ Ping is 25ms average (13ms min, 109ms max).
+	✅ Ping is 25ms average, 13ms min, 109ms max.
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -18,19 +17,14 @@
 param([string]$hosts = "amazon.com,bing.com,cnn.com,dropbox.com,facebook.com,google.com,live.com,twitter.com,youtube.com")
 
 try {
-	write-progress "Sending pings to $hosts..."
+	Write-Progress "⏳ Pinging $hosts..."
 	$HostsArray = $hosts.Split(",")
 	$Pings = Test-Connection -count 1 -computerName $HostsArray
 
 	[int]$Min = 9999999
-	[int]$Max = 0
-	[int]$Avg = 0
+	[int]$Max = [int]$Avg = 0
 	foreach($Ping in $Pings) {
-		if ($IsLinux) {
-			[int]$Latency = $Ping.latency
-		} else {
-			[int]$Latency = $Ping.ResponseTime
-		}
+		if ($IsLinux) {	[int]$Latency = $Ping.latency } else { [int]$Latency = $Ping.ResponseTime }
 		if ($Latency -lt $Min) { $Min = $Latency }
 		if ($Latency -gt $Max) { $Max = $Latency }
 		$Avg += $Latency
