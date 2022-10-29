b5d19fdde27384e6b9f3c95106ff76f72872c298
Markus Fleschutz <markus@fleschutz.de>
2022-10-29 18:37:41 +0200

    Update start-ipfs-server.ps1 and publish-to-ipfs.ps1

--- Scripts/start-ipfs-server.ps1
+++ Scripts/start-ipfs-server.ps1
@@ -14,14 +14,13 @@
 try {
 	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	"⏳ Step 1/5: Searching for IPFS executable..."
+	Write-Host "⏳ (1/5) Searching for IPFS executable...  " -noNewline
 	& ipfs --version
 	if ($lastExitCode -ne "0") { throw "Can't execute 'ipfs' - make sure IPFS is installed and available" }
-	""
-	"⏳ Step 2/5: Initializing IPFS with server profile..."
+	"⏳ (2/5) Initializing IPFS with server profile..."
 	& ipfs init --profile server
 
-	"⏳ Step 3/5: Configuring IPFS..."
+	"⏳ (3/5) Configuring IPFS..."
 	& ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 	if ($lastExitCode -ne "0") { throw "'ipfs config Addresses.API' failed (exit code $lastExitCode)" }
 
@@ -41,11 +40,10 @@ try {
 	& ipfs config --json Swarm.Transports.Network.Websocket 'false'
 	if ($lastExitCode -ne "0") { throw "'ipfs config Swarm.Transports.Network.Websocket' failed (exit code $lastExitCode)" }
 	""
-	"⏳ Step 4/5: Increasing UDP receive buffer size..."
+	Write-Host "⏳ (4/5) Increasing UDP receive buffer size..." -noNewline
 	& sudo sysctl -w net.core.rmem_max=2500000
 	if ($lastExitCode -ne "0") { throw "'sysctl' failed (exit code $lastExitCode)" }
-	""
-	"⏳ Step 5/5: Starting IPFS daemon..."
+	"⏳ (5/5) Starting IPFS daemon..."
 #	Start-Process nohup 'ipfs daemon'
 	Start-Process nohup -ArgumentList 'ipfs','daemon' -RedirectStandardOutput "$HOME/console.out" -RedirectStandardError "$HOME/console.err"
 
