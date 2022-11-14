0d99de3988f0557a99b61b5e73c465c8a591884a
Markus Fleschutz <markus.fleschutz@gmail.com>
2022-11-14 21:40:35 +0100

    Update list-weather.ps1

--- Scripts/list-weather.ps1
+++ Scripts/list-weather.ps1
@@ -17,14 +17,15 @@ param([string]$Location = "") # empty means determine automatically
 
 function Describe { param([string]$Desc)
 	switch($Desc) {
-	"Clear"			{ return "☀️clear       " }
-	"Cloudy"		{ return "☁️cloudy      " }
-	"Light drizzle"		{ return "💧light drizzle" }
-	"Light rain shower"	{ return "💧light rain   " }
+	"Clear"			{ return "🌙 clear       " }
+	"Cloudy"			{ return "☁️ cloudy      " }
+	"Light drizzle"		{ return "💧 light drizzle" }
+	"Light rain shower"	{ return "💧 light rain   " }
 	"Mist"			{ return "🌫 misty      " }
-	"Overcast"		{ return "☁️overcast    " }
-	"Partly cloudy"		{ return "☁️partly cloudy" }
-	"Sunny"			{ return "☀️sunny       " }
+	"Overcast"			{ return "☁️ overcast    " }
+	"Partly cloudy"		{ return "☁️ partly cloudy" }
+	"Patchy rain possible"  { return "💧 patchy rain possible" }
+	"Sunny"			{ return "☀️ sunny       " }
 	default			{ return "$Desc" }
 	}
 }
