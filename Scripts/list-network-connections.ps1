fae30d1d3f4adb853bd3e790fb84152c310ccc15
Markus Fleschutz <markus.fleschutz@atcsim.de>
2022-11-22 08:40:38 +0100

    Add list-network-connections.ps1

---
+++ Scripts/list-network-connections.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Lists network connections
+.DESCRIPTION
+	This PowerShell script lists all active network connections on the local computer.
+.EXAMPLE
+	PS> ./list-network-connections
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	& netstat -n
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
