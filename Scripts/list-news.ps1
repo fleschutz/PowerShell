f23f3518b6ac0feae1500a5538765c900d4a62b3
Markus Fleschutz <markus@fleschutz.de>
2022-11-26 15:21:57 +0100

    Update list-news.ps1

--- Scripts/list-news.ps1
+++ Scripts/list-news.ps1
@@ -5,8 +5,10 @@
 	This PowerShell script lists the latest news by using RSS (Really Simple Syndication) feeds.
 .PARAMETER RSS_URL
 	Specifies the URL to the RSS feed (Yahoo News by default)
-.PARAMETER MaxCount
-	Specifies the number of lines to list (20 by default)
+.PARAMETER MaxLines
+	Specifies the maximum number of lines to list (24 by default)
+.PARAMETER Speed
+        Specifies the speed to write the text (10 ms by default)
 .EXAMPLE
 	PS> ./list-news
 .LINK
@@ -15,18 +17,18 @@
 	Author: Markus Fleschutz | License: CC0
 #>
 
-param([string]$RSS_URL = "https://yahoo.com/news/rss/world", [int]$MaxCount = 20)
+param([string]$RSS_URL = "https://yahoo.com/news/rss/world", [int]$MaxLines = 24, [int]$Speed = 10)
 
 try {
 	[xml]$Content = (Invoke-WebRequest -URI $RSS_URL -useBasicParsing).Content
 	[int]$Count = 1
 	foreach ($Item in $Content.rss.channel.item) {
-		& "$PSScriptRoot/write-typewriter.ps1" "→ $($Item.title)" 20 # ms speed
-		if ($Count++ -eq $MaxCount) { break }
+		& "$PSScriptRoot/write-typewriter.ps1" "→ $($Item.title)" $Speed
+		if ($Count++ -eq $MaxLines) { break }
 	}
 	$Source = $Content.rss.channel.title
 	$Date = $Content.rss.channel.pubDate
-	"  By *$($Source)* as of $Date"
+	"     (by *$($Source)* as of $Date)"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
