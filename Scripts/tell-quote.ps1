6b5e29d501650196ee78082aa61b13b269a35a0c
Markus Fleschutz <markus@fleschutz.de>
2022-12-02 09:16:16 +0100

    Update tell-quote.ps1

--- Scripts/tell-quote.ps1
+++ Scripts/tell-quote.ps1
@@ -18,9 +18,8 @@ try {
 	$Index = [int]$Generator.next(0, $Table.Count - 1)
 	$Quote = $Table[$Index].Quote
 	$Author = $Table[$Index].Author
-	$Reply = "$Quote (by $Author)"
 
-	& "$PSScriptRoot/speak-english.ps1" "$Reply"
+	& "$PSScriptRoot/speak-english.ps1" "$Quote (by $Author)"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
