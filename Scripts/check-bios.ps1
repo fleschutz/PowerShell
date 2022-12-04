aed29e2154b6ae642a80ba8e5ca6b1553674e402
Markus Fleschutz <markus.fleschutz@gmail.com>
2022-12-04 10:03:10 +0100

    Update check-bios.ps1

--- Scripts/check-bios.ps1
+++ Scripts/check-bios.ps1
@@ -5,6 +5,7 @@
 	This PowerShell script queries BIOS details and prints it.
 .EXAMPLE
 	PS> ./check-bios
+	✅ BIOS V1.10 by INSYDE Corp. (S/N NXA82EV0EBB07600, version ACRSYS - 2)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -12,15 +13,16 @@
 #>
 
 try {
+	Write-Progress "Querying BIOS details..."
 	if ($IsLinux) {
 		# TODO
 	} else {
 		$BIOS = Get-CimInstance -ClassName Win32_BIOS
 		$Manufacturer = $BIOS.Manufacturer
 		$Model = $BIOS.Name
-		$SerialNumber = $BIOS.SerialNumber
+		$Serial = $BIOS.SerialNumber
 		$Version = $BIOS.Version
-		"✅ BIOS $Manufacturer $($Model): S/N $SerialNumber, version $Version"
+		"✅ BIOS $Model by $Manufacturer (S/N $Serial, version $Version)"
 	}
 	exit 0 # success
 } catch {
