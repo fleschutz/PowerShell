4ef2f3e3844f54e24c6df7e5ab0eb9266c516754
Markus Fleschutz <markus.fleschutz@gmail.com>
2022-12-04 11:31:38 +0100

    Update sync-repo.ps1

--- Scripts/sync-repo.ps1
+++ Scripts/sync-repo.ps1
@@ -1,6 +1,6 @@
 ﻿<#
 .SYNOPSIS
-	Synchronizes a Git repository 
+	Syncs a Git repo 
 .DESCRIPTION
 	This PowerShell script synchronizes a Git repository by push & pull (including submodules).
 .PARAMETER RepoDir
@@ -16,25 +16,26 @@
 param([string]$RepoDir = "$PWD")
 
 try {
-	 $StopWatch = [system.diagnostics.stopwatch]::startNew()
+	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	"⏳ Step 1/3: Checking requirements..."
-	if (-not(test-path "$RepoDir" -pathType container)) { throw "Can't access directory: $RepoDir" }
+	Write-Host "⏳ (1/4) Searching for Git executable...  " -noNewline
+ 	& git --version
+      if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 
-	& git --version
-	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
+	$RepoDirName = (Get-Item "$RepoDir").Name
+      Write-Host "⏳ (2/4) Checking Git repository...       📂$RepoDirName"
+      if (!(Test-Path "$RepoDir" -pathType container)) { throw "Can't access folder: $RepoDir" }
 
-	"⏳ Step 2/3: Pushing local updates..."
+	Write-Host "⏳ (3/4) Pushing local updates...         " -noNewline
 	& git -C "$RepoDir" push
 	if ($lastExitCode -ne "0") { throw "'git push' failed" }
 
-	"⏳ Step 3/3: Pulling remote updates..."
-	& git -C "$RepoDir" pull --all --recurse-submodules --jobs=4
+	Write-Host "⏳ (4/4) Pulling remote updates...        " -noNewline
+	& git -C "$RepoDir" pull --all --recurse-submodules
 	if ($lastExitCode -ne "0") { throw "'git pull' failed" }
 
-	$RepoDirName = (Get-Item "$RepoDir").Name
 	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ synchronized Git repo 📂$RepoDirName in $Elapsed sec"
+	"✔️ synchronized 📂$RepoDirName repository in $Elapsed sec."
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
