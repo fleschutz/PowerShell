91ba386b39fab236760696cd4f749238cd8661c2
Markus Fleschutz <markus.fleschutz@gmail.com>
2022-12-04 11:49:19 +0100

    Update pull-repo.ps1

--- Scripts/pull-repo.ps1
+++ Scripts/pull-repo.ps1
@@ -6,7 +6,7 @@
 .PARAMETER RepoDir
 	Specifies the file path to the local Git repository (default is working directory)
 .EXAMPLE
-	PS> ./pull-repo 
+	PS> ./pull-repo C:\MyRepo
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -23,17 +23,17 @@ try {
 	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 
 	$RepoDirName = (Get-Item "$RepoDir").Name
-	"⏳ (2/4) Checking Git repository 📂$RepoDirName... "
+	Write-Host "⏳ (2/4) Checking Git repository...        📂$RepoDirName"
 	if (-not(Test-Path "$RepoDir" -pathType container)) { throw "Can't access folder: $RepoDir" }
 
 	$Result = (git -C "$RepoDir" status)
 	if ("$Result" -match "HEAD detached at ") { throw "Currently in detached HEAD state (not on a branch!), so nothing to pull" }
 
-	"⏳ (3/4) Pulling updates..."
+	Write-Host "⏳ (3/4) Pulling updates...                " -noNewline
 	& git -C "$RepoDir" pull --recurse-submodules=yes
 	if ($lastExitCode -ne "0") { throw "'git pull' failed with exit code $lastExitCode" }
 
-	"⏳ (4/4) Updating submodules... "
+	Write-Host "⏳ (4/4) Updating submodules... "
 	& git -C "$RepoDir" submodule update --init --recursive
 	if ($lastExitCode -ne "0") { throw "'git submodule update' failed with exit code $lastExitCode" }
 
