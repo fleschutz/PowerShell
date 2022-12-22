42ad2bef59cc5afa376e86157db59b27ca576cf6
Markus Fleschutz <markus.fleschutz@gmail.com>
2022-12-22 17:13:53 +0100

    Update introduce-powershell.ps1

--- Scripts/introduce-powershell.ps1
+++ Scripts/introduce-powershell.ps1
@@ -12,25 +12,30 @@
 #>
 
 try {
-	Clear
-	& "$PSScriptRoot/write-big.ps1" "     WELCOME"
+	Clear-Host
+	""
+	& "$PSScriptRoot/write-animated.ps1" "Welcome to"
+	""
+	& "$PSScriptRoot/write-big.ps1" "     POWERSHELL"
+	""
+	& "$PSScriptRoot/write-animated.ps1" "Make the command-line great again"
+	""
+	""
 	""
 	$Version = $PSVersionTable.PSVersion
 	$Edition = $PSVersionTable.PSEdition
 	$NumModules = (Get-Module).Count
-	$NumCmdlets = (Get-Command -Command-Type cmdlet).Count
 	$NumAliases = (Get-Alias).Count
-	$Details = "to POWERSHELL $Version ($Edition edition) with $NumModules modules, $NumCmdlets cmdlets and $NumAliases aliases"
-	& "$PSScriptRoot/write-animated.ps1" "$Details"
+	$Details = "PowerShell $Version ($Edition edition) with $NumModules modules and $NumAliases aliases"
+	& "$PSScriptRoot/write-typewriter.ps1" ">>> This is $Details"
 	""
+	& "$PSScriptRoot/write-typewriter.ps1" ">>> Want to learn PowerShell? See the tutorials at: https://www.guru99.com/powershell-tutorial.html" 25
 	""
-	& "$PSScriptRoot/write-typewriter.ps1" "* Want to learn PowerShell? See the tutorials at: https://www.guru99.com/powershell-tutorial.html" 25
+	& "$PSScriptRoot/write-typewriter.ps1" ">>> Need docs? See the official documentation at: https://docs.microsoft.com/en-us/powershell" 25
 	""
-	& "$PSScriptRoot/write-typewriter.ps1" "* Need docs? See the official documentation at: https://docs.microsoft.com/en-us/powershell" 25
+	& "$PSScriptRoot/write-typewriter.ps1" ">>> Take a look under the hood and visit the PowerShell Github repository at: https://github.com/PowerShell/PowerShell" 25
 	""
-	& "$PSScriptRoot/write-typewriter.ps1" "* Take a look under the hood and visit the PowerShell Github repository at: https://github.com/PowerShell/PowerShell" 25
-	""
-	& "$PSScriptRoot/write-typewriter.ps1" "* Want examples? See the mega collection of PowerShell scripts at: https://github.com/fleschutz/PowerShell" 25
+	& "$PSScriptRoot/write-typewriter.ps1" ">>> Want examples? See the mega collection of PowerShell scripts at: https://github.com/fleschutz/PowerShell" 25
 	""
 
 	& "$PSScriptRoot/write-typewriter.ps1" "NOTE: use <Ctrl> + <Click> to follow the links above and HAVE FUN!" 20
