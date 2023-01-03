0e845922067c885f1657dab016a0d6e0810e6410
Markus Fleschutz <markus@fleschutz.de>
2023-01-03 10:20:51 +0100

    Added uninstall-all-apps.ps1

---
+++ Scripts/uninstall-all-apps.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Uninstalls all apps
+.DESCRIPTION
+	This PowerShell script uninstalls all applications from the local computer. Useful for de-bloating Windows to clean up a PC quickly for an industrial use case without any security risks.
+.EXAMPLE
+	PS> ./uninstall-all-apps
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	"Uninstalling all apps, please wait... (ignore any error messages about packages that can't be removed, it's fine)"
+
+	Get-AppxPackage | Remove-AppxPackage
+	
+	"Uninstalled all applications, your PC is clean now."
+	exit 0 # success
+} catch {
+	"Sorry: $($Error[0])"
+	exit 1
+}
