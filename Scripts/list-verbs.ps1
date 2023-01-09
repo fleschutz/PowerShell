afb0e2b9e4dc02b8647654b650f99bd3e6dcfe73
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-01-09 13:55:16 +0100

    Add list-verbs.ps1

---
+++ Scripts/list-verbs.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+	Lists verbs
+.DESCRIPTION
+	This PowerShell script lists all allowed verbs.
+.EXAMPLE
+	PS> ./list-verbs
+
+	Verb        Group
+	----        -----
+	Add         Common            
+	...
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	Get-Verb | Sort-Object -Property Verb
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
