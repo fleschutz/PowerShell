78d9fb668b017d34935a23b65aad522b4b713209
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-01-18 12:47:54 +0100

    Add list-window-titles.ps1

---
+++ Scripts/list-window-titles.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Lists window titles
+.DESCRIPTION
+	This PowerShell script list all main window titles and displays them in a table.
+.EXAMPLE
+	PS> ./list-window-titles
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	Get-Process | Where-Object {$_.mainWindowTitle} | Format-Table Id,Name,mainWindowtitle -AutoSize
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
