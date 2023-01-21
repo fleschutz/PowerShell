2342f245661653d4db3ecb4c1bd3c7f6325d2e6c
Markus Fleschutz <markus@fleschutz.de>
2023-01-21 12:36:42 +0100

    Add speak-turkish.ps1

---
+++ Scripts/speak-turkish.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Speaks text in Turkish
+.DESCRIPTION
+	This PowerShell script speaks the given text with a Turkish text-to-speech (TTS) voice.
+.PARAMETER text
+	Specifies the text to speak
+.EXAMPLE
+	PS> ./speak-turkish Merhaba
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$text = "")
+
+try {
+	if ($text -eq "") { $text = read-host "Enter the Turkish text to speak" }
+
+	$TTS = New-Object -ComObject SAPI.SPVoice
+	foreach ($Voice in $TTS.GetVoices()) {
+		if ($Voice.GetDescription() -like "*- Turkish*") { 
+			$TTS.Voice = $Voice
+			[void]$TTS.Speak($text)
+			exit 0 # success
+		}
+	}
+	throw "No Turkish voice for text-to-speech (TTS) found - please install one"
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
