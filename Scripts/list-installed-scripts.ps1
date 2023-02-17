8fd6cdaf3bbe2dbc7abc30b880e3d2d2db274778
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-02-17 13:03:55 +0100

    Add list-installed-scripts.ps1

---
+++ Scripts/list-installed-scripts.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	Lists all installed PowerShell scripts 
+.DESCRIPTION
+	This PowerShell script lists all installed PowerShell scripts.
+.EXAMPLE
+	PS> ./list-installed-scripts
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+Get-InstalledScript
+exit # success
