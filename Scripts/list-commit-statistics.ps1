e035500b5437780070f89f2888eb110105fdc8b6
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-03-06 13:49:00 +0100

    Update list-commit-statistics.ps1

--- Scripts/list-commit-statistics.ps1
+++ Scripts/list-commit-statistics.ps1
@@ -1,14 +1,17 @@
 ﻿<#
 .SYNOPSIS
-	Lists the commit statistics of a Git repository
+	Lists the Git commit statistics
 .DESCRIPTION
 	This PowerShell script lists the commit statistics of a Git repository.
 .PARAMETER RepoDir
 	Specifies the path to the Git repository.
 .EXAMPLE
 	PS> ./list-commit-statistics
-	    1720 Markus Fleschutz <markus@fleschutz.de>
-	    ...
+  
+        Commits Author
+        ------- ------
+	   2034 Markus Fleschutz <markus@fleschutz.de>
+	...
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -18,22 +21,23 @@
 param([string]$RepoDir = "$PWD")
 
 try {
-	Write-Progress "⏳ Step 1/4 - Searching for Git executable..."
+	Write-Progress "⏳ (1/4) Searching for Git executable..."
 	$null = (git --version)
 	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 
 	$RepoDirName = (Get-Item "$RepoDir").Name
-	Write-Progress "⏳ Step 2/4 - Checking folder 📂$RepoDirName..."
-	if (-not(test-path "$RepoDir" -pathType container)) { throw "Can't access directory: $RepoDir" }
+	Write-Progress "⏳ (2/4) Checking folder 📂$RepoDirName..."
+	if (-not(Test-Path "$RepoDir" -pathType container)) { throw "Can't access directory: $RepoDir" }
 
-	Write-Progress "⏳ Step 3/4 - Fetching updates..."
+	Write-Progress "⏳ (3/4) Fetching updates..."
 	& git -C "$RepoDir" fetch --all --quiet
 	if ($lastExitCode -ne "0") { throw "'git fetch' failed with exit code $lastExitCode" }
 
-	Write-Progress "⏳ Step 4/4 - Querying commits..."
+	Write-Progress "⏳ (4/4) Querying commits..."
 	" "
 	"Commits Author"
 	"------- ------"
+	Write-Progress -completed " "
 	git -C "$RepoDir" shortlog --summary --numbered --email --no-merges
 	if ($lastExitCode -ne "0") { throw "'git shortlog' failed with exit code $lastExitCode" }
 	exit 0 # success
