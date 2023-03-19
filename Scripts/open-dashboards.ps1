3e6bff15b592197931c9e2c051b2a715d6b5a1ca
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-03-19 19:07:59 +0100

    Add open-dashboards.ps1

---
+++ Scripts/open-dashboards.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Open dashboards
+.DESCRIPTION
+	This PowerShell script launches the Web browser with some dashboard websites.
+.EXAMPLE
+	PS> ./open-dashboards
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" https://news.google.com
+& "$PSScriptRoot/open-default-browser.ps1" https://www.windy.com/de/-Wetterradar-radar
+& "$PSScriptRoot/open-default-browser.ps1" https://www.arcgis.com/apps/dashboards/c8af9c5411814584b460cc87cb7c3780
+& "$PSScriptRoot/open-default-browser.ps1" https://radio.garden/visit/kempten/V5Ryr1S4
+& "$PSScriptRoot/open-default-browser.ps1" https://www.foto-webcam.eu/
+& "$PSScriptRoot/open-default-browser.ps1" https://sunnyportal.com/FixedPages/Dashboard.aspx
+exit 0 # success
