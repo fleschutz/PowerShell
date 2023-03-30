762c7a0dd2abd4eedb2f6463ae410ebeb854da65
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-03-30 11:27:50 +0200

    Update open-dashboards.ps1

--- Scripts/open-dashboards.ps1
+++ Scripts/open-dashboards.ps1
@@ -12,20 +12,19 @@
 #>
 
 try {
+	$URLs = @("https://track.toggl.com/timer", "https://news.google.com", "https://www.windy.com/de/-Wetterradar-radar",
+		"https://www.flightradar24.com/27.63,-6.98/3","https://www.arcgis.com/apps/dashboards/c8af9c5411814584b460cc87cb7c3780",
+		"https://www.foto-webcam.eu", "https://zugspitze.panomax.com", "https://livecam.salzburg-airport.com",
+		"https://sunnyportal.com/FixedPages/Dashboard.aspx", "https://covid19.who.int", "https://top10.netflix.com")
+
 	$stopWatch = [system.diagnostics.stopwatch]::startNew()
-	"⏳ Opening 10 Web dashboards in your browser..."
-	& "$PSScriptRoot/open-default-browser.ps1" "https://track.toggl.com/timer"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://news.google.com"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://www.windy.com/de/-Wetterradar-radar"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://www.flightradar24.com/27.63,-6.98/3"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://www.arcgis.com/apps/dashboards/c8af9c5411814584b460cc87cb7c3780"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://radio.garden"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://www.foto-webcam.eu"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://sunnyportal.com/FixedPages/Dashboard.aspx"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://covid19.who.int"
-	& "$PSScriptRoot/open-default-browser.ps1" "https://top10.netflix.com"
+	"⏳ Opening Web dashboards in your browser..."
+	foreach($URL in $URLs) {
+		& "$PSScriptRoot/open-default-browser.ps1" "$URL"
+		Start-Sleep -milliseconds 100
+	}
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
-	"✅ Opened 10 dashboards in $elapsed sec (use switch-tabs.ps1 to switch the tabs automatically)"
+	"✅ Opened 12 Web dashboards in $elapsed sec (use switch-tabs.ps1 to switch the tabs automatically)"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
