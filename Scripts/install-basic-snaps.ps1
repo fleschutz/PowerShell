bba502758a2ff3d2be2fbd1d1516b0ecdac28542
Markus Fleschutz <markus@fleschutz.de>
2023-04-07 16:50:27 +0200

    Update install-basic-snaps.ps1

--- Scripts/install-basic-snaps.ps1
+++ Scripts/install-basic-snaps.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Installs basic Linux snaps 
 .DESCRIPTION
-	This PowerShell script installs 16 basic Linux snaps.
+	This PowerShell script installs 17 basic Linux snaps.
 .EXAMPLE
 	PS> ./install-basic-snaps
 .LINK
@@ -15,7 +15,7 @@ try {
 	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 	if (!$IsLinux) { throw "Only Linux currently support snaps" }
 
-	"⏳ Installing 16 basic snaps (sorted alphabetically)..."
+	"⏳ Installing 17 basic snaps (sorted alphabetically)..."
 	sudo snap install ant
 	sudo snap install audacity
 	sudo snap install bashtop
@@ -23,18 +23,19 @@ try {
 	sudo snap install cmake
 	sudo snap install cups
 	sudo snap install emacs --classic
-	sudo snap install --edge ffmpeg
+	sudo snap install ffmpeg --edge
 	sudo snap install firefox
 	sudo snap install gimp
 	sudo snap install go
 	sudo snap install groovy --classic
 	sudo snap install hugo
+	sudo snap install jenkins --edge --classic 
 	sudo snap install nano --classic
 	sudo snap install nextcloud
 	sudo snap install plexmediaserver
 	
 	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ installed basic snaps in $Elapsed sec"
+	"✔️ Basic Linux snaps installed in $Elapsed sec"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
