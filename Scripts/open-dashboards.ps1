1eba16843f3e4d111db8a22cebc13829a42ef2c9
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-04-07 19:24:35 +0200

    Update open-dashboards.ps1

--- Scripts/open-dashboards.ps1
+++ Scripts/open-dashboards.ps1
@@ -13,13 +13,17 @@
 
 try {
 	$stopWatch = [system.diagnostics.stopwatch]::startNew()
-	Write-Progress "⏳ Opening Web dashboards in your browser..."
+	Write-Progress "⏳ Loading Data/popular-dashboards.csv..."
 	$Table = Import-CSV "$PSScriptRoot/../Data/popular-dashboards.csv"
 	$NumRows = $Table.Length
 	foreach($Row in $Table) {
-		& "$PSScriptRoot/open-default-browser.ps1" "$Row.URL"
+		$Name = $Row.NAME
+		$URL = $Row.URL
+		Write-Progress "⏳ Opening $Name in your browser..."
+		& "$PSScriptRoot/open-default-browser.ps1" "$URL"
 		Start-Sleep -milliseconds 100
 	}
+	Write-Progress -completed "."
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
 	"✅ Opened $NumRows Web dashboards in $elapsed sec (use switch-tabs.ps1 to switch the tabs automatically)"
 	exit 0 # success
