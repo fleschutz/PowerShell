ad4fc918ef8ee3a1f1b783377a7e8c18e3175d3a
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-04-12 11:35:57 +0200

    Update sync-repo.ps1

--- Scripts/sync-repo.ps1
+++ Scripts/sync-repo.ps1
@@ -1,8 +1,8 @@
 ﻿<#
 .SYNOPSIS
-	Syncs a Git repo 
+	Synchronizes a repo 
 .DESCRIPTION
-	This PowerShell script synchronizes a Git repository by push & pull (including submodules).
+	This PowerShell script synchronizes a local Git repository by push and pull (including submodules).
 .PARAMETER RepoDir
 	Specifies the path to the Git repository
 .EXAMPLE
@@ -20,11 +20,11 @@ try {
 
 	Write-Host "⏳ (1/4) Searching for Git executable...  " -noNewline
  	& git --version
-      if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
+ 	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 
+	Write-Host "⏳ (2/4) Checking local repository...     📂$RepoDir"
+	if (!(Test-Path "$RepoDir" -pathType container)) { throw "Can't access folder: $RepoDir" }
 	$RepoDirName = (Get-Item "$RepoDir").Name
-      Write-Host "⏳ (2/4) Checking Git repository...       📂$RepoDirName"
-      if (!(Test-Path "$RepoDir" -pathType container)) { throw "Can't access folder: $RepoDir" }
 
 	Write-Host "⏳ (3/4) Pushing local updates...         " -noNewline
 	& git -C "$RepoDir" push
@@ -35,7 +35,7 @@ try {
 	if ($lastExitCode -ne "0") { throw "'git pull' failed" }
 
 	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ synchronized 📂$RepoDirName repository in $Elapsed sec."
+	"✔️ synchronized repo 📂$RepoDirName in $Elapsed sec"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
