f77735e7414e17abd8ebaccb591f71117cc01361
Markus Fleschutz <markus@fleschutz.de>
2023-04-16 10:29:17 +0200

    Add list-updates.ps1

---
+++ Scripts/list-updates.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Lists software updates
+.DESCRIPTION
+	This PowerShell script lists available updates for the local machine.
+.EXAMPLE
+	PS> ./list-updates
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	if ($IsLinux) {
+		"⏳ (1/4) Querying updates for installed Debian packages..."
+		& sudo apt update
+	} else {
+		Write-Progress "⏳ Querying available updates..."
+		" "
+		& winget upgrade
+		Write-Progress -Completed " "
+	}
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
