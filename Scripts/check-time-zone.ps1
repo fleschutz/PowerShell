037bc52bdf21946345b9d579d8a7d5ee115aa20f
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-05-04 11:18:24 +0200

    Update check-time-zone.ps1

--- Scripts/check-time-zone.ps1
+++ Scripts/check-time-zone.ps1
@@ -2,9 +2,10 @@
 .SYNOPSIS
 	Checks the time zone
 .DESCRIPTION
-	This PowerShell script queries the user's time zone and prints it.
+	This PowerShell script queries the time zone and prints it.
 .EXAMPLE
 	PS> ./check-time-zone
+	✅ 11:13 AM (UTC + 01:00:00 W. Europe Standard Time + 01:00:00 daylight saving time)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -15,8 +16,8 @@ try {
 	[system.threading.thread]::currentThread.currentCulture = [system.globalization.cultureInfo]"en-US"
 	$Time = $((Get-Date).ToShortTimeString())
 	$TZ = (Get-Timezone)
-	if ($TZ.SupportsDaylightSavingTime) { $DST=" & +01:00:00 DST" } else { $DST="" }
-	Write-Host "✅ $Time in $($TZ.Id) (UTC+$($TZ.BaseUtcOffset)$DST)."
+	if ($TZ.SupportsDaylightSavingTime) { $DST="+ 01:00:00 daylight saving time" } else { $DST="" }
+	Write-Host "✅ $Time (UTC + $($TZ.BaseUtcOffset) $($TZ.Id) $DST)"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
