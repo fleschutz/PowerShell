9d4808737aee134091982a79ccf06cb4a6e6cdbc
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-05-22 08:29:33 +0200

    Update open-dashboards.ps1

--- Scripts/open-dashboards.ps1
+++ Scripts/open-dashboards.ps1
@@ -16,17 +16,18 @@ try {
 	Write-Host "⏳ (1/2) Loading Data/popular-dashboards.csv..."
 	$Table = Import-CSV "$PSScriptRoot/../Data/popular-dashboards.csv"
 	$NumRows = $Table.Length
-	Write-Host "⏳ (2/2) Opening dashboards in Web browser: " -noNewLine
+	Write-Host "⏳ (2/2) Launching Web browser with dashboards: " -noNewLine
 	foreach($Row in $Table) {
 		$Name = $Row.NAME
 		$URL = $Row.URL
-		Write-Host "$Name, " -noNewline
+		Write-Host "$Name · " -noNewline
 		& "$PSScriptRoot/open-default-browser.ps1" "$URL"
 		Start-Sleep -milliseconds 100
 	}
 	Write-Host ""
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
-	"✅ Opened $NumRows dashboards in $elapsed sec (use switch-tabs.ps1 to switch the tabs automatically)"
+	"Hint: use switch-tabs.ps1 to switch the browser tabs automatically"
+	"✅ Opened $NumRows dashboards in $elapsed sec"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
