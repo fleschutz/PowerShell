7b942b4feceef89fcf5a0c226f8ee8fbffd50872
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-05-22 10:03:51 +0200

    Add cd-crashdumps.ps1

---
+++ Scripts/cd-crashdumps.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+	Change to the crash dumps folder
+.DESCRIPTION
+	This PowerShell script changes the working directory to the crash dumps directory.
+.EXAMPLE
+	PS> ./cd-crashdumps
+	📂C:\Users\Markus\AppData\Local\CrashDumps
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	$Path = Resolve-Path -Path "~/AppData/Local/CrashDumps"
+	Set-Location "$Path"
+	"📂$Path"
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
