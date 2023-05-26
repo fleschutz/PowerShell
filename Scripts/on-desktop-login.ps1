689da89e893dff8e7adba26a44b924b5dcf32e28
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-05-26 14:06:10 +0200

    Add on-desktop-login.ps1

---
+++ Scripts/on-desktop-login.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+	Executes tasks on desktop login
+.DESCRIPTION
+	This PowerShell script executes tasks on every desktop login.
+.EXAMPLE
+	PS> ./on-desktop-login
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	& "$PSScriptRoot/open-dashboards.ps1"
+	& "$PSScriptRoot/open-windows-terminal.ps1"
+	exit 0 # success
+} catch {
+        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+        exit 1
+}
