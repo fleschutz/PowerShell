c4faf8ff6a97c7c08bea73e7448182f4abaace56
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-05-31 15:40:59 +0200

    Add cd-etc.ps1 script

---
+++ Scripts/cd-etc.ps1
@@ -0,0 +1,30 @@
+﻿<#
+.SYNOPSIS
+	Changes to the /etc directory
+.DESCRIPTION
+	This PowerShell script changes the working directory to the /etc directory.
+.EXAMPLE
+	PS> ./cd-etc
+	📂C:\Windows\System32\drivers\etc
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	if ($IsLinx) {
+		$Path = "/etc"
+	} else {
+		$Path = Resolve-Path "$env:WINDIR\System32\drivers\etc"
+	}
+	if (-not(Test-Path "$Path" -pathType container)) {
+		throw "/etc directory at 📂$Path doesn't exist (yet)"
+	}
+	Set-Location "$Path"
+	"📂$Path"
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
