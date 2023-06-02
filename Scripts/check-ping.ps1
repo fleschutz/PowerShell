140f47049be68e646608291d85af983f8fcdaa3e
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-06-02 12:29:49 +0200

    Update check-ping.ps1

--- Scripts/check-ping.ps1
+++ Scripts/check-ping.ps1
@@ -17,19 +17,38 @@
 param([string]$hosts = "amazon.com,bing.com,cnn.com,dropbox.com,facebook.com,google.com,live.com,twitter.com,youtube.com")
 
 try {
-	Write-Progress "⏳ Sending a ping to 9 popular hosts..."
+	Write-Progress "⏳ Sending pings to 9 popular hosts..."
 	$HostsArray = $hosts.Split(",")
-	$Pings = Test-Connection -computerName $HostsArray -count 1
+	if ($IsLinux) {
+		$t = $HostsArray | foreach {
+			(New-Object Net.NetworkInformation.Ping).SendPingAsync($_, 250)
+		}
+		[Threading.Tasks.Task]::WaitAll($t)
+		[int]$Min = 9999999
+		[int]$Max = [int]$Avg = [int]$Count = 0
+		foreach($ping in $t.Result) {
+			if ($ping.Status -eq "Success") {
+				[int]$Latency = $ping.RoundtripTime
+				if ($Latency -lt $Min) { $Min = $Latency }
+				if ($Latency -gt $Max) { $Max = $Latency }
+				$Avg += $Latency
+				$Count++
+			}
+		}
+		$Avg /= $Count
+	} else {
+		$Pings = Test-Connection -computerName $HostsArray -count 1
 
-	[int]$Min = 9999999
-	[int]$Max = [int]$Avg = 0
-	foreach($Ping in $Pings) {
-		if ($IsLinux) {	[int]$Latency = $Ping.latency } else { [int]$Latency = $Ping.ResponseTime }
-		if ($Latency -lt $Min) { $Min = $Latency }
-		if ($Latency -gt $Max) { $Max = $Latency }
-		$Avg += $Latency
+		[int]$Min = 9999999
+		[int]$Max = [int]$Avg = 0
+		foreach($Ping in $Pings) {
+			if ($IsLinux) {	[int]$Latency = $Ping.latency } else { [int]$Latency = $Ping.ResponseTime }
+			if ($Latency -lt $Min) { $Min = $Latency }
+			if ($Latency -gt $Max) { $Max = $Latency }
+			$Avg += $Latency
+		}
+		$Avg /= $Pings.count
 	}
-	$Avg /= $Pings.count
 
 	Write-Progress -Completed "."
 	Write-Host "✅ Ping latency is $($Min)ms...$($Max)ms with $($Avg)ms average"
