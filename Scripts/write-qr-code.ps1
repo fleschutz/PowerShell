206a6e7596277392fdf79b5015e9cee167f8d6a3
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-06-15 11:28:09 +0200

    Add write-qr-code.ps1

---
+++ Scripts/write-qr-code.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+	Writes a QR code
+.DESCRIPTION
+	This PowerShell script writes a QR code to the console output.
+.EXAMPLE
+	PS> ./write-qr-code
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+" ▄▄▄▄▄▄▄ ▄ ▄▄▄ ▄▄▄▄▄▄▄ "
+" █ ▄▄▄ █ ▄▄▀█  █ ▄▄▄ █ "
+" █ ███ █ █▀ ▄▀ █ ███ █ "
+" █▄▄▄▄▄█ ▄▀█▀█ █▄▄▄▄▄█ "
+" ▄▄▄▄  ▄ ▄▄▄██▄  ▄▄▄ ▄ "
+" ▀ █ ▀▄▄▀█▀▀█▀█▀█▀▀▀▄█ "
+" ▄▄██▀▀▄▄█ ▄▀▄▄▄▀ ▀ ▀▄ "
+" ▄▄▄▄▄▄▄ ▀▀██▄▄██▄ ▀ ▀ "
+" █ ▄▄▄ █  ▀▄ ▄▀ ▀ ██▀▀ "
+" █ ███ █ █▄  ▄ ▀▄ ▀█▀  "
+" █▄▄▄▄▄█ █▀▄█ ███▀ ▄ ▀ "
+
+exit 0 # success
