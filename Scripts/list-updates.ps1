aa49dc412fa2c0f8655cbea4342561c477646d57
Markus Fleschutz <markus@fleschutz.de>
2023-07-01 16:55:09 +0200

    Update list-updates.ps1

--- Scripts/list-updates.ps1
+++ Scripts/list-updates.ps1
@@ -2,8 +2,8 @@
 .SYNOPSIS
 	Lists updates
 .DESCRIPTION
-	This PowerShell script lists available software updates for the local machine.
-	Use "install-updates.ps1" to install the listed updates.
+	This PowerShell script queries and lists available software updates for the local machine.
+	NOTE: use 'install-updates.ps1' to install the listed updates.
 .EXAMPLE
 	PS> ./list-updates
 .LINK
@@ -14,18 +14,18 @@
 
 try {
 	if ($IsLinux) {
-		"⏳ (1/2) Querying package updates... (use install-updates.ps1 to install them)"
+		"⏳ (1/2) Querying package updates..."
 		& sudo apt update
 		& sudo apt list --upgradable
-		"⏳ (2/2) Querying Snap updates... (use install-updates.ps1 to install them)"
+		"⏳ (2/2) Querying Snap updates..."
 		sudo snap refresh --list
 	} else {
-		Write-Progress "⏳ Querying available software updates..."
 		" "
+		Write-Progress "⏳ Querying available software updates..."
 		& winget upgrade
 		Write-Progress -completed "."
-		Write-Host "(use install-updates.ps1 to install these updates)"
 	}
+	"NOTE: use 'install-updates.ps1' to install the listed updates."
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
