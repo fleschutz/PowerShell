ae2a9913bb5d66463acaefed7a7e648833d75ccf
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-07-21 20:59:45 +0200

    Update open-dashboards.ps1

--- Scripts/open-dashboards.ps1
+++ Scripts/open-dashboards.ps1
@@ -6,7 +6,7 @@
 .EXAMPLE
 	PS> ./open-dashboards.ps1
 	⏳ (1/2) Loading Data/web-dashboards.csv...
-	⏳ (2/2) Launching web browser with tabs: Toggl Track · Google Calendar · CNN News...
+	⏳ (2/2) Launching web browser with tabs of Toggl Track · Google Calendar · CNN News...
 	...
 .LINK
 	https://github.com/fleschutz/PowerShell
@@ -19,7 +19,7 @@ try {
 	Write-Host "⏳ (1/2) Loading Data/web-dashboards.csv..."
 	$table = Import-CSV "$PSScriptRoot/../Data/web-dashboards.csv"
 	$numRows = $table.Length
-	Write-Host "⏳ (2/2) Launching web browser with tabs: " -noNewline
+	Write-Host "⏳ (2/2) Launching web browser with tabs of " -noNewline
 	foreach($row in $table) {
 		Write-Host "$($row.NAME) · " -noNewline
 		& "$PSScriptRoot/open-default-browser.ps1" "$($row.URL)"
@@ -27,7 +27,7 @@ try {
 	}
 	Write-Host ""
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
-	"✅ Opened $NumRows web dashboards in $elapsed sec (Hint: use switch-tabs.ps1 to switch between browser tabs automatically)"
+	"✅ Opened $NumRows web dashboards in $elapsed sec (Hint: use 'switch-tabs.ps1' to switch between the tabs automatically)"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
