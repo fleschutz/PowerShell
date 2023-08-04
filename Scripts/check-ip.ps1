2a115dce0aa24815fe70ea707b580baf21fbede0
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-08-04 17:41:02 +0200

    Update check-ip.ps1

--- Scripts/check-ip.ps1
+++ Scripts/check-ip.ps1
@@ -5,7 +5,7 @@
         This PowerShell script queries the public IP address and prints it.
 .EXAMPLE
         PS> ./check-ip.ps1
-	✅ Public IPv4 185.77.209.161, IPv6 2003:f2:6128:fc01:e503:601:30c2:a028, near Munich
+	✅ Public IPv4 185.77.209.161, IPv6 2003:f2:6128:fc01:e503:601:30c2:a028 near Munich
 .LINK
         https://github.com/fleschutz/PowerShell
 .NOTES
@@ -22,7 +22,7 @@ try {
 		$PublicIPv6 = (curl.exe -6 --silent ifconfig.co)
 		$City = (curl.exe --silent ifconfig.co/city)
 	}
-	Write-Output "✅ Public IPv4 $PublicIPv4, IPv6 $PublicIPv6, near $City"
+	Write-Output "✅ Public IPv4 $PublicIPv4, IPv6 $PublicIPv6 near $City"
 	exit 0 # success
 } catch {
         "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
