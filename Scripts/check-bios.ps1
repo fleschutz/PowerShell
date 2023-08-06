5c4abfba6fe0720d2f8a39ae637dfd34c566c7b2
Markus Fleschutz <markus@fleschutz.de>
2023-08-06 18:48:56 +0200

    Update check-bios.ps1

--- Scripts/check-bios.ps1
+++ Scripts/check-bios.ps1
@@ -5,7 +5,7 @@
 	This PowerShell script queries the BIOS status and prints it.
 .EXAMPLE
 	PS> ./check-bios
-	✅ BIOS 'F6', release ALASKA - 1072009, S/N NXA82EV0EBB0760 by American Megatrends Inc.
+	✅ BIOS model F6 version ALASKA - 1072009 by American Megatrends Inc.
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -14,24 +14,26 @@
 
 try {
 	if ($IsLinux) {
-		Write-Progress "⏳ Querying BIOS..."
+		Write-Progress "⏳ Querying BIOS details..."
 		$Model = (sudo dmidecode -s system-product-name)
 		if ("$Model" -ne "") {
-			$Manufacturer = (sudo dmidecode -s system-manufacturer)
 			$Version = (sudo dmidecode -s bios-version)
 			$RelDate = (sudo dmidecode -s bios-release-date)
-			Write-Host "✅ BIOS $Model by $Manufacturer ($Version release of $RelDate)"
+			$Manufacturer = (sudo dmidecode -s system-manufacturer)
+			Write-Host "✅ BIOS model $Model version $Version of $RelDate by $Manufacturer"
 		}
 		Write-Progress -completed "."
 	} else {
-		# Write-Progress "⏳ Querying BIOS..."
 		$BIOS = Get-CimInstance -ClassName Win32_BIOS
 		$Model = $BIOS.Name.Trim()
-		$Manufacturer = $BIOS.Manufacturer.Trim()
-		$Serial = $BIOS.SerialNumber.Trim()
 		$Version = $BIOS.Version.Trim()
-		# Write-Progress -completed "."
-		Write-Host "✅ BIOS '$Model', release $Version, S/N $Serial by $Manufacturer"
+		$Serial = $BIOS.SerialNumber.Trim()
+		$Manufacturer = $BIOS.Manufacturer.Trim()
+		if ($Serial -eq "To be filled by O.E.M.") {
+			Write-Host "✅ BIOS model $Model version $Version by $Manufacturer"
+		} else {
+			Write-Host "✅ BIOS model $Model version $Version, S/N $Serial, by $Manufacturer"
+		}
 	}
 	exit 0 # success
 } catch {
