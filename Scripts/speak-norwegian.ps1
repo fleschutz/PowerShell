d6205dfcb184d88ded281f77cea43b33f96f7bb3
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-08-06 12:10:49 +0200

    Add speak-norwegian.ps1

---
+++ Scripts/speak-norwegian.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Speaks text in Norwegian
+.DESCRIPTION
+	This PowerShell script speaks the given text with a Norwegian text-to-speech (TTS) voice.
+.PARAMETER text
+	Specifies the Norwegian text to speak
+.EXAMPLE
+	PS> ./speak-norwegian.ps1 Hallo
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$text = "")
+
+try {
+	if ($text -eq "") { $text = Read-Host "Enter the Norwegian text to speak" }
+
+	$TTS = New-Object -ComObject SAPI.SPVoice
+	foreach ($voice in $TTS.GetVoices()) {
+		if ($voice.GetDescription() -like "*- Norwegian*") {
+			$TTS.Voice = $voice
+			[void]$TTS.Speak($text)
+			exit 0 # success
+		}
+	}
+	throw "No Norwegian text-to-speech voice found - please install one."
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
