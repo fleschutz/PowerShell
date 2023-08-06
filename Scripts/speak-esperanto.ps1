75d513a1e54fd6812d687a07b06c9a237ed0b403
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-08-06 12:20:15 +0200

    Add speak-esperanto.ps1

---
+++ Scripts/speak-esperanto.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Speaks text in Esperanto
+.DESCRIPTION
+	This PowerShell script speaks the given text with an Esperanto text-to-speech (TTS) voice.
+.PARAMETER text
+	Specifies the Esperanto text to speak
+.EXAMPLE
+	PS> ./speak-esperanto.ps1 Saluton
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$text = "")
+
+try {
+	if ($text -eq "") { $text = Read-Host "Enter the Esperanto text to speak" }
+
+	$TTS = New-Object -ComObject SAPI.SPVoice
+	foreach ($voice in $TTS.GetVoices()) {
+		if ($voice.GetDescription() -like "*- Esperanto*") {
+			$TTS.Voice = $voice
+			[void]$TTS.Speak($text)
+			exit 0 # success
+		}
+	}
+	throw "No Esperanto text-to-speech voice found - please install one."
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
