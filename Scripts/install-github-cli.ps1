5b0c4a8022120cb13412c4c2b92269aebb0ff130
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-08-07 19:52:34 +0200

    Update install-github-cli.ps1

--- Scripts/install-github-cli.ps1
+++ Scripts/install-github-cli.ps1
@@ -2,10 +2,11 @@
 .SYNOPSIS
 	Installs GitHub CLI
 .DESCRIPTION
-	This PowerShell script installs GitHub command-line interface (CLI).
+	This PowerShell script installs the GitHub command-line interface (CLI).
 .EXAMPLE
 	PS> ./install-github-cli.ps1
-	✔ GitHub CLI installed successfully in 9 sec
+	⏳ Installing GitHub CLI...
+	✔ Installation of GitHub CLI took 17 sec
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -13,20 +14,21 @@
 #>
 
 try {
+	"⏳ Installing GitHub CLI..."
 	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 
 	if ($IsMacOS) {
 		& brew install gh
 	} elseif ($IsLinux) {
-		curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+		& curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-		sudo apt update
-		sudo apt install gh
+		& sudo apt update
+		& sudo apt install gh
 	} else {
 		& winget install --id GitHub.cli
-
+	}
 	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ GitHub CLI installed successfully in $Elapsed sec"
+	"✔️ Installation of GitHub CLI took $Elapsed sec"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
