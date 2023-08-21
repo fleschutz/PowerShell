72263ffb3ad0799b26f94e7664a17f3e288fe04b
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-08-21 20:00:49 +0200

    Update measure-BubbleSort.ps1

--- Scripts/measure-BubbleSort.ps1
+++ Scripts/measure-BubbleSort.ps1
@@ -1,4 +1,24 @@
-﻿param([int]$numIntegers = 1000)
+﻿<#
+.SYNOPSIS
+        Measures the BubbleSort algorithm
+.DESCRIPTION
+        This PowerShell script measures the speed of the BubbleSort algorithm.
+	BubbleSort is a simple sorting algorithm that repeatedly steps through the list,
+	compares adjacent elements and swaps them if they are in the wrong order. The pass
+	through the list is repeated until the list is sorted. The algorithm, which is a
+	comparison sort, is named for the way smaller or larger elements "bubble" to the top of the list.
+.PARAMETER numIntegers
+        Specifies the number of integers to sort
+.EXAMPLE
+        PS> ./measure-bubblesort.ps1
+	🕒 BubbleSort of 1000 integers took 0.7291663 sec
+.LINK
+        https://github.com/fleschutz/PowerShell
+.NOTES
+        Author: Markus Fleschutz | License: CC0
+#>
+
+param([int]$numIntegers = 1000)
 
 class BubbleSort {
     static Sort($targetList) {
