da876a6e5a1ae67dd8b4649f5d9786008f86c58e
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-08-24 13:31:26 +0200

    Update write-shit.ps1

--- Scripts/write-shit.ps1
+++ Scripts/write-shit.ps1
@@ -1,1 +1,16 @@
-﻿"💩"
+﻿<#
+.SYNOPSIS
+	Writes shit
+.DESCRIPTION
+	This PowerShell script writes shit to the console (fun).
+.EXAMPLE
+	PS> ./write-shit.ps1
+	💩
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+Write-Output "💩"
+exit 0 # success
