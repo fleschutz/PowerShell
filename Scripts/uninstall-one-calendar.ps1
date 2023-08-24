68decf6c893c70a0fab9822eb6d7c3e1751a0647
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-08-24 13:45:27 +0200

    Updaate uninstall-one-calender.ps1

--- Scripts/uninstall-one-calendar.ps1
+++ Scripts/uninstall-one-calendar.ps1
@@ -1,10 +1,10 @@
-﻿c<#
+﻿<#
 .SYNOPSIS
 	Uninstalls One Calendar
 .DESCRIPTION
 	This PowerShell script uninstalls One Calendar from the local computer.
 .EXAMPLE
-	PS> ./uninstall-one-calendar
+	PS> ./uninstall-one-calendar.ps1
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -12,12 +12,14 @@
 #>
 
 try {
-	"Uninstalling One Calendar, please wait..."
+	"⏳ Uninstalling One Calendar ..."
+	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 
 	& winget uninstall "One Calendar"
 	if ($lastExitCode -ne "0") { throw "Can't uninstall One Calendar, is it installed?" }
 
-	"One Calendar is uninstalled now."
+	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
+	"✔️ Removal of One Calendar tool $Elapsed sec"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
