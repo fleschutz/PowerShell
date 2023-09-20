71b9cd04ed308864ec30b257e9961f75ede449c5
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-09-20 13:56:23 +0200

    Add open-rick-astley.ps1

---
+++ Scripts/open-rick-astley.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+	Opens Rick Astley's song
+.DESCRIPTION
+	This PowerShell script launches the Web browser with YouTube playing Rick Astley.
+.EXAMPLE
+	PS> ./open-rick-astley.ps1
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+& "$PSScriptRoot/open-default-browser.ps1" "https://www.youtube.com/watch?v=v7ScGV5128A"
+"You've been Rick-Rolled 🤣"
+exit 0 # success
