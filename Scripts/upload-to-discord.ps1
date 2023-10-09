e14f126f4c88f56a5a5d8121de918882632b75a5
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-10-09 14:17:07 +0200

    Add upload-to-discord.ps1

---
+++ Scripts/upload-to-discord.ps1
@@ -0,0 +1,22 @@
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = 'YOUR-DISCORD-WEBHOOK'
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
