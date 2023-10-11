fc2f04a133dfafb0fa24de928662de9b0feb08e1
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-10-11 14:06:01 +0200

    Update check-software.ps1

--- Scripts/check-software.ps1
+++ Scripts/check-software.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Checks the software
 .DESCRIPTION
-	This PowerShell script queries the software details of the local computer and prints it.
+	This PowerShell script queries the software status of the local computer and prints it.
 .EXAMPLE
 	PS> ./check-software.ps1
 
@@ -20,9 +20,9 @@
 & "$PSScriptRoot/write-green.ps1" "   S O F T W A R E"
 & "$PSScriptRoot/check-bios.ps1"
 & "$PSScriptRoot/check-os.ps1"
+& "$PSScriptRoot/check-uptime.ps1"
 & "$PSScriptRoot/check-apps.ps1"
 & "$PSScriptRoot/check-powershell.ps1"
-& "$PSScriptRoot/check-uptime.ps1"
 & "$PSScriptRoot/check-time-zone.ps1"
 & "$PSScriptRoot/check-swap-space.ps1"
 & "$PSScriptRoot/check-pending-reboot.ps1"
