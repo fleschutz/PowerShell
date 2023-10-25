23c3ed729dfe7a3f4a0369f1fdb003d9bda53ff0
Markus Fleschutz <markus.fleschutz@atcsim.de>
2023-10-25 17:13:40 +0200

    Update on-desktop-login.ps1

--- Scripts/on-desktop-login.ps1
+++ Scripts/on-desktop-login.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Executes tasks on desktop login
 .DESCRIPTION
-	This PowerShell script executes useful tasks on every desktop login by the user (e.g. open Web dashboards, list the news, fetch repos).
+	This PowerShell script executes some 'welcome' tasks on every user's desktop login.
 	NOTE: For installation on Windows create and put a symbolic link to this script into the Autostart folder (usually at: C:\Users\<USERNAME>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup).
 .EXAMPLE
 	PS> ./on-desktop-login.ps1
@@ -15,6 +15,11 @@
 try {
 	& "$PSScriptRoot/write-big.ps1" "Welcome back"
 	" "
+	& "$PSScriptRoot/check-uptime.ps1"
+	& "$PSScriptRoot/check-firewall.ps1"
+	& "$PSScriptRoot/check-swap-space.ps1"
+	& "$PSScriptRoot/check-drives.ps1"
+	& "$PSScriptRoot/check-pending-reboot.ps1"
 	& "$PSScriptRoot/open-dashboards.ps1"
 	" "
 	& "$PSScriptRoot/write-quote.ps1"
