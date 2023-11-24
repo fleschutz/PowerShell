194dc78d3d8c0b1377b4bc94b8aa35d49622efe2
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-11-24 13:53:56 +0100

    Update install-calibre-server.ps1

--- scripts/install-calibre-server.ps1
+++ scripts/install-calibre-server.ps1
@@ -1,12 +1,14 @@
 ﻿<#
 .SYNOPSIS
-	Installs Calibre server (needs admin rights)
+	Installs the Calibre server (needs admin rights)
 .DESCRIPTION
-	This PowerShell script installs and starts a local Calibre server as background process (using Web port 8099 by default).
+	This PowerShell script installs and starts a local Calibre server as background process.
 .PARAMETER port
 	Specifies the Web port number (8099 by default)
 .EXAMPLE
 	PS> ./install-calibre-server.ps1
+	⏳ (1/5) Updating package infos...
+	...
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -15,32 +17,36 @@
 
 #Requires -RunAsAdministrator
 
-param([int]$Port = 8099, [string]$UserDB = "$HOME/CalibreUsers.sqlite", [string]$Logfile = "$HOME/CalibreServer.log")
+param([int]$port = 8099, [string]$mediaFolder = "$HOME/Calibre Library", [string]$userDB = "$HOME/CalibreUsers.sqlite", [string]$logfile = "$HOME/CalibreServer.log")
 
 try {
-	$StopWatch = [system.diagnostics.stopwatch]::startNew()
+	if ($IsLinux) {
+		$stopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	"⏳ (1/5) Updating package infos..."
-	& sudo apt update -y
-	if ($lastExitCode -ne "0") { throw "'apt update' failed" }
+		"⏳ (1/5) Updating package infos..."
+		& sudo apt update -y
+		if ($lastExitCode -ne "0") { throw "'apt update' failed" }
 
-	"⏳ (2/5) Installing the Calibre package..."
-	& sudo apt install calibre -y
-	if ($lastExitCode -ne "0") { throw "'apt install calibre' failed" }
+		"⏳ (2/5) Installing Calibre..."
+		& sudo apt install calibre -y
+		if ($lastExitCode -ne "0") { throw "'apt install calibre' failed" }
 
-	"⏳ (3/5) Searching for Calibre server executable..."
-	& calibre-server --version
-	if ($lastExitCode -ne "0") { throw "Can't execute 'calibre-server' - make sure Calibre server is installed and available" }
+		"⏳ (3/5) Searching for Calibre server executable..."
+		& calibre-server --version
+		if ($lastExitCode -ne "0") { throw "Can't execute 'calibre-server' - make sure Calibre server is installed and available" }
 
-	"⏳ (4/5) Creating folder 'Calibre Library' in your home directory..."
-	mkdir $HOME/'Calibre Library'
+		"⏳ (4/5) Creating media folder at: $mediaFolder ... (if non-existent)"
+		mkdir $mediaFolder
 
-	"⏳ (5/5) Starting Calibre server as background process..."
-	& calibre-server --port $Port --num-per-page 100 --userdb $UserDB --log $Logfile --daemonize $HOME/'Calibre Library'
+		"⏳ (5/5) Starting Calibre server as background process..."
+		& calibre-server --port $port --num-per-page 100 --userdb $userDB --log $logfile --daemonize $HOME/'Calibre Library'
 
-	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ installed Calibre in $Elapsed sec (Web port $Port, user DB at $UserDB, log file at $Logfile)"
-	exit 0 # success
+		[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
+		"✔️ Installed Calibre server on Web port $port in $elapsed sec (media folder: $mediaFolder, user database: $userDB, log file: $logfile)"
+		exit 0 # success
+	} else {
+		throw "Currently only supported on Linux"
+	}
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
 	exit 1
