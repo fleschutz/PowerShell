c89673f442616fff7a90afa531f26cfd3514b031
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-12-26 11:57:02 +0100

    Rename set-profile.ps1 to write-posershell-profile.ps1

---
+++ scripts/write-powershell-profile.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+	Writes the user's PowerShell profile
+.DESCRIPTION
+	This PowerShell script writes the PowerShell profile for the current user.
+.EXAMPLE
+	PS> ./write-powershell-profile.ps1
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	"⏳ (1/3) Querying path to PowerShell profile 'CurrentUserCurrentHost'..."
+	$pathToProfile = $PROFILE.CurrentUserCurrentHost
+
+	"⏳ (2/3) Creating the profile (if non-existent)..."
+	$null = New-Item -Path $pathToProfile -ItemType "file" -Force
+
+	"⏳ (3/3) Updating the profile by my-profile.ps1..."
+	Copy-Item "$PSScriptRoot/my-profile.ps1" "$pathToProfile" -force
+
+	"✔️ Updated your PowerShell profile - it gets active on next login"
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
