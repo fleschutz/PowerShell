e2aed21d874ee18f6985e439497bcc0e4c8829e9
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-12-30 10:19:44 +0100

    Improve check-powershell.ps1

--- scripts/check-powershell.ps1
+++ scripts/check-powershell.ps1
@@ -5,23 +5,33 @@
 	This PowerShell script queries the PowerShell status and prints it.
 .EXAMPLE
 	PS> ./check-powershell.ps1
-	✅ PowerShell 5.1.19041.2673 Desktop edition (10 modules, 1458 cmdlets, 172 aliases)
+	✅ PowerShell 5.1.19041.2673 Desktop edition (1 profile, 10 modules, 1458 cmdlets, 172 aliases)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
+function GetNumberOfProfiles {
+	[int]$count = 0
+	if (Test-Path $PROFILE.AllUsersAllHosts) { $count++ }
+	if (Test-Path $PROFILE.AllUsersCurrentHost) { $count++ }
+	if (Test-Path $PROFILE.CurrentUserAllHosts) { $count++ }
+	if (Test-Path $PROFILE.CurrentUserCurrentHost) { $count++ }
+	return $count
+}
+
 try {
 	$version = $PSVersionTable.PSVersion
 	$edition = $PSVersionTable.PSEdition
+	$numProfiles = GetNumberOfProfiles
 	$numModules = (Get-Module).Count
 	$numAliases = (Get-Alias).Count
 	if ($IsLinux) {
-		"✅ PowerShell $version $edition edition ($numModules modules, $numAliases aliases)"
+		"✅ PowerShell $version $edition edition ($numProfiles profile, $numModules modules, $numAliases aliases)"
 	} else {
 		$numCmdlets = (Get-Command -Command-Type cmdlet).Count
-		"✅ PowerShell $version $edition edition ($numModules modules, $numCmdlets cmdlets, $numAliases aliases)"
+		"✅ PowerShell $version $edition edition ($numProfiles profile, $numModules modules, $numCmdlets cmdlets, $numAliases aliases)"
 	}
 	exit 0 # success
 } catch {
