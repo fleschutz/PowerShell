b759eaa68a544d6e4265d66182589bccb7557fdc
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-01-15 09:59:13 +0100

    Added save-credentials.ps1

---
+++ scripts/save-credentials.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+	Saves credentials encrypted
+.DESCRIPTION
+	This PowerShell script asks for credentials and saves them encrypted into a target file ("$HOME\my.credentials" by default).
+.PARAMETER TargetFile
+	Specifies the target file ("$HOME\my.credentials" by default)
+.EXAMPLE
+	PS> ./save-credentials.ps1
+	Enter username and password, please.
+ 	✔️ Your credentials have been saved encrypted into C:\Users\Markus\my.credentials
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$TargetFile = "$HOME\my.credentials")
+
+try {
+	Write-Host "Enter username and password, please." -foreground red
+	$cred = Get-Credential
+	$cred.Password | ConvertFrom-SecureString | Set-Content "$TargetFile"
+	"✔️ Your credentials have been saved encrypted into $TargetFile"
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
