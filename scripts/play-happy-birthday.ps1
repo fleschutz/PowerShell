e73e26f9e798033359d8a41c6adc36d0bf3865a9
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-01-25 13:52:52 +0100

    Update play-happy-birthday.ps1

--- scripts/play-happy-birthday.ps1
+++ scripts/play-happy-birthday.ps1
@@ -4,36 +4,42 @@
 .DESCRIPTION
 	This PowerShell script plays the famous Happy Birthday song.
 .EXAMPLE
-	PS> ./play-happy-birthday
+	PS> ./play-happy-birthday.ps1
+	(listen and enjoy)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
-[System.Console]::Beep(1059.274, 300)
-[System.Console]::Beep(1059.274, 200)
-[System.Console]::Beep(1188.995, 500)
-[System.Console]::Beep(1059.274, 500)
-[System.Console]::Beep(1413.961, 500)
-[System.Console]::Beep(1334.601, 950)
-[System.Console]::Beep(1059.274, 300)
-[System.Console]::Beep(1059.274, 200)
-[System.Console]::Beep(1188.995, 500)
-[System.Console]::Beep(1059.274, 500)
-[System.Console]::Beep(1587.117, 500)
-[System.Console]::Beep(1413.961, 950)
-[System.Console]::Beep(1059.274, 300)
-[System.Console]::Beep(1059.274, 200)
-[System.Console]::Beep(2118.547, 500)
-[System.Console]::Beep(1781.479, 500)
-[System.Console]::Beep(1413.961, 500)
-[System.Console]::Beep(1334.601, 500)
-[System.Console]::Beep(1188.995, 500)
-[System.Console]::Beep(1887.411, 300)
-[System.Console]::Beep(1887.411, 200)
-[System.Console]::Beep(1781.479, 500)
-[System.Console]::Beep(1413.961, 500)
-[System.Console]::Beep(1587.117, 500)
-[System.Console]::Beep(1413.961, 900)
-exit 0 # success
+try {
+	[System.Console]::Beep(1059.274, 300)
+	[System.Console]::Beep(1059.274, 200)
+	[System.Console]::Beep(1188.995, 500)
+	[System.Console]::Beep(1059.274, 500)
+	[System.Console]::Beep(1413.961, 500)
+	[System.Console]::Beep(1334.601, 950)
+	[System.Console]::Beep(1059.274, 300)
+	[System.Console]::Beep(1059.274, 200)
+	[System.Console]::Beep(1188.995, 500)
+	[System.Console]::Beep(1059.274, 500)
+	[System.Console]::Beep(1587.117, 500)
+	[System.Console]::Beep(1413.961, 950)
+	[System.Console]::Beep(1059.274, 300)
+	[System.Console]::Beep(1059.274, 200)
+	[System.Console]::Beep(2118.547, 500)
+	[System.Console]::Beep(1781.479, 500)
+	[System.Console]::Beep(1413.961, 500)
+	[System.Console]::Beep(1334.601, 500)
+	[System.Console]::Beep(1188.995, 500)
+	[System.Console]::Beep(1887.411, 300)
+	[System.Console]::Beep(1887.411, 200)
+	[System.Console]::Beep(1781.479, 500)
+	[System.Console]::Beep(1413.961, 500)
+	[System.Console]::Beep(1587.117, 500)
+	[System.Console]::Beep(1413.961, 900)
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
