7501f717942bacb4f4539d3fa34a4293c395bded
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-01-25 13:50:47 +0100

    Updated play-beep-sound.ps1

--- scripts/play-beep-sound.ps1
+++ scripts/play-beep-sound.ps1
@@ -2,15 +2,21 @@
 .SYNOPSIS
 	Plays a short beep sound
 .DESCRIPTION
-	This PowerShell script plays a short beep sound.
+	This PowerShell script plays a short beep sound at 500Hz for 300ms.
 .EXAMPLE
-	PS> ./play-beep-sound
+	PS> ./play-beep-sound.ps1
+	(listen and enjoy)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
-[System.Console]::Beep(500,300)
-Start-Sleep -milliseconds 300
-exit 0 # success
+try {
+	[System.Console]::Beep(500,300)
+	Start-Sleep -milliseconds 300
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
