66d6bc3d52f9901ec4cdc48b0c3e3d9ee0b5d7bd
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-01-25 13:58:06 +0100

    Updated some play-* scripts

--- scripts/play-imperial-march.ps1
+++ scripts/play-imperial-march.ps1
@@ -4,29 +4,35 @@
 .DESCRIPTION
 	This PowerShell script plays the Imperial March used in the Star Wars film series.
 .EXAMPLE
-	PS> ./play-imperial-march
+	PS> ./play-imperial-march.ps1
+	(listen and enjoy)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
-[System.Console]::beep(440, 500)      
-[System.Console]::beep(440, 500)
-[System.Console]::beep(440, 500)       
-[System.Console]::beep(349, 350)       
-[System.Console]::beep(523, 150)       
-[System.Console]::beep(440, 500)       
-[System.Console]::beep(349, 350)       
-[System.Console]::beep(523, 150)       
-[System.Console]::beep(440, 1000)
-[System.Console]::beep(659, 500)       
-[System.Console]::beep(659, 500)       
-[System.Console]::beep(659, 500)       
-[System.Console]::beep(698, 350)       
-[System.Console]::beep(523, 150)       
-[System.Console]::beep(415, 500)       
-[System.Console]::beep(349, 350)       
-[System.Console]::beep(523, 150)       
-[System.Console]::beep(440, 1000)
-exit 0 # success
+try {
+	[System.Console]::beep(440, 500)      
+	[System.Console]::beep(440, 500)
+	[System.Console]::beep(440, 500)       
+	[System.Console]::beep(349, 350)       
+	[System.Console]::beep(523, 150)       
+	[System.Console]::beep(440, 500)       
+	[System.Console]::beep(349, 350)       
+	[System.Console]::beep(523, 150)       
+	[System.Console]::beep(440, 1000)
+	[System.Console]::beep(659, 500)       
+	[System.Console]::beep(659, 500)       
+	[System.Console]::beep(659, 500)       
+	[System.Console]::beep(698, 350)       
+	[System.Console]::beep(523, 150)       
+	[System.Console]::beep(415, 500)       
+	[System.Console]::beep(349, 350)       
+	[System.Console]::beep(523, 150)       
+	[System.Console]::beep(440, 1000)
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
--- scripts/play-mission-impossible.ps1
+++ scripts/play-mission-impossible.ps1
@@ -4,57 +4,63 @@
 .DESCRIPTION
 	This PowerShell script plays the Mission Impossible theme.
 .EXAMPLE
-	PS> ./play-mission-impossible
+	PS> ./play-mission-impossible.ps1
+	(listen and enjoy)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[System.Console]::Beep(932,150)
-Start-Sleep -m 150
-[System.Console]::Beep(1047,150)
-Start-Sleep -m 150
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[console]::Beep(699,150)
-Start-Sleep -m 150
-[System.Console]::Beep(740,150)
-Start-Sleep -m 150
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[System.Console]::Beep(932,150)
-Start-Sleep -m 150
-[System.Console]::Beep(1047,150)
-Start-Sleep -m 150
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[System.Console]::Beep(784,150)
-Start-Sleep -m 300
-[System.Console]::Beep(699,150)
-Start-Sleep -m 150
-[System.Console]::Beep(740,150)
-Start-Sleep -m 150
-[System.Console]::Beep(932,150)
-[System.Console]::Beep(784,150)
-[System.Console]::Beep(587,1200)
-Start-Sleep -m 75
-[System.Console]::Beep(932,150)
-[System.Console]::Beep(784,150)
-[System.Console]::Beep(554,1200)
-Start-Sleep -m 75
-[System.Console]::Beep(932,150)
-[System.Console]::Beep(784,150)
-[System.Console]::Beep(523,1200)
-Start-Sleep -m 150
-[System.Console]::Beep(466,150)
-[System.Console]::Beep(523,150)
-exit 0 # success
+try {
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[System.Console]::Beep(932,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(1047,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[console]::Beep(699,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(740,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[System.Console]::Beep(932,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(1047,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[System.Console]::Beep(784,150)
+	Start-Sleep -m 300
+	[System.Console]::Beep(699,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(740,150)
+	Start-Sleep -m 150
+	[System.Console]::Beep(932,150)
+	[System.Console]::Beep(784,150)
+	[System.Console]::Beep(587,1200)
+	Start-Sleep -m 75
+	[System.Console]::Beep(932,150)
+	[System.Console]::Beep(784,150)
+	[System.Console]::Beep(554,1200)
+	Start-Sleep -m 75
+	[System.Console]::Beep(932,150)
+	[System.Console]::Beep(784,150)
+	[System.Console]::Beep(523,1200)
+	Start-Sleep -m 150
+	[System.Console]::Beep(466,150)
+	[System.Console]::Beep(523,150)
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
