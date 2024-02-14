0ae669da266c0793f712fa4aec61e0ed03bb2034
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-02-14 09:08:14 +0100

    Update list-network-shares.ps1

--- scripts/list-network-shares.ps1
+++ scripts/list-network-shares.ps1
@@ -1,14 +1,14 @@
 ﻿<#
 .SYNOPSIS
-	Lists all network shares of the local computer
+	Lists the local network shares
 .DESCRIPTION
 	This PowerShell script lists all network shares of the local computer.
 .EXAMPLE
 	PS> ./list-network-shares.ps1
 
-	Name  Path     Description
-	----  ----     -----------
-	Users C:\Users
+	Name    Path       Description
+	----    ----       -----------
+	Public  C:\Public  Public folder for file transfer
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -16,7 +16,7 @@
 #>
 
 try {
-	Get-WmiObject win32_share | where {$_.name -NotLike "*$"}
+	Get-WmiObject win32_share | where {$_.name -NotLike "*$"} 
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
