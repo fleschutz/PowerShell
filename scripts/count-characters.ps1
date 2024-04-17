46c80fcd363542b41f069af1cbba698c34f0ab20
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-04-17 16:48:46 +0200

    Added count-characters.ps1

---
+++ scripts/count-characters.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Counts the number of characters
+.DESCRIPTION
+	This PowerShell script counts the number of characters in the given string.
+.PARAMETER GivenString
+	Specifies the given string.
+.EXAMPLE
+	PS> ./count-characters.ps1 "Hello World"
+	✔️ 11 characters counted in 'Hello World'.
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$givenString = "")
+
+try {
+	if ($givenString -eq "" ) { $givenString = Read-Host "Enter the string" }
+
+	[int64]$numChars = $givenString.Length
+	"✔️ $numChars characters counted in '$givenString'." 
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
