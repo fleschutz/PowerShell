746825f40f24828f134473e286d1e6578d797f85
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-04-17 11:23:41 +0200

    Updated pull-repo.ps1

--- scripts/pull-repo.ps1
+++ scripts/pull-repo.ps1
@@ -1,48 +1,48 @@
 ﻿<#
 .SYNOPSIS
-	Pulls updates into a Git repository
+	Pulls updates into a Git repo
 .DESCRIPTION
-	This PowerShell script pulls the latest updates into a local Git repository (including submodules).
-.PARAMETER RepoDir
+	This PowerShell script pulls remote updates into a local Git repository (including submodules).
+.PARAMETER pathToRepo
 	Specifies the file path to the local Git repository (default is working directory)
 .EXAMPLE
-	PS> ./pull-repo.ps1 C:\MyRepo
-	⏳ (1/4) Searching for Git executable...  git version 2.42.0.windows.1
+	PS> ./pull-repo.ps1 C:\Repos\rust
+	⏳ (1/4) Searching for Git executable...  git version 2.44.0.windows.1
 	⏳ (2/4) Checking local repository...
-	⏳ (3/4) Pulling updates...
+	⏳ (3/4) Pulling remote updates...
 	⏳ (4/4) Updating submodules...
-	✔️ Pulled updates into repo 📂MyRepo in 14 sec
+	✔️ Pulled updates into 📂rust repository in 14 sec.
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
-param([string]$RepoDir = "$PWD")
+param([string]$pathToRepo = "$PWD")
 
 try {
-	$StopWatch = [system.diagnostics.stopwatch]::startNew()
+	$stopWatch = [system.diagnostics.stopwatch]::startNew()
 
 	Write-Host "⏳ (1/4) Searching for Git executable...  " -noNewline
 	& git --version
 	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 
 	Write-Host "⏳ (2/4) Checking local repository..."
-	if (-not(Test-Path "$RepoDir" -pathType container)) { throw "Can't access folder: $RepoDir" }
-	$Result = (git -C "$RepoDir" status)
-	if ("$Result" -match "HEAD detached at ") { throw "Currently in detached HEAD state (not on a branch!), so nothing to pull" }
-	$RepoDirName = (Get-Item "$RepoDir").Name
+	if (-not(Test-Path "$pathToRepo" -pathType container)) { throw "Can't access folder: $pathToRepo" }
+	$result = (git -C "$pathToRepo" status)
+	if ("$result" -match "HEAD detached at ") { throw "Currently in detached HEAD state (not on a branch!), so nothing to pull" }
+	$pathToRepoName = (Get-Item "$pathToRepo").Name
 
-	Write-Host "⏳ (3/4) Pulling updates..."
-	& git -C "$RepoDir" pull --recurse-submodules=yes
+	Write-Host "⏳ (3/4) Pulling remote updates..."
+	& git -C "$pathToRepo" pull --recurse-submodules=yes
 	if ($lastExitCode -ne "0") { throw "'git pull' failed with exit code $lastExitCode" }
 
 	Write-Host "⏳ (4/4) Updating submodules... "
-	& git -C "$RepoDir" submodule update --init --recursive
+	& git -C "$pathToRepo" submodule update --init --recursive
 	if ($lastExitCode -ne "0") { throw "'git submodule update' failed with exit code $lastExitCode" }
 
-	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✔️ Pulled updates into repo 📂$RepoDirName in $Elapsed sec"
+	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
+	"✔️ Pulled updates into 📂$pathToRepoName repository in $elapsed sec."
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
