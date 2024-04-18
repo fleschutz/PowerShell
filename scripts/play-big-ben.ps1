900809ed2d4f478272d1b3624ed82b3159fc2515
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-04-18 09:15:03 +0200

    Add play-big-ben.ps1

---
+++ scripts/play-big-ben.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+	Plays the Big Ben sound
+.DESCRIPTION
+	This PowerShell script plays the sound of Big Ben.
+.EXAMPLE
+	PS> ./play-big-ben.ps1
+	(listen and enjoy)
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+& "$PSScriptRoot/play-mp3.ps1" "$PSScriptRoot/../data/sounds/Big Ben.mp3"
+exit 0 # success
