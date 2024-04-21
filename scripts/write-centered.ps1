d9f5847aedd468eae44636185285fc6f37c5e43e
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-04-21 17:23:21 +0200

    Added write-centered.ps1

--- scripts/write-centered.ps1
+++ scripts/write-centered.ps1
@@ -1,15 +1,13 @@
 ﻿<#
 .SYNOPSIS
-	Writes a headline
+	Writes text centered
 .DESCRIPTION
-	This PowerShell script writes the given text as a headline.
+	This PowerShell script writes the given text centered to the console.
 .PARAMETER text
 	Specifies the text to write
 .EXAMPLE
 	PS> ./write-headline.ps1 "Hello World"
-
-	* Hello World *
-	---------------
+							Hello World
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -21,11 +19,13 @@ param([string]$text = "")
 try {
 	if ($text -eq "") { $text = Read-Host "Enter the text to write" }
 
-	Write-Host "`n* $text *" -foregroundColor green
-	[int]$len = $text.Length
-	[string]$line = "----"
-	for ([int]$i = 0; $i -lt $len; $i++) { $line += "-" }
-	Write-Host "$line" -foregroundColor green
+	$ui = (Get-Host).ui
+	$rui = $ui.rawui 
+	[int]$numSpaces = ($rui.MaxWindowSize.Width - $text.Length) / 2
+
+	[string]$spaces = ""
+	for ([int]$i = 0; $i -lt $numSpaces; $i++) { $spaces += " " }
+	Write-Host "$spaces$text"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
