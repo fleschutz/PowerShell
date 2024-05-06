bbd9fd4e99316f0abcc0352d9dc1107c7e7ab507
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-05-06 18:23:36 +0200

    Renamed to tell-new-year.ps1

---
+++ scripts/tell-new-year.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Tells the time until New Year
+.DESCRIPTION
+	This PowerShell script calculates the time until New Year and replies by text-to-speech (TTS).
+.EXAMPLE
+	PS> ./tell-new-year.ps1
+	(listen and enjoy)
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	$now = [DateTime]::Now
+	$NewYear = [Datetime]("12/31/" + $now.Year)
+	$days = ($NewYear – $now).Days + 1
+	if ($days -gt 1) {
+		& "$PSScriptRoot/speak-english.ps1" "New Year is in $days days."
+	} elseif ($days -eq 1) {
+		& "$PSScriptRoot/speak-english.ps1" "New Year is tomorrow."
+	}
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
