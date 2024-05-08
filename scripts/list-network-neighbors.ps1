adfc40c2cf0d6f5d984814e24c98ee5c2a1eece3
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-05-08 13:32:42 +0200

    Add list-network-neighbors.ps1

---
+++ scripts/list-network-neighbors.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+	Lists the (cached) network neighborspwd
+.DESCRIPTION
+	This PowerShell script lists all cached network neighbors of the local computer.
+.EXAMPLE
+	PS> ./list-network-neighbors.ps1
+
+	Name                             InterfaceAlias               IPAddress                 LinkLayerAddress        State
+	----                             --------------               ---------                 ----------------        -----
+	<<>8:8:8<?;55;=55;               Ethernet                     224.0.0.251               01-00-5E-00-00-FB   Permanent
+	...
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	Get-NetNeighbor -includeAllCompartments | Format-Table -property Name,InterfaceAlias,IPAddress,LinkLayerAddress,State -autoSize
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
