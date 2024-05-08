54974f6660a89a29f02653bf125f83bd7a1dba1b
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-05-08 18:56:05 +0200

    Update list-network-neighbors.ps1

--- scripts/list-network-neighbors.ps1
+++ scripts/list-network-neighbors.ps1
@@ -6,9 +6,9 @@
 .EXAMPLE
 	PS> ./list-network-neighbors.ps1
 
-	Name                             InterfaceAlias               IPAddress                 LinkLayerAddress        State
-	----                             --------------               ---------                 ----------------        -----
-	<<>8:8:8<?;55;=55;               Ethernet                     224.0.0.251               01-00-5E-00-00-FB   Permanent
+	Name                  InterfaceAlias     IPAddress       LinkLayerAddress        State
+	----                  --------------     ---------       ----------------        -----
+	<<>8:8:8<?;55;=55;    Ethernet           224.0.0.251     01-00-5E-00-00-FB   Permanent
 	...
 .LINK
 	https://github.com/fleschutz/PowerShell
@@ -17,6 +17,8 @@
 #>
 
 try {
+	if ($IsLinux -or $IsMacOS) { throw "Sorry, currently available for Windows only" }
+
 	Get-NetNeighbor -includeAllCompartments | Format-Table -property Name,InterfaceAlias,IPAddress,LinkLayerAddress,State -autoSize
 	exit 0 # success
 } catch {
