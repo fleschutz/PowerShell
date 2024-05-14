b360ab234b6ca6d23299642387088ea3873bf98b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-05-14 15:50:59 +0200

    Updated list-earthquakes.ps1

--- scripts/list-earthquakes.ps1
+++ scripts/list-earthquakes.ps1
@@ -2,12 +2,14 @@
 .SYNOPSIS
 	Lists major earthquakes
 .DESCRIPTION
-	This PowerShell script lists major earthquakes with magnitude >= 6.0 for the last 30 days.
+	This PowerShell script lists major earthquakes for the last 30 days.
+.PARAMETER minMagnitude
+	Specifies the minimum magnitude to list (5.5 by default)
 .EXAMPLE
 	PS> ./list-earthquakes.ps1
 
-	Mag   Location                                   Depth        Time
-	---   --------                                   -----        ----
+	Mag   Location                                   Depth        Time UTC
+	---   --------                                   -----        --------
 	7.2   98 km S of Sand Point, Alaska              33 km        2023-07-16T06:48:22.606Z
 	...
 .LINK
@@ -16,22 +18,25 @@
 	Author: Markus Fleschutz | License: CC0
 #>
 
+param([float]$minMagnitude=5.5)
+
 $Format="csv" # cap, csv, geojson, kml, kmlraw, quakeml, text, xml
-$MinMagnitude=5.7
 $OrderBy="magnitude" # time, time-asc, magnitude, magnitude-asc
 
 function ListEarthquakes { 
 	Write-Progress "Loading data from earthquake.usgs.gov..."
-	$Quakes = (Invoke-WebRequest -URI "https://earthquake.usgs.gov/fdsnws/event/1/query?format=$Format&minmagnitude=$MinMagnitude&orderby=$OrderBy" -userAgent "curl" -useBasicParsing).Content | ConvertFrom-CSV
-	foreach ($Quake in $Quakes) {
-		[int]$Depth = $Quake.depth
-		New-Object PSObject -Property @{ Mag=$Quake.mag; Depth="$Depth km"; Location=$Quake.place; Time=$Quake.time }
+	$quakes = (Invoke-WebRequest -URI "https://earthquake.usgs.gov/fdsnws/event/1/query?format=$Format&minmagnitude=$minMagnitude&orderby=$OrderBy" -userAgent "curl" -useBasicParsing).Content | ConvertFrom-CSV
+	Write-Progress -completed "done."
+
+	foreach($quake in $quakes) {
+		[int]$depth = $quake.depth
+		New-Object PSObject -Property @{ Mag=$quake.mag; Depth="$depth km"; Location=$quake.place; 'Time UTC'=$quake.time }
 	}
-	Write-Progress -completed "Loading finished."
+
 }
  
 try {
-	ListEarthquakes | Format-Table -property @{e='Mag';width=5},@{e='Location';width=42},@{e='Depth';width=12},Time 
+	ListEarthquakes | Format-Table -property @{e='Mag';width=5},@{e='Location';width=42},@{e='Depth';width=12},'Time UTC' 
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
