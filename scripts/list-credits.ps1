6136ae80f203bd10a794f1bc3499630d7510ab03
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-05-16 17:02:44 +0200

    Updated list-credits.ps1

--- scripts/list-credits.ps1
+++ scripts/list-credits.ps1
@@ -12,20 +12,17 @@
 #>
 
 try {
-	clear-host
-	""
+	Clear-Host
 	"			 ______     ______     ______     _____     __     ______   ______    "
 	"			/\  ___\   /\  == \   /\  ___\   /\  __-.  /\ \   /\__  _\ /\  ___\   "
 	"			\ \ \____  \ \  __<   \ \  __\   \ \ \/\ \ \ \ \  \/_/\ \/ \ \___  \  "
 	"			 \ \_____\  \ \_\ \_\  \ \_____\  \ \____-  \ \_\    \ \_\  \/\_____\ "
 	"			  \/_____/   \/_/ /_/   \/_____/   \/____/   \/_/     \/_/   \/_____/ "
-	""
-                                                                      
+	""                                                       
 	& "$PSScriptRoot/write-animated.ps1" "Typos: Markus Fleschutz"
 	& "$PSScriptRoot/write-animated.ps1" "Keyboard: Rapoo 12335 E9270P WL Ultra-Slim Touch"
 	& "$PSScriptRoot/write-animated.ps1" "Operating Systems: Windows 11 23H3 & Ubuntu Server 24.04 LTS"
 	& "$PSScriptRoot/write-animated.ps1" "Console: Windows Terminal 1.20.11271.0"
-	& "$PSScriptRoot/write-animated.ps1" "Background Image: Asteroid Field by starwars.com"
 	& "$PSScriptRoot/write-animated.ps1" "Shell: PowerShell 5.1.22621.2506 & PowerShell 7.4.2"
 	& "$PSScriptRoot/write-animated.ps1" "Scripts: PowerShell Scripts 1.1"
 	& "$PSScriptRoot/write-animated.ps1" "GitHub: github.com/fleschutz/PowerShell"
@@ -38,7 +35,6 @@ try {
 	& "$PSScriptRoot/write-animated.ps1" "Special Thanks: Andrea Fleschutz"
 	& "$PSScriptRoot/write-animated.ps1" "Copyright: (c) 2024. All Rights Reserved"
 	& "$PSScriptRoot/write-animated.ps1" "No Animals Were Harmed in the Making of This Film"
-
 	& "$PSScriptRoot/write-big.ps1" " Thanx 4 watching"
 	exit 0 # success
 } catch {
