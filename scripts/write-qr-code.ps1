f70946156f487264549c7d78308f4fc63691e076
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-05-23 16:49:44 +0200

    Improved write-qr-code.ps1

--- scripts/write-qr-code.ps1
+++ scripts/write-qr-code.ps1
@@ -4,25 +4,29 @@
 .DESCRIPTION
 	This PowerShell script writes "Hello World" as QR code to the console output.
 .EXAMPLE
-	PS> ./write-qr-code
+	PS> ./write-qr-code.ps1
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
-Write-Host "                         " -foregroundColor black -backgroundColor white
-Write-Host "  ▄▄▄▄▄▄▄ ▄ ▄▄▄ ▄▄▄▄▄▄▄  " -foregroundColor black -backgroundColor white
-Write-Host "  █ ▄▄▄ █ ▄▄▀█  █ ▄▄▄ █  " -foregroundColor black -backgroundColor white
-Write-Host "  █ ███ █ █▀ ▄▀ █ ███ █  " -foregroundColor black -backgroundColor white
-Write-Host "  █▄▄▄▄▄█ ▄▀█▀█ █▄▄▄▄▄█  " -foregroundColor black -backgroundColor white
-Write-Host "  ▄▄▄▄  ▄ ▄▄▄██▄  ▄▄▄ ▄  " -foregroundColor black -backgroundColor white
-Write-Host "  ▀ █ ▀▄▄▀█▀▀█▀█▀█▀▀▀▄█  " -foregroundColor black -backgroundColor white
-Write-Host "  ▄▄██▀▀▄▄█ ▄▀▄▄▄▀ ▀ ▀▄  " -foregroundColor black -backgroundColor white
-Write-Host "  ▄▄▄▄▄▄▄ ▀▀██▄▄██▄ ▀ ▀  " -foregroundColor black -backgroundColor white
-Write-Host "  █ ▄▄▄ █  ▀▄ ▄▀ ▀ ██▀▀  " -foregroundColor black -backgroundColor white
-Write-Host "  █ ███ █ █▄  ▄ ▀▄ ▀█▀   " -foregroundColor black -backgroundColor white
-Write-Host "  █▄▄▄▄▄█ █▀▄█ ███▀ ▄ ▀  " -foregroundColor black -backgroundColor white
-Write-Host "                         " -foregroundColor black -backgroundColor white
+Write-Host "                             " -foregroundColor black -backgroundColor white
+Write-Host "                             " -foregroundColor black -backgroundColor white
+Write-Host "                             " -foregroundColor black -backgroundColor white
+Write-Host "    ▄▄▄▄▄▄▄ ▄ ▄▄▄ ▄▄▄▄▄▄▄    " -foregroundColor black -backgroundColor white
+Write-Host "    █ ▄▄▄ █ ▄▄▀█  █ ▄▄▄ █    " -foregroundColor black -backgroundColor white
+Write-Host "    █ ███ █ █▀ ▄▀ █ ███ █    " -foregroundColor black -backgroundColor white
+Write-Host "    █▄▄▄▄▄█ ▄▀█▀█ █▄▄▄▄▄█    " -foregroundColor black -backgroundColor white
+Write-Host "    ▄▄▄▄  ▄ ▄▄▄██▄  ▄▄▄ ▄    " -foregroundColor black -backgroundColor white
+Write-Host "    ▀ █ ▀▄▄▀█▀▀█▀█▀█▀▀▀▄█    " -foregroundColor black -backgroundColor white
+Write-Host "    ▄▄██▀▀▄▄█ ▄▀▄▄▄▀ ▀ ▀▄    " -foregroundColor black -backgroundColor white
+Write-Host "    ▄▄▄▄▄▄▄ ▀▀██▄▄██▄ ▀ ▀    " -foregroundColor black -backgroundColor white
+Write-Host "    █ ▄▄▄ █  ▀▄ ▄▀ ▀ ██▀▀    " -foregroundColor black -backgroundColor white
+Write-Host "    █ ███ █ █▄  ▄ ▀▄ ▀█▀     " -foregroundColor black -backgroundColor white
+Write-Host "    █▄▄▄▄▄█ █▀▄█ ███▀ ▄ ▀    " -foregroundColor black -backgroundColor white
+Write-Host "                             " -foregroundColor black -backgroundColor white
+Write-Host "                             " -foregroundColor black -backgroundColor white
+Write-Host "                             " -foregroundColor black -backgroundColor white
 
 exit 0 # success
