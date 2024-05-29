e52a77bba26b36b29f07bfd689f8712789eed5e4
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-05-29 18:57:07 +0200

    Renamed to remember.ps1

---
+++ scripts/remember.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+	Remembers a text 
+.DESCRIPTION
+	This PowerShell script saves the given text to Memos.csv in your home folder.
+.PARAMETER text1
+	Specifies the text to memorize
+.EXAMPLE
+	PS> ./remember.ps1 "Buy apples"
+	✔️ Saved to /home/Markus/Memos.csv in 0s.
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$text1 = "", [string]$text2 = "", [string]$text3 = "")
+
+try {
+	if ($text1 -eq "") { $text1 = Read-Host "Enter what needs to be remembered" }
+
+	$stopWatch = [system.diagnostics.stopwatch]::startNew()
+	$timestamp = Get-Date -format FileDateTimeUniversal
+	$path = "~/Memos.csv"
+
+	if (-not(Test-Path "$path" -pathType leaf)) {
+		Write-Output "TIME,TEXT" > $path
+	}
+	Write-Output "$($timestamp),$text1 $text2 $text3" >> $path
+	$path = Resolve-Path $path
+
+	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
+	"✔️ Saved to $path in $($elapsed)s."
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
