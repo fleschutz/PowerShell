6bd855a888acb24ac86c2a0d340563cdc67653ad
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-06-21 08:16:02 +0200

    Added open-voice-recorder.ps1

---
+++ scripts/open-voice-recorder.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+        Launches the Voice Recorder app
+.DESCRIPTION
+        This PowerShell script launches the Windows Voice Recorder application.
+.EXAMPLE
+        PS> ./open-voice-recorder.ps1
+.LINK
+        https://github.com/fleschutz/PowerShell
+.NOTES
+        Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	Start-Process explorer.exe shell:appsFolder\Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe!App
+        exit 0 # success
+} catch {
+        "⚠️ Error: $($Error[0]) (script line $($_.InvocationInfo.ScriptLineNumber))"
+        exit 1
+}
