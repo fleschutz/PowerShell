14bb69aef015887a855ea6890950377bb5aa5083
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-09-04 18:06:25 +0200

    Updated new-ssh-key.ps1

--- scripts/new-ssh-key.ps1
+++ scripts/new-ssh-key.ps1
@@ -5,7 +5,8 @@
         This PowerShell script creates a new SSH key for the user.
 .EXAMPLE
         PS> ./new-ssh-key.ps1
-		✔️ Your public key: ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILb8s5zU9YDApGQ82H45fMKVPMr5cw9fzh3PEBjZZ+Rm markus@PI
+	✔️ New SSH key with Ed25519 encryption saved to ~/.ssh - your public key is:
+	    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILb8s5zU9YDApGQ82H45fMKVPMr5cw9fzh3PEBjZZ+Rm markus@PI
 .LINK
         https://github.com/fleschutz/PowerShell
 .NOTES
@@ -18,12 +19,15 @@ try {
 
 	if (Test-Path "~/.ssh/id_ed25519.pub") {
 		$publicKey = Get-Content "~/.ssh/id_ed25519.pub"
+		$enc = "Ed25519"
 	} elseif (Test-Path "~/.ssh/id_rsa.pub") {
 		$publicKey = Get-Content "~/.ssh/id_rsa.pub"
+		$enc = "RSA"
 	} else {
 		throw "No public key found."
 	}
-	"✔️ Your public key: $publicKey"
+	"✔️ New SSH key with $enc encryption saved to ~/.ssh - your public key is:"
+	"   $publicKey"
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
