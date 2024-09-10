ad73e693cbc670fa830b5458fec654e80e3fc7a4
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-09-10 16:03:05 +0200

    Updated list-wsl-distros.ps1

--- scripts/list-wsl-distros.ps1
+++ scripts/list-wsl-distros.ps1
@@ -1,4 +1,22 @@
-﻿& wsl.exe --list --online
-" "
-& wsl.exe --status
-exit 0 # success
+﻿<#
+.SYNOPSIS
+        Lists distros for Windows Subsystem for Linux
+.DESCRIPTION
+        This PowerShell script lists available Linux distributions for Windows Subsystem for Linux (WSL).
+.EXAMPLE
+        PS> ./list-wsl-distros.ps1
+.LINK
+        https://github.com/fleschutz/PowerShell
+.NOTES
+        Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	& wsl.exe --list --online
+	" "
+	& wsl.exe --status
+	exit 0 # success
+} catch {
+        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+        exit 1
+}
