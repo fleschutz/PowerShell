d01b5969eb8bac95c1cf14f5ae66f87fd3c8b5aa
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-10-01 13:42:27 +0200

    Added hello-world.ps1

---
+++ scripts/hello-world.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+	Sample Script
+.DESCRIPTION
+	This PowerShell script is a sample script writing "Hello World" to the console.
+.EXAMPLE
+	PS> ./hello-world.ps1
+	Hello World
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+Write-Output "Hello World"
+exit 0 # success
