b2413adad688472ad214beb00fe86e7bb224ee91
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-10-01 13:50:57 +0200

    Added cd-jenkins.ps1

---
+++ scripts/cd-jenkins.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+	Changes to the Jenkins home directory
+.DESCRIPTION
+	This PowerShell script changes the working directory to the Jenkins home directory.
+.EXAMPLE
+	PS> ./cd-jenkins
+	📂C:\Users\Markus\.jenkins
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	if (Test-Path "~/.jenkins" -pathType container) {
+		$path = "~/.jenkins"
+	} elseif (Test-Path "/var/snap/jenkins" -pathType container) {
+		$path = "/var/snap/jenkins"
+	} else {
+		throw "The Jenkins home directory doesn't exist (yet)"
+	}
+	Set-Location "$path"
+	"📂$path"
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
