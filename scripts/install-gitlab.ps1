435837b1cd8a71ea38b7fb5af31403743d7a6e44
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-10-15 13:13:46 +0200

    Added install-gitlab.ps1

---
+++ scripts/install-gitlab.ps1
@@ -0,0 +1,12 @@
+
+"1. Install and configure the necessary dependencies"
+& sudo apt-get update
+& sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
+
+
+# sudo apt-get install -y postfix
+
+"2. Add the GitLab package repository and install the package"
+& curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+
+
