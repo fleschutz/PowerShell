73a3646ab763d61e8b9ac857a34695b4ed08bd42
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-10-28 14:07:23 +0100

    Updated save-credentials.ps1

--- scripts/save-credentials.ps1
+++ scripts/save-credentials.ps1
@@ -2,26 +2,26 @@
 .SYNOPSIS
 	Saves credentials encrypted
 .DESCRIPTION
-	This PowerShell script asks for credentials and saves them encrypted into a target file ("$HOME\my.credentials" by default).
-.PARAMETER TargetFile
-	Specifies the target file ("$HOME\my.credentials" by default)
+	This PowerShell script asks for credentials and saves them encrypted into a target file.
+.PARAMETER targetFile
+	Specifies the target file ("~\my.credentials" by default)
 .EXAMPLE
 	PS> ./save-credentials.ps1
 	Enter username and password, please.
- 	✅ Your credentials have been saved encrypted into C:\Users\Markus\my.credentials
+ 	✅ Your credentials have been saved to C:\Users\Markus\my.credentials (encrypted).
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
 	Author: Markus Fleschutz | License: CC0
 #>
 
-param([string]$TargetFile = "$HOME\my.credentials")
+param([string]$targetFile = "~\my.credentials")
 
 try {
 	Write-Host "Enter username and password, please." -foreground red
 	$cred = Get-Credential
-	$cred.Password | ConvertFrom-SecureString | Set-Content "$TargetFile"
-	"✅ Your credentials have been saved encrypted into $TargetFile"
+	$cred.Password | ConvertFrom-SecureString | Set-Content "$targetFile"
+	"✅ Your credentials have been saved to $targetFile (encrypted)."
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
