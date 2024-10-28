b6e703d96e74ed71796f8982b160edd9954a2a43
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-10-28 14:20:47 +0100

    Updated list-fibonacci.ps1

--- scripts/list-fibonacci.ps1
+++ scripts/list-fibonacci.ps1
@@ -12,13 +12,13 @@
 	Author: Markus Fleschutz | License: CC0
 #>
 
-function fibonacci($n) {
+function fibo([int]$n) {
     if ($n -lt 2) { return 1 }
-    return (fibonacci($n - 1)) + (fibonacci($n - 2))
+    return (fibo($n - 1)) + (fibo($n - 2))
 }
 
 
 foreach ($i in 0..100) {
-	Write-Host "$(fibonacci $i), " -noNewline
+	Write-Host "$(fibo $i), " -noNewline
 }
 exit 0 # success
