312686d50f9cce2c5ad18fa84df3cf6d43926e3d
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-10-31 08:04:27 +0100

    Update ping-local-devices.ps1

--- scripts/ping-local-devices.ps1
+++ scripts/ping-local-devices.ps1
@@ -4,8 +4,8 @@
 .DESCRIPTION
         This PowerShell script pings devices in the local network and lists which one are up.
 .EXAMPLE
-        PS> ./ping-local-hosts.ps1
-	✅ Up: hippo jenkins01 jenkins02 rocket vega 
+        PS> ./ping-local-devices.ps1
+	✅ Up: epson raspi tux
 .LINK
         https://github.com/fleschutz/PowerShell
 .NOTES
@@ -16,7 +16,7 @@ param([int]$timeout = 600) # ms ping timeout
 
 
 try {
-	$names = @('accesspoint','AD','AP','amnesiac','archlinux','auriga','berlin','boston','brother','canon','castor','cisco','echodot','epson','epson2815','fedora','fireball','firewall','fritz.box','fritz.nas','fritz.powerline','fritz.repeater','gassensor','gateway','hippo','heizung','hodor','homemanager','io','iphone','jarvis','jenkins','LA','laptop','linux','jupiter','mars','mercury','miami','mobile','none','none-1','none-2','NY','o2.lte','octo','office','officepc','paris','PI','pixel-6a','PC','pluto','printer','proxy','R2D2','raspberry','rocket','rome','router','sentinel','server','shelly','shelly1','smartphone','smartwatch','soundbar','speedport.ip','sunnyboy','surface','switch','tablet','tau','tigercat','tolino','tux','TV','ubuntu','vega','venus','xrx','zeus') # sorted alphabetically
+	$names = @('accesspoint','AD','AP','amnesiac','archlinux','auriga','berlin','boston','brother','canon','castor','cisco','echodot','epson','epson2815','fedora','fireball','firewall','fritz.box','fritz.nas','fritz.powerline','fritz.repeater','gassensor','gateway','hippo','heizung','hodor','homemanager','io','iphone','jarvis','jenkins','LA','laptop','linux','jupiter','mars','mercury','miami','mobile','none','none-1','none-2','NY','o2.lte','octo','office','officepc','paris','PI','pixel-6a','PC','pluto','printer','proxy','R2D2','raspberry','raspi','rocket','rome','router','sentinel','server','shelly','shelly1','smartphone','smartwatch','soundbar','speedport.ip','sunnyboy','surface','switch','tablet','tau','tigercat','tolino','tux','TV','ubuntu','vega','venus','xrx','zeus') # sorted alphabetically
         $queue = [System.Collections.Queue]::new()
 	foreach($name in $names) { $ping = [System.Net.Networkinformation.Ping]::new()
 		$queue.Enqueue( @{Host=$name;Ping=$ping;Async=$ping.SendPingAsync($name,$timeout)} )
