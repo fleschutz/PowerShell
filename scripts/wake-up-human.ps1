8aeeb8b8112229fbb3ada1f20ed9a2a33111c671
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-11-22 11:26:06 +0100

    Add wake-up-human.ps1

---
+++ scripts/wake-up-human.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+	Wakes up an human
+.DESCRIPTION
+	This PowerShell script plays the sound of Big Ben to wake a human up.
+.EXAMPLE
+	PS> ./wake-up-human.ps1
+	(listen and enjoy)
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+& "$PSScriptRoot/play-mp3.ps1" "$PSScriptRoot/../data/sounds/Big Ben.mp3"
+exit 0 # success
