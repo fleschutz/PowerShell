c5432f1708b85250655f7b6178fc31008d6f0360
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-01-06 10:29:54 +0100

    Updated install-calibre-server.ps1

--- scripts/install-calibre-server.ps1
+++ scripts/install-calibre-server.ps1
@@ -5,6 +5,12 @@
 	This PowerShell script installs and starts a local Calibre server as background process.
 .PARAMETER port
 	Specifies the Web port number (8099 by default)
+.PARAMETER mediaFolder
+	Specifies the file path to the media ('/opt/Calibre Library' by default)
+.PARAMETER userDB
+	Specifies the file path to the user database ('/opt/CalibreUsers.sqlite' by default)
+.PARAMETER logfile
+	Specifies the file path to the log file ('/opt/CalibreServer.log' by default)
 .EXAMPLE
 	PS> ./install-calibre-server.ps1
 	⏳ (1/5) Updating package infos...
@@ -37,10 +43,10 @@ try {
 	if ($lastExitCode -ne "0") { throw "Can't execute 'calibre-server' - make sure Calibre server is installed and available" }
 
 	"`n⏳ (4/5) Creating media folder at: $mediaFolder ... (if non-existent)"
-	mkdir $mediaFolder
+	& mkdir $mediaFolder
 
 	"`n⏳ (5/5) Starting Calibre server as background process..."
-	& calibre-server --port $port --num-per-page 100 --userdb $userDB --log $logfile --daemonize $mediaFolder
+	& sudo calibre-server --port $port --num-per-page 100 --userdb $userDB --log $logfile --daemonize $mediaFolder
 
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
 	"✅ Installed and started Calibre server on port $port in $($elapsed)s."
