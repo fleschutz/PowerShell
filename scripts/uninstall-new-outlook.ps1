be9a93de8298cd50aac009388780b9f6686ec70b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-01-14 08:19:09 +0100

    Added uninstall-new-outlook.ps1

---
+++ scripts/uninstall-new-outlook.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Uninstalls the new Outlook
+.DESCRIPTION
+	This PowerShell script uninstalls the new Outlook application.
+.EXAMPLE
+	PS> ./uninstall-new-outlook.ps1
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+try {
+	"⏳ Uninstalling new Outlook..."
+
+	Remove-AppxProvisionedPackage -AllUsers -Online -PackageName (Get-AppxPackage Microsoft.OutlookForWindows).PackageFullName
+	
+	"✅ New Outlook has been removed."
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
