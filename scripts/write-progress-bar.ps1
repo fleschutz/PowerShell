0fecf9ce78e438b916cd18279ea9c33495bff190
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-02-19 09:09:53 +0100

    Added write-progress-bar.ps1

---
+++ scripts/write-progress-bar.ps1
@@ -0,0 +1,8 @@
+﻿$progressBar = @('⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷')
+$progressIndex = 0
+    
+do {
+	Write-Host "`r$($progressBar[$progressIndex]) Working on something..." -NoNewline
+	$progressIndex = ($progressIndex + 1) % $progressBar.Length
+	Start-Sleep -milliseconds 100
+} while ($true)
