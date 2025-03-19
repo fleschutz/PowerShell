3cca0d75d9e24b90b1ba14b8fda8f64662902d15
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-03-19 15:41:06 +0100

    Updated install-basic-snaps.ps1

--- scripts/install-basic-snaps.ps1
+++ scripts/install-basic-snaps.ps1
@@ -1,10 +1,11 @@
 ﻿<#
 .SYNOPSIS
-	Installs basic Linux snaps 
+	Installs basic Snap apps
 .DESCRIPTION
-	This PowerShell script installs 18 basic Linux snaps.
+	This PowerShell script installs 20 basic Snap apps.
 .EXAMPLE
 	PS> ./install-basic-snaps.ps1
+	⏳ Installing 20 Snap apps (sorted alphabetically)...
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -15,29 +16,30 @@ try {
 	$StopWatch = [system.diagnostics.stopwatch]::startNew()
 	if (!$IsLinux) { throw "Only Linux currently support snaps" }
 
-	"⏳ Installing 19 Snaps (sorted alphabetically)..."
-	sudo snap install ant
-	sudo snap install audacity
-	sudo snap install bashtop
-	sudo snap install chromium
-	sudo snap install cmake
-	sudo snap install cups
-	sudo snap install emacs --classic
-	sudo snap install ffmpeg --edge
-	sudo snap install firefox
-	sudo snap install gimp
-	sudo snap install go
-	sudo snap install gradle --classic
-	sudo snap install groovy --classic
-	sudo snap install hugo
-	sudo snap install jenkins --edge --classic 
-	sudo snap install nano --classic
-	sudo snap install nextcloud
-	sudo snap install node --classic
-	sudo snap install plexmediaserver
+	"⏳ Installing 20 Snap apps (sorted alphabetically)..."
+	& sudo snap install ant
+	& sudo snap install audacity
+	& sudo snap install bashtop
+	& sudo snap install blender
+	& sudo snap install chromium
+	& sudo snap install cmake
+	& sudo snap install cups
+	& sudo snap install emacs --classic
+	& sudo snap install ffmpeg --edge
+	& sudo snap install firefox
+	& sudo snap install gimp
+	& sudo snap install go
+	& sudo snap install gradle --classic
+	& sudo snap install groovy --classic
+	& sudo snap install hugo
+	& sudo snap install jenkins --edge --classic 
+	& sudo snap install nano --classic
+	& sudo snap install nextcloud
+	& sudo snap install node --classic
+	& sudo snap install plexmediaserver
 	
-	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"✅ installed 19 Snaps in $Elapsed sec"
+	[int]$elapsed = $StopWatch.Elapsed.TotalSeconds
+	"✅ Installed 20 Snap apps in $($elapsed)s."
 	exit 0 # success
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
