53345ebdcfc672bef72d0439099a571a5907ec89
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-05-19 10:02:14 +0200

    Added touch.ps1

---
+++ scripts/touch.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+	Create an empty file
+.DESCRIPTION
+	This PowerShell script creates a new empty file.
+.PARAMETER filename
+	Path and filename of the new file
+.EXAMPLE
+	PS> ./touch.ps1 test.txt
+	✅ Created a new empty file called 'test.txt'.
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$filename = "")
+
+try {
+	if ($filename -eq "") { $filename = Read-Host "Enter the filename" }
+	"" | Out-File $filename -encoding ASCII
+	"✅ Created a new empty file called '$filename'."
+	exit 0 # success
+} catch {
+	"⚠️ Error: $($Error[0])"
+	exit 1
+}
