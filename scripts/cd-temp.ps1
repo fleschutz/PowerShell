e3f3c0b3996abe4dd6ecc40fe39c415c7c61d83e
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-07-08 15:58:38 +0200

    Updated cd-temp.ps1

--- scripts/cd-temp.ps1
+++ scripts/cd-temp.ps1
@@ -2,10 +2,10 @@
 .SYNOPSIS
 	Sets the working directory to the temporary folder
 .DESCRIPTION
-	This PowerShell script changes the working directory to the temporary folder.
+	This PowerShell script changes the current working directory to the temporary folder.
 .EXAMPLE
-	PS> ./cd-temp
-	📂C:\Users\Markus\AppData\Local\Temp (has 2 files and 3 subfolders)
+	PS> ./cd-temp.ps1
+	📂C:\Users\Markus\AppData\Local\Temp with 2 files and 3 subfolders entered.
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -15,21 +15,21 @@
 function GetTempDir {
         if ("$env:TEMP" -ne "") { return "$env:TEMP" }
         if ("$env:TMP" -ne "")  { return "$env:TMP" }
-        if ($IsLinux) { return "/tmp" }
+	if ($IsLinux -or $IsMacOS) { return "/tmp" }
         return "C:\Temp"
 }
 
 try {
 	$path = GetTempDir
 	if (-not(Test-Path "$path" -pathType container)) { throw
-		"No temporary folder at $path"
+		"No temporary folder at: $path"
 	}
 	Set-Location "$path"
 	$files = Get-ChildItem $path -attributes !Directory
 	$folders = Get-ChildItem $path -attributes Directory
-	"📂$path entered (has $($files.Count) files and $($folders.Count) subfolders)"
+	"📂$path with $($files.Count) files and $($folders.Count) subfolders entered."
 	exit 0 # success
 } catch {
-	"⚠️ Error: $($Error[0])"
+	"⚠️ ERROR: $($Error[0])"
 	exit 1
 }
