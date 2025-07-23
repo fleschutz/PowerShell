2790065a7c21cdde8d5f6216b9f9677b7146637d
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-07-23 09:01:20 +0200

    Renamed to 'remove-dir.ps1'

---
+++ scripts/remove-dir.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.SYNOPSIS
+        Removes a directory
+.DESCRIPTION
+        This PowerShell script removes the given directory tree recursively.
+	NOTE: Use with care! This cannot be undone!
+.PARAMETER pathToDir
+        Specifies the file path to the directory tree
+.EXAMPLE
+        PS> ./remove-dir.ps1 C:\Temp
+	⏳ Removing 'C:\Temp'...      (please wait)"
+	✅ Directory C:\Temp\ removed (took 9s).
+.LINK
+        https://github.com/fleschutz/PowerShell
+.NOTES
+        Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$pathToDir = "")
+
+try {
+	if ($pathToDir -eq "" ) { $pathToDir = Read-Host "Enter the file path to the obsolete directory" }
+	$stopWatch = [system.diagnostics.stopwatch]::startNew()
+	"⏳ Removing '$pathToDir'...      (please wait)"
+
+	Remove-Item -Force -Recurse -Confirm:$false $pathToDir
+	if ($lastExitCode -ne 0) { throw "'Remove-Item' failed with exit code $lastExitCode" }
+
+	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
+	"✅ Directory $pathToDir removed (took $($elapsed)s)."
+	exit 0 # success
+} catch {
+        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+        exit 1
+}
