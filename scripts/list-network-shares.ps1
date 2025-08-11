6e7ac68a6a060895cb688492297be4efbd6ec1f5
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-08-11 13:30:54 +0200

    Updated list-network-shares.ps1

--- scripts/list-network-shares.ps1
+++ scripts/list-network-shares.ps1
@@ -5,7 +5,7 @@
 	This PowerShell script lists all network shares (aka "shared folders") of the local computer.
 .EXAMPLE
 	PS> ./list-network-shares.ps1
-	✅ Shared folder \\LAPTOP\Public ("File transfer folder") -> D:\Public 
+	✅ Shared folder \\LAPTOP\Public -> D:\Public ("File transfer folder")
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -13,12 +13,16 @@
 #>
 
 try {
-	if ($IsLinux) {
+	if ($IsLinux -or $IsMacOS) {
 		# TODO
 	} else {
 		$shares = Get-WmiObject win32_share | where {$_.name -NotLike "*$"} 
 		foreach ($share in $shares) {
-			Write-Output "✅ Shared folder \\$(hostname)\$($share.Name) (`"$($share.Description)`") -> $($share.Path)"
+			if ($share.Description -eq "") {
+				Write-Host "✅ Shared folder \\$(hostname)\$($share.Name) -> $($share.Path)"
+			} else {
+				Write-Host "✅ Shared folder \\$(hostname)\$($share.Name) -> $($share.Path) (`"$($share.Description)`")"
+			}
 		}
 	}
 	exit 0 # success
