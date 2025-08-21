a9096dd2adfaeeae2410ec3392e5960f214d316a
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-08-21 17:19:33 +0200

    Updated calculate-BMI.ps1

--- scripts/calculate-BMI.ps1
+++ scripts/calculate-BMI.ps1
@@ -5,6 +5,8 @@
 	This PowerShell script calculates the BMI.
 .EXAMPLE
 	PS> ./calculate-BMI.ps1
+	Enter your height in m :
+	..g
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -17,7 +19,15 @@ try {
 	[float]$height = Read-Host("Enter your height in m ")
 	[float]$weight = Read-Host("Enter your weight in kg")
 	$BMI = $weight / ($height * $height)
-	"Your BMI is $BMI, for adults the WHO regards <16 as Underweight (severe thinness), 16-17 as Underweight (moderate thinness), 17-18.5 as Underweight (mild thinness), 18.5-25 as Normal range, 25-30 as Overweight (pre-obese), 30-35 as Obese (class I), 35-40 as Obese (class II), and >=40 as Obese (class III)."
+	"Your BMI is $BMI, for adults the WHO regards:"
+	" <16     as Underweight (severe thinness)"
+	" 16-17   as Underweight (moderate thinness)"
+	" 17-18.5 as Underweight (mild thinness)"
+	" 18.5-25 as Normal range"
+	" 25-30   as Overweight (pre-obese)"
+	" 30-35   as Obese (class I)"
+	" 35-40   as Obese (class II)"
+	" >=40    as Obese (class III)"
 	exit 0 # success
 } catch {
 	"⚠️ ERROR: $($Error[0]) in script line $($_.InvocationInfo.ScriptLineNumber)."
