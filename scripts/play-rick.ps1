2937d46a9b0380bffd68ba057bf88376fc1ea1ea
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-08-25 16:21:08 +0200

    Further improved play-rick.ps1

--- scripts/play-rick.ps1
+++ scripts/play-rick.ps1
@@ -14,9 +14,10 @@
 
 $host.ui.RawUI.WindowTitle = "You've been Rick-Rolled! 🤣"
 & "$PSScriptRoot/open-URL.ps1" "https://www.youtube.com/watch?v=v7ScGV5128A"
+& "$PSScriptRoot/show-notification.ps1" "You've been Rick-Rolled!"
+& "$PSScriptRoot/speak-english.ps1" "You've been Rick-Rolled!"
 if ($IsLinux -or $IsMacOS) {
 	& curl ascii.live/rick
 } else {
 	& curl.exe ascii.live/rick
 }
-exit 0 # success
