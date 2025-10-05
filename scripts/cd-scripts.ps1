8ade091fa477f8b9a951508724dec9574a09e2af
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-10-05 11:49:33 +0200

    Updated cd-scripts.ps1

--- scripts/cd-scripts.ps1
+++ scripts/cd-scripts.ps1
@@ -5,7 +5,7 @@
 	This PowerShell script changes the currrent working directory to the PowerShell scripts folder.
 .EXAMPLE
 	PS> ./cd-scripts.ps1
-	📂C:\Repos\PowerShell\scripts entered (has 655 scripts)
+	📂C:\Repos\PowerShell\scripts entered, has 655 scripts.
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -19,7 +19,7 @@ try {
 	if (-not(Test-Path "$path" -pathType container)) { throw "No scripts folder at: $path" }
 	Set-Location "$path"
 	$files = Get-ChildItem $path -attributes !Directory
-	"📂$path entered (has $($files.Count) scripts)"
+	"📂$path entered, has $($files.Count) scripts."
 	exit 0 # success
 } catch {
 	"⚠️ ERROR: $($Error[0])"
