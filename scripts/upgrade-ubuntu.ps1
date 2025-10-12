96dee6c70875731a79347b5a579a450763dec860
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-10-12 18:38:39 +0200

    Improved upgrade-ubuntu.ps1

--- scripts/upgrade-ubuntu.ps1
+++ scripts/upgrade-ubuntu.ps1
@@ -12,41 +12,49 @@
 #>
 
 try {
+	"⏳ (1/4) Checking requirements..." -foregroundColor green
+	& "$PSScriptRoot/check-power.ps1"
+	& "$PSScriptRoot/check-smart-devices.ps1"
+	if ($IsLinux -or $IsMacOS) {
+		& "$PSScriptRoot/check-drive-space.ps1" /
+	} else {
+		& "$PSScriptRoot/check-drive-space.ps1" C
+	}
+	& "$PSScriptRoot/check-swap-space.ps1"
+	"NOTE: It's strongly recommended to backup your data BEFORE the upgrade!"
+	$confirm = Read-Host "Press <Return> to continue..."
 	""
-	"⏳ (1/4) Perform a backup!"
-	"It's strongly recommended to backup your data BEFORE the upgrade!"
-	$Confirm = Read-Host "Press <Return> to continue..."
-
-	""
-	"⏳ (2/4) Install update-manager-core, Upgrade Packages & Reboot"
-	$Confirm = Read-Host "Enter <yes> to perform this step (otherwise it will be skipped)"
-	if ($Confirm -eq "yes") {
+	"⏳ (2/4) Upgrade installed packages, then reboot"
+	$confirm = Read-Host "Enter <yes> to perform this step (otherwise it will be skipped)"
+	if ($confirm -eq "yes") {
 		sudo apt install update-manager-core
+		"⏳ Updating..."
 		sudo apt update
 		sudo apt list --upgradable
+		"⏳ Upgrading..."
 		sudo apt upgrade
+		"⏳ Rebooting..."
 		sudo reboot 
 	}
-
 	""
 	"⏳ (3/4) Remove obsolete kernel modules"
-	$Confirm = Read-Host "Enter <yes> to perform this step (otherwise it will be skipped)"
-	if ($Confirm -eq "yes") {
+	$confirm = Read-Host "Enter <yes> to perform this step (otherwise it will be skipped)"
+	if ($confirm -eq "yes") {
 		sudo apt --purge autoremove
 	}
-
 	""
-	"⏳ (4/4) Upgrade Ubuntu & reboot"
-	$Confirm = Read-Host "Enter <LTS> to upgrade to latest LTS release, <latest> to upgrade to latest Ubuntu release (otherwise this step will be skipped)"
-	if ($Confirm -eq "LTS") {
+	"⏳ (4/4) Upgrade Ubuntu OS, then reboot"
+	$confirm = Read-Host "Enter <LTS> to upgrade to latest LTS release, <latest> to upgrade to latest Ubuntu release (otherwise this step will be skipped)"
+	if ($confirm -eq "LTS") {
 		sudo do-release-upgrade
+		"⏳ Rebooting..."
 		sudo reboot
-	} elseif ($Confirm -eq "latest") {
+	} elseif ($confirm -eq "latest") {
 		sudo do-release-upgrade -d
+		"⏳ Rebooting..."
 		sudo reboot
 	}
-
-	"✅  Done."
+	"✅ Update done."
 	exit 0 # success
 } catch {
 	"⚠️ ERROR: $($Error[0]) (script line $($_.InvocationInfo.ScriptLineNumber))"
