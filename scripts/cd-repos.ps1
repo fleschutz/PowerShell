888af9bf975f79c3dd8832f8dc5fa2a97b9fc11f
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-11-01 11:45:36 +0100

    Updated cd-repos.ps1

--- scripts/cd-repos.ps1
+++ scripts/cd-repos.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Sets the working dir to the repos folder
 .DESCRIPTION
-	This PowerShell script changes the current working directory to the folder for Git repositories.
+	This PowerShell script changes the current working directory to the Git repositories folder.
 .EXAMPLE
 	PS> ./cd-repos.ps1
 	📂C:\Repos entered, has 33 subfolders.
@@ -19,12 +19,19 @@ try {
 	} elseif (Test-Path "~/repos"        -pathType container) { $path = "~/repos"
 	} elseif (Test-Path "~/Repositories" -pathType container) { $path = "~/Repositories"
 	} elseif (Test-Path "~/repositories" -pathType container) { $path = "~/repositories"
+	} elseif (Test-Path "~/Git"          -pathType container) { $path = "~/Git"
+	} elseif (Test-Path "~/git"          -pathType container) { $path = "~/git"
 	} elseif (Test-Path "/Repos"         -pathType container) { $path = "/Repos"
 	} elseif (Test-Path "/repos"         -pathType container) { $path = "/repos"
 	} elseif (Test-Path "/Repositories"  -pathType container) { $path = "/Repositories"
 	} elseif (Test-Path "/repositories"  -pathType container) { $path = "/repositories"
+	} elseif (Test-Path "/Git"           -pathType container) { $path = "/Git"
+	} elseif (Test-Path "/git"           -pathType container) { $path = "/git"
 	} elseif (Test-Path "~/source/repos" -pathType container) { $path = "~/source/repos" # Visual Studio default
-	} elseif (Test-Path "D:/Repos"	     -pathType container) { $path = "D:/Repos"       # second HDD
+	} elseif (Test-Path "D:/Repos"	     -pathType container) { $path = "D:/Repos"       # on HDD
+	} elseif (Test-Path "D:/Repositories" -pathType container) { $path = "D:/Repositories" # on HDD
+	} elseif (Test-Path "D:/Git"	     -pathType container) { $path = "D:/Git"         # on HDD
+	} elseif (Test-Path "D:/git"	     -pathType container) { $path = "D:/git"         # on HDD
 	} else { throw "Found no folder for Git repositories (in home or root directory) - Please create one." }
 	$path = Resolve-Path $path
 	Set-Location "$path"
