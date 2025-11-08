5e6aa7727f3e00eee1ca0c4be15c5612f3a0812c
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-11-08 09:12:32 +0100

    Updated cd-autostart.ps1

--- scripts/cd-autostart.ps1
+++ scripts/cd-autostart.ps1
@@ -1,11 +1,11 @@
 ﻿<#
 .SYNOPSIS
-	Sets the working directory to the user's autostart folder
+	Sets the working directory to the autostart folder
 .DESCRIPTION
-	This PowerShell script changes the working directory to the user's autostart folder.
+	This PowerShell script changes the current working directory to the autostart folder.
 .EXAMPLE
 	PS> ./cd-autostart.ps1
-	📂C:\Users\Markus\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup entered (has 0 files)
+	📂C:\Users\Markus\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup entered, has 0 files.
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -17,11 +17,11 @@
 try {
 	if ($IsLinux -or $IsMacOS) { throw "This script requires Windows" }
 
-	$path = Resolve-Path "~/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+	$path = Resolve-Path "$($env:APPDATA)/Microsoft/Windows/Start Menu/Programs/Startup"
 	if (-not(Test-Path "$path" -pathType container)) { throw "The path to autostart folder '$path' doesn't exist (yet)" }
 	Set-Location "$path"
 	$files = Get-ChildItem $path -attributes !Directory
-	"📂$path entered (has $($files.Count) files)"
+	"📂$path entered, has $($files.Count) files."
 	exit 0 # success
 } catch {
 	"⚠️ ERROR: $($Error[0])"
