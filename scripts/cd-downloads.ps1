00c7d2eb7691f4c2940ba64700a1eb2cd1bff7be
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-11-24 11:23:22 +0100

    Updated cd-downloads.ps1

--- scripts/cd-downloads.ps1
+++ scripts/cd-downloads.ps1
@@ -5,7 +5,7 @@
 	This PowerShell script changes the working directory to the user's downloads folder.
 .EXAMPLE
 	PS> ./cd-downloads.ps1
-	📂C:\Users\Markus\Downloads with 0 files and 0 folders entered.
+	📂C:\Users\Markus\Downloads entered, has 0 files and 0 folders.
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -23,10 +23,10 @@ try {
 		$path = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
 		if (-not(Test-Path "$path" -pathType container)) { throw "The path to downloads folder '$path' doesn't exist (yet)" }
 	}
-	Set-Location "$path"
+	Set-Location $path
 	$files = Get-ChildItem $path -attributes !Directory
 	$folders = Get-ChildItem $path -attributes Directory
-	"📂$path with $($files.Count) files and $($folders.Count) folders entered."
+	"📂$path entered, has $($files.Count) files and $($folders.Count) folders."
 	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0])"
