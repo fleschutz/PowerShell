8fe10e22b9e9fe904a56f6f909df2796b5b09f20
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-01-07 16:38:27 +0100

    Improved list-old-branches.ps1

--- scripts/list-old-branches.ps1
+++ scripts/list-old-branches.ps1
@@ -2,37 +2,42 @@
 .SYNOPSIS
 	Lists old Git branches
 .DESCRIPTION
-	This PowerShell script lists branches in a Git repository matching: (1) search pattern, (2) already merged, (3) older than 2024
+	This PowerShell script only lists Git repository branches matching: (1) branch name pattern + (2) branch was already merged + (3) last commit pattern
 .PARAMETER pathToRepo
 	Specifies the path to the Git repository (current working directory by default)
+.PARAMETER branchNamePattern
+	Specifies the branch names
+.PARAMETER lastCommitPattern
+	Specifies the last commit dates
 .EXAMPLE
-	PS> ./list-old-branches.ps1 C:\Repos\UFA
+	PS> ./list-old-branches.ps1 C:\Repos\ninja
 q#>
 
-param([string]$pathToRepo = "$PWD", [string]$searchPattern = "origin/dev/*")
+param([string]$pathToRepo = "$PWD", [string]$branchNamePattern = "origin/dev/mf/*", [string]$lastCommitPattern = "2021-*")
 
 try {
-	"(1/3) Searching for Git executable...    "
+	Write-Host "(1/3) Searching for Git executable...    " -noNewline
         & git --version
         if ($lastExitCode -ne 0) { throw "Can't execute 'git' - make sure Git is installed and available" }
 
-        "(2/3) Checking local repository..."
+        "(2/4) Checking local repository..."
         if (!(Test-Path "$pathToRepo" -pathType container)) { throw "Can't access repo folder: $pathToRepo" }
 
-	"(3/3) Querying already merged branches with name '$searchPattern'..."
-	$branches = $(git -C "$pathToRepo" branch --list --remotes --no-color --no-column --merged HEAD "$searchPattern")
+	"(3/4) Querying branches matching: branch-name='$branchNamePattern' + already merged"
+	$branches = $(git -C "$pathToRepo" branch --list --remotes --no-color --no-column --merged HEAD "$branchNamePattern")
 	if ($lastExitCode -ne 0) { throw "'git branch --list' failed" }
 
+	"(4/4) Listing branches matching: last-commit-date='$lastCommitPattern'"
 	""
-	"LAST COMMIT DATE   BRANCH NAME"
-	"----------------   -----------"
+	"DATE OF LAST COMMIT  BRANCH NAME"
+	"-------------------  -----------"
 	foreach($branch in $branches) {
 		$branchName = $branch.Trim() # remove leading spaces
 
 		$dateOfLastCommit = (git -C "$pathToRepo" log -1 --format=%cs $branchName)
-		if ($dateOfLastCommit -like "2024-*") { continue} # too young
+		if ($dateOfLastCommit -notlike $lastCommitPattern) { continue} # too young
 
-		"$dateOfLastCommit         $branchName"
+		"$dateOfLastCommit           $branchName"
 	}
 	exit 0 # success
 } catch {
