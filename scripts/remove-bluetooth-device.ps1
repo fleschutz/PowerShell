196e0dad41dbb225ba0b568ccee66dd9e2f6720d
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-02-17 14:25:12 +0100

    Improved remove-bluetooth-device.ps1

--- scripts/remove-bluetooth-device.ps1
+++ scripts/remove-bluetooth-device.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 .SYNOPSIS
 	Removes a Bluetooth device
 .DESCRIPTION
@@ -32,30 +32,33 @@ function Get-BTDevice {
 				@{N='Address';E={[uInt64]('0x{0}' -f $_.HardwareID[0].Substring(12))}}
 }
 
-################## Execution Begins Here ################
+try {
+	"⏳ Querying Bluetooth devices..."
+	$BTR       = Add-Type -MemberDefinition $Source -Name "BTRemover"  -Namespace "BStuff" -PassThru
+	$BTDevices = @(Get-BTDevice) # Force array if null or single item
+	if ($BTDevices.Count -eq 0) { throw "No Bluetooth devices found" }
 
-$BTR       = Add-Type -MemberDefinition $Source -Name "BTRemover"  -Namespace "BStuff" -PassThru
-$BTDevices = @(Get-BTDevice) # Force array if null or single item
-
-Do {
-	If ($BTDevices.Count) {
-		"`n******** Bluetooth Devices ********`n" | Write-Host
-		For ($i=0; $i -lt $BTDevices.Count; $i++) {
-			('{0,5} - {1} ({2}/{3}/{4})' -f ($i+1), $BTDevices[$i].FriendlyName, $BTDevices[$i].Status, $BTDevices[$i].Class, $BTDevices[$i].Address) | Write-Host
-		}
-		$selected = Read-Host "`nSelect a device to remove (0 to Exit)"
-		If ([int]$selected -in 1..$BTDevices.Count) {
-			'Removing device: {0}' -f $BTDevices[$Selected-1].FriendlyName | Write-Host
-			$Result = $BTR::Unpair($BTDevices[$Selected-1].Address)
-			If (!$Result) {
-				"Device removed successfully." | Write-Host
-			} Else {
-				("Sorry, an error occured. Return was: {0}" -f $Result) | Write-Host
+	do {
+		if ($BTDevices.Count) {
+			for ($i=0; $i -lt $BTDevices.Count; $i++) {
+				('{0,5} - {1} ({2}/{3}/{4})' -f ($i+1), $BTDevices[$i].FriendlyName, $BTDevices[$i].Status, $BTDevices[$i].Class, $BTDevices[$i].Address) | Write-Host
 			}
+			$selected = Read-Host "`nSelect a device to remove (0 to Exit)"
+			If ([int]$selected -in 1..$BTDevices.Count) {
+				'Removing device: {0}' -f $BTDevices[$Selected-1].FriendlyName | Write-Host
+				$Result = $BTR::Unpair($BTDevices[$Selected-1].Address)
+				If (!$Result) {
+					"Device removed successfully." | Write-Host
+				} Else {
+					("Sorry, an error occured. Return was: {0}" -f $Result) | Write-Host
+				}
+			}
+		} else {
+			"No more Bluetooth devices found"
 		}
-	} Else {
-		"`n********* No devices found ********" | Write-Host
-	}
-} While (($BTDevices = @(Get-BTDevice)) -and [int]$selected)
-Write-Host "`n`nPress any key to exit...";
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+	} while (($BTDevices = @(Get-BTDevice)) -and [int]$selected)
+	exit 0 # success
+} catch {
+	"⚠️ ERROR: $($Error[0]) (script line $($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
