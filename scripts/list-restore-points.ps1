3767e770868f4a589f9f5c77d24c0ff350a494d4
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-03-02 14:53:44 +0100

    Added list-restore-points.ps1

---
+++ scripts/list-restore-points.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+	Lists restore points
+.DESCRIPTION
+	This PowerShell script lists the system restore points of the local computer.
+.EXAMPLE
+	PS> ./list-restore-points.ps1 
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+#requires -version 5.1 -RunAsAdministrator
+
+try {
+	Get-ComputerRestorePoint
+	if ($lastExitCode -ne 0) { throw "Executing Get-ComputerRestorePoint has failed with exit code $lastExitCode" }
+
+	exit 0 # success
+} catch {
+	"⚠️ ERROR: $($Error[0]) (in script line $($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
