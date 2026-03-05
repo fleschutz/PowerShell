0e46c574edbfb8421adb23ce2ad7c1d980afb7e0
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-03-05 12:33:49 +0100

    Updated ping-host.ps1

--- scripts/ping-host.ps1
+++ scripts/ping-host.ps1
@@ -7,7 +7,7 @@
 	Specifies the hostname or IP address to ping (x.com by default)
 .EXAMPLE
 	PS> ./ping-host.ps1 x.com
-	⏳ Pinging 'x.com'... online with a 20ms ping to it's IP 104.244.42.1
+	⏳ Pinging...   from 'Laptop' -> 20ms -> 'x.com' (IP 104.244.42.1)
 .LINK
 	https://github.com/fleschutz/PowerShell
 .NOTES
@@ -17,20 +17,20 @@
 param([string]$hostname = "x.com")
 
 try {
-	Write-Host "⏳ Pinging '$hostname'... " -noNewline
+	Write-Host "⏳ Pinging... " -noNewline
 	$remoteHosts = $hostname.Split(",")
 	$tasks = $remoteHosts | foreach { (New-Object Net.NetworkInformation.Ping).SendPingAsync($_,5000) }
 	[Threading.Tasks.Task]::WaitAll($tasks)
 	foreach($ping in $tasks.Result) {
 		if ($ping.Status -eq "Success") {
-			Write-Host "online with a $($ping.RoundtripTime / 2)ms ping to it's IP $($ping.Address)"
+			Write-Host "  from '$(hostname)' -> $($ping.RoundtripTime / 2.0)ms -> '$($hostname)' (IP $($ping.Address))"
 			exit 0 # success
 		} else {
-			Write-Host "No reply for 5sec from IP $($ping.Address) - check the connection or maybe the host is down."
+			Write-Host "No reply from IP $($ping.Address) for 5sec - check the connection or maybe the host is down."
 			exit 1
 		}
 	}
-	Write-Host "No reply for 5sec - check the connection or maybe the host is down."
+	Write-Host "No reply from '$($hostname)' for 5sec - check the connection or maybe the host is down."
 	exit 1
 } catch {
 	"⚠️ ERROR: $($Error[0]) (script line $($_.InvocationInfo.ScriptLineNumber))"
