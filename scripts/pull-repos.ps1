cce4a4873e4bdfa1da60193dfc6007333de439ad
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-03-08 17:42:11 +0100

    Updated pull-repos.ps1

--- scripts/pull-repos.ps1
+++ scripts/pull-repos.ps1
@@ -7,9 +7,9 @@
 	Specifies the path to the parent folder
 .EXAMPLE
 	PS> ./pull-repos C:\MyRepos
-	⏳ (1) Searching for Git executable...       git version 2.43.0
-	⏳ (2) Checking parent folder...             33 subfolders
-	⏳ (3/35) Pulling into 'base256U'...
+	⏳ Searching for Git executable...       git version 2.43.0
+	⏳ Checking parent folder...             33 subfolders
+	⏳ Pulling into 'base256U' (1/33)...
 	...
 .LINK
 	https://github.com/fleschutz/PowerShell
@@ -22,22 +22,22 @@ param([string]$parentDir = "$PWD")
 try {
 	$stopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	Write-Host "⏳ (1) Searching for Git executable...`t`t" -NoNewline
+	Write-Host "⏳ Searching for Git executable...`t`t" -noNewline
 	& git --version
 	if ($lastExitCode -ne 0) { throw "Can't execute 'git' - make sure Git is installed and available" }
 
-	Write-Host "⏳ (2) Checking parent folder...`t`t" -NoNewline
+	Write-Host "⏳ Checking parent folder...`t`t`t" -noNewline
 	if (-not(Test-Path "$parentDir" -pathType container)) { throw "Can't access folder: $parentDir" }
 	$folders = (Get-ChildItem "$parentDir" -attributes Directory)
 	$numFolders = $folders.Count
 	$parentDirName = (Get-Item "$parentDir").Name
 	Write-Host "$parentDir with $numFolders subfolders"
 
-	[int]$step = 3
+	[int]$step = 1
 	[int]$numFailed = 0
 	foreach ($folder in $folders) {
 		$folderName = (Get-Item "$folder").Name
-		Write-Host "⏳ ($step/$($numFolders + 2)) Pulling into '$folderName'...`t`t" -NoNewline
+		Write-Host "⏳ Pulling into '$folderName' ($step/$($numFolders))...`t`t" -noNewline
 
 		& git -C "$folder" pull --recurse-submodules=yes
 		if ($lastExitCode -ne 0) { $numFailed++; Write-Warning "'git pull' into 📂$folderName failed" }
@@ -48,10 +48,10 @@ try {
 	}
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
 	if ($numFailed -eq 0) {
-		"✅ Pulled updates into $numFolders Git repos at 📂$parentDir in $($elapsed)s."
+		"✅ Updated $numFolders Git repos at 📂$parentDir in $($elapsed)s."
 		exit 0 # success
 	} else {
-		"⚠️ Pulled updates into $numFolders Git repos at 📂$parentDir but $numFailed failed (took $($elapsed)s)!"
+		"⚠️ Updated $numFolders Git repos at 📂$parentDir but $numFailed failed (took $($elapsed)s)!"
 		exit 1
 	}
 } catch {
