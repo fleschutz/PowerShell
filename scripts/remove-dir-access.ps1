b2536549c5bda4e0559346a005ea877def1bbb7a
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-03-16 16:14:04 +0100

    Added remove-dir-access.ps1

---
+++ scripts/remove-dir-access.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+	Removes directory access
+.DESCRIPTION
+	This PowerShell script denies access of the current user to a directory.
+.PARAMETER directory
+	Specified the path to the diretory
+.EXAMPLE
+	PS> ./remove-dir-access C:\Temp
+.LINK
+        https://github.com/fleschutz/PowerShell
+.NOTES
+        Author: Markus Fleschutz | License: CC0
+#>
+
+#requires -version 5.1
+
+param([string]$directory = "$PWD")
+
+try {
+	$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+	$acl = Get-ACL $directory
+
+	$denyRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
+	    $currentUser, "FullControl", "ContainerInherit,ObjectInherit", "None", "FullControl")
+
+	$acl.SetAccessRule($denyRule)
+
+	Set-ACL $Directory $acl
+
+	"✔️ Access for $currentUser to $directory is denied now."
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
