7052fab226d0b26358044c236e6fc90d71a756bf
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-04-20 19:45:33 +0200

    Added unzip-file.ps1

---
+++ scripts/unzip-file.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Unzips a file
+.DESCRIPTION
+	This PowerShell script unzips a file (.ZIP suffix).
+.PARAMETER path
+	Specifies the path to the .ZIP file.
+.EXAMPLE
+	PS> ./unzip-file.ps1 C:\file.zip
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$path = "")
+
+try {
+	if ($path -eq "" ) { $path = Read-Host "Enter the path to the .ZIP file" }
+	$stopWatch = [system.diagnostics.stopwatch]::startNew()
+
+	"⏳ Extracting $path to $pwd ..."
+	$fullPath = Get-ChildItem -Path $pwd -Filter $path | ForEach-Object { $_.FullName }
+
+	Expand-Archive -Path $fullPath -DestinationPath $pwd
+
+	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
+	"✅  File unzipped in $($elapsed)s."
+	exit 0 # success
+} catch {
+	"⚠️ ERROR: $($Error[0]) (at line $($_.InvocationInfo.ScriptLineNumber))"
+	exit 1
+}
