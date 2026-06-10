d5a3846c99ae1497bcbec9a65dd74c683a9ac8c4
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-06-10 16:37:53 +0200

    Updated clear-caches.ps1 and uninstall-bloatware.ps1

--- scripts/clear-caches.ps1
+++ scripts/clear-caches.ps1
@@ -14,25 +14,29 @@
 try {
 	$stopWatch = [system.diagnostics.stopwatch]::startNew()
 
-	"⏳ (1/7) Clearing DNS client cache..."
-	Clear-DnsClientCache
-
-	"⏳ (2/7) Clearing Windows Prefetch folder..."
-	Remove-Item -Path "$env:SystemRoot\Prefetch\*" -Force -ErrorAction SilentlyContinue
-
-	"⏳ (3/7) Clearing Windows Temp folder..."
-	Remove-Item -Path "$env:SystemRoot\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
-
-	"⏳ (4/7) Clearing User Temp folder..."
-	Remove-Item -Path "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
-
-	"⏳ (5/7) Clearing Internet Explorer Cache folder..."
-	Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\Windows\INetCache\*" -Recurse -Force -ErrorAction SilentlyContinue
-
-	"⏳ (6/7) Clearing Recycle Bin..."
+	"⏳ (1/8) Clearing Recycle Bin..."
 	Clear-RecycleBin -Confirm:$false
 
-	"⏳ (7/7) Performing Disk Cleanup..."
+	"⏳ (2/8) Clearing DNS client cache..."
+	Clear-DnsClientCache
+
+	"⏳ (3/8) Clearing Windows Prefetch folder..."
+	Remove-Item -Path "$env:SystemRoot\Prefetch\*" -Force -ErrorAction SilentlyContinue
+
+	"⏳ (4/8) Clearing Windows Temp folder..."
+	Remove-Item -Path "$env:SystemRoot\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+	"⏳ (5/8) Clearing User Temp folder..."
+	Remove-Item -Path "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+	"⏳ (6/8) Clearing File Explorer icon cache database..."
+	Remove-Item -Path "$env:LOCALAPPDATA\IconCache.db" -Force -ErrorAction SilentlyContinue
+	Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\Windows\Explorer\*.db" -Force -ErrorAction SilentlyContinue
+
+	"⏳ (7/8) Clearing Internet Explorer Cache folder..."
+	Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\Windows\INetCache\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+	"⏳ (8/8) Performing Disk Cleanup..."
 	& cleanmgr /sagerun:1
 
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
--- scripts/uninstall-bloatware.ps1
+++ scripts/uninstall-bloatware.ps1
@@ -11,55 +11,71 @@
         Author: Markus Fleschutz | License: CC0
 #>
 
-"⏳ (1/3) Removing System Applications..."
-winget uninstall 'OMEN Audio Control'
-winget uninstall 'OMEN Gaming Hub'
-winget uninstall 'Clipchamp'
-winget uninstall 'Cortana'
-winget uninstall 'Microsoft News'
-winget uninstall 'MSN Weather'
-winget uninstall 'Get Help'
-winget uninstall 'Microsoft Tips'
-winget uninstall 'Office'
-winget uninstall 'Microsoft.Office'
-winget uninstall 'Microsoft Solitaire Collection'
-winget uninstall 'Solitaire & Casual Games'
-winget uninstall 'Microsoft Sticky Notes'
-winget uninstall 'Paint'
-winget uninstall 'Microsoft People'
-winget uninstall 'Power Automate'
-winget uninstall 'Microsoft To Do'
-winget uninstall 'Microsoft Photos'
-winget uninstall 'Windows Camera'
-winget uninstall 'Windows Clock'
-winget uninstall 'Windows Calculator'
-winget uninstall 'Feedback Hub'
-winget uninstall 'Windows Maps'
-winget uninstall 'Windows Voice Recorder'
-winget uninstall 'Your Phone'
-winget uninstall 'Windows Media Player'
-winget uninstall 'Movies & TV'
-winget uninstall 'Microsoft Family'
-winget uninstall 'Quick Assist'
-winget uninstall 'Microsoft Teams'
-winget uninstall 'Mail and Calendar'
-winget uninstall 'Snipping Tool'
+"⏳ (1/4) Removing system applications..."
+& winget uninstall 'OMEN Audio Control'
+& winget uninstall 'OMEN Gaming Hub'
+& winget uninstall 'Clipchamp'
+& winget uninstall 'Cortana'
+& winget uninstall 'Microsoft News'
+& winget uninstall 'MSN Weather'
+& winget uninstall 'Get Help'
+& winget uninstall 'Microsoft Tips'
+& winget uninstall 'Office'
+& winget uninstall 'Microsoft.Office'
+& winget uninstall 'Microsoft Solitaire Collection'
+& winget uninstall 'Solitaire & Casual Games'
+& winget uninstall 'Microsoft Sticky Notes'
+& winget uninstall 'Paint'
+& winget uninstall 'Microsoft People'
+& winget uninstall 'Power Automate'
+& winget uninstall 'Microsoft To Do'
+& winget uninstall 'Microsoft Photos'
+& winget uninstall 'Windows Camera'
+& winget uninstall 'Windows Clock'
+& winget uninstall 'Windows Calculator'
+& winget uninstall 'Feedback Hub'
+& winget uninstall 'Windows Maps'
+& winget uninstall 'Windows Voice Recorder'
+& winget uninstall 'Your Phone'
+& winget uninstall 'Windows Media Player'
+& winget uninstall 'Movies & TV'
+& winget uninstall 'Microsoft Family'
+& winget uninstall 'Quick Assist'
+& winget uninstall 'Microsoft Teams'
+& winget uninstall 'Mail and Calendar'
+& winget uninstall 'Snipping Tool'
 
-"⏳ (2/3) Removing Xbox associated applications..."
-winget uninstall 'Xbox'
-winget uninstall 'Xbox TCUI'
-winget uninstall 'Xbox Game Bar Plugin'
-winget uninstall 'Xbox Identity Provider'
-winget uninstall 'Xbox Game Speech Window'
+"⏳ (2/4) Removing Xbox associated applications..."
+& winget uninstall 'Xbox'
+& winget uninstall 'Xbox TCUI'
+& winget uninstall 'Xbox Game Bar Plugin'
+& winget uninstall 'Xbox Identity Provider'
+& winget uninstall 'Xbox Game Speech Window'
 
-"⏳ (3/3) Removing miscellaneous Apps that crowd the Start Menu..."
-winget uninstall 'Spotify Music'
-winget uninstall 'Messenger'
-winget uninstall 'Instagram'
-winget uninstall 'Whatsapp'
-winget uninstall 'Netflix'
-winget uninstall 'LinkedIn'
-winget uninstall 'Prime Video for Windows'
+"⏳ (3/4) Removing HP bloatware..."
+& winget uninstall "AD2F1837.HPJumpStarts"
+& winget uninstall "AD2F1837.HPPCHardwareDiagnosticsWindows"
+& winget uninstall "AD2F1837.HPPowerManager"
+& winget uninstall "AD2F1837.HPPrivacySettings"
+& winget uninstall "AD2F1837.HPSupportAssistant"
+& winget uninstall "AD2F1837.HPSureShieldAI"
+& winget uninstall "AD2F1837.HPSystemInformation"
+& winget uninstall "AD2F1837.HPQuickDrop"
+& winget uninstall "AD2F1837.HPWorkWell"
+& winget uninstall "AD2F1837.myHP"
+& winget uninstall "AD2F1837.HPDesktopSupportUtilities"
+& winget uninstall "AD2F1837.HPQuickTouch"
+& winget uninstall "AD2F1837.HPEasyClean"
+& winget uninstall "AD2F1837.HPSystemInformation"
 
-"✅ Finished"
+"⏳ (4/4) Removing miscellaneous Apps that crowd the Start Menu..."
+& winget uninstall 'Spotify Music'
+& winget uninstall 'Messenger'
+& winget uninstall 'Instagram'
+& winget uninstall 'Whatsapp'
+& winget uninstall 'Netflix'
+& winget uninstall 'LinkedIn'
+& winget uninstall 'Prime Video for Windows'
+
+"✅ Bloatware removed."
 exit 0 # success
