dede93c33b4f6855a11aa319f8f1ab30a49640d2
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-06-17 07:59:00 +0200

    Updated list-ssh-key.ps1

--- scripts/list-ssh-key.ps1
+++ scripts/list-ssh-key.ps1
@@ -2,10 +2,10 @@
 .SYNOPSIS
         List the public SSH key
 .DESCRIPTION
-        This PowerShell script lists the user's public SSH key (resides in the ~/.ssh folder).
+        This PowerShell script lists the user's public SSH key which resides in the ~/.ssh folder.
 .EXAMPLE
         PS> ./list-ssh-key.ps1
-	✅ Public SSH key is: ssh-ed25519 AAAC3NzaC1lZDINTE5AAAAIKjhD0zlk9HV6OAXVtluUJ5c2BawfetGDLWu0CA1R markus@tux
+	✅ Public key: ssh-ed25519 AAAC3NzaC1lZDINTE5AAAAIKjhD0zlk9HV6OAXVtluUJ5c2BawfetGDLWu0CA1R markus@tux
 .LINK
         https://github.com/fleschutz/PowerShell
 .NOTES
@@ -17,10 +17,12 @@ try {
 		$key = Get-Content "~/.ssh/id_ed25519.pub"
 	} elseif (Test-Path "~/.ssh/id_rsa.pub") {
 		$key = Get-Content "~/.ssh/id_rsa.pub"	
+	} elseif (Test-Path "~/.ssh/id_dsa.pub") {
+		$key = Get-Content "~/.ssh/id_dsa.pub"	
 	} else {
 		throw "No public SSH key found - execute 'new-ssh-key.ps1' to create one"
 	}
-	Write-Host "✅ Public SSH key is: $key"
+	Write-Host "✅ Public key: $key"
 	exit 0 # success
 } catch {
 	"⚠️ ERROR: $($Error[0])"
