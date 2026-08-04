63a946487fa8a41618efd81d6bdf1331c01e8893
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-08-04 08:51:14 +0200

    Added new.c, new.cpp, new.h, and new.jpg

---
+++ scripts/data/templates/new.c
@@ -0,0 +1,7 @@
+// This source code does ...
+#include <stdio.h>
+
+int main() {
+	printf("Hello World\n");
+	return 0;
+}
---
+++ scripts/data/templates/new.cpp
@@ -0,0 +1,7 @@
+// This source code does ...
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
