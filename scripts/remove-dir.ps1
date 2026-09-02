19946f5b5bc006c70d0c266f8b3cd6b4dab3db82
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-09-02 18:35:06 +0200

    Updated remove-dir.ps1

--- scripts/remove-dir.ps1
+++ scripts/remove-dir.ps1
@@ -3,13 +3,13 @@
         Removes a directory
 .DESCRIPTION
         This PowerShell script removes the given directory. It removes all files and all subfolders in the directory!
-	IMPORTANT NOTE: Use with care! This cannot be undone!
+	NOTE: Use with care! This cannot be undone!
 .PARAMETER path
         Specifies the path to the directory tree to remove
 .EXAMPLE
         PS> ./remove-dir.ps1 C:\Temp
-	⏳ Removing directory tree at: C:\Temp... (please wait)
-	✅ Removed 'C:\Temp\' in 9s.
+	⏳ Removing directory C:\Temp and subfolder... (please wait)
+	✅ Directory C:\Temp\ removed in 9s.
 .LINK
         https://github.com/fleschutz/PowerShell
 .NOTES
@@ -24,14 +24,14 @@ try {
 	$stopWatch = [system.diagnostics.stopwatch]::startNew()
 	if (!(Test-Path "$path" -pathType container)) { throw "Cannot access directory at: '$path'" }
 
-	"⏳ Removing directory tree at: $path ... (please wait)"
+	"⏳ Removing directory $path and subfolder... (please wait)"
 	$lastExitCode = 0
 	Remove-Item -force -recurse -confirm:$false $path
 	if ($lastExitCode -ne 0) { throw "'Remove-Item $path' failed with exit code $lastExitCode" }
 	if (Test-Path "$path" -pathType container) { throw "Removal of '$path' failed, it's still available" }
 
 	[int]$elapsed = $stopWatch.Elapsed.TotalSeconds
-	"✅ Removed '$path' in $($elapsed)s."
+	"✅ Directory $path removed in $($elapsed)s."
 	exit 0 # success
 } catch {
 	"⚠️ ERROR: $($Error[0]) (in script line $($_.InvocationInfo.ScriptLineNumber))"
